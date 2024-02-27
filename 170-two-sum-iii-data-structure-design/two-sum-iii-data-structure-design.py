bfca44a0b051bd048f07b85c4b86d6c9d3e7bb9e
fury93 <www.fanreala@gmail.com>
2024-02-27 04:41:30 +0300

    Time: 204 ms (87.95%) | Memory: 21.2 MB (47.97%) - LeetSync

--- 170-two-sum-iii-data-structure-design/two-sum-iii-data-structure-design.py
+++ 170-two-sum-iii-data-structure-design/two-sum-iii-data-structure-design.py
@@ -1,27 +1,26 @@
 #from sortedcontainers import SortedList
-
 class TwoSum:
 
     def __init__(self):
-        self.data = []
-        #self.data = defaultdict(int)
+        self.data = defaultdict(int)
         #self.data = SortedList()
+        #self.data = []
 
     def add(self, number: int) -> None:
-        #self.data[number] += 1
+        self.data[number] += 1
         #self.data.add(number)
-        self.data.append(number)
+        #self.data.append(number)
 
     def find(self, value: int) -> bool:
-        #for n1 in self.data:
-        #    n2 = value - n1
-        #    if n2 in self.data:
-        #        if n2 != n1 or self.data[n2] > 1: return True
+        for n1 in self.data:
+            n2 = value - n1
+            if n2 in self.data:
+                if n2 != n1 or self.data[n2] > 1: return True
         
-        #return False
+        return False
         
         #solution3
-        self.data.sort()
+        #self.data.sort()
         # solution2
         l, r = 0, len(self.data) - 1
         while l < r:
@@ -33,11 +32,3 @@ class TwoSum:
             else:
                 return True
         return False
-
-        
-
-
-# Your TwoSum object will be instantiated and called as such:
-# obj = TwoSum()
-# obj.add(number)
-# param_2 = obj.find(value)
