131198120c50f68f47534e1d8d7a5db941cb6281
fury93 <www.fanreala@gmail.com>
2024-02-28 18:42:37 +0300

    Time: 513 ms (97.38%) | Memory: 76.5 MB (99.79%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -5,7 +5,7 @@ class LinkedNode:
         self.prev = None
         self.next = None
 
-class LRUCache:
+class LRUCache2:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = dict()
@@ -51,3 +51,23 @@ class LRUCache:
         node.next = self.tail
         self.tail.prev.next = node
         self.tail.prev = node
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        self.cache[key] = self.cache.pop(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
+
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.pop(next(iter(self.cache)))
