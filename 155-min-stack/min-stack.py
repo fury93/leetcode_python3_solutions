092214724377ee5991cd3cef100f9688e26d6838
fury93 <www.fanreala@gmail.com>
2024-03-02 00:32:08 +0300

    Time: 61 ms (29.99%) | Memory: 20.3 MB (85.25%) - LeetSync

---
+++ 155-min-stack/min-stack.py
@@ -0,0 +1,56 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+        
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val < self.min_stack[-1][0]:
+            self.min_stack.append([val, 1])
+        elif self.min_stack and self.min_stack[-1][0] == val:
+            self.min_stack[-1][1] += 1
+        
+    def pop(self) -> None:
+        if not self.stack: return
+        val = self.stack.pop()
+        if self.min_stack[-1][0] == val:
+            self.min_stack[-1][1] -= 1
+        if self.min_stack[-1][1] == 0:
+            self.min_stack.pop()
+        
+    def top(self) -> int:
+        return self.stack[-1] if self.stack else None
+
+    def getMin(self) -> int:
+        if not self.min_stack: return None
+        return self.min_stack[-1][0]
+
+class MinStack2:
+
+    def __init__(self):
+        self.data = []
+        
+    def push(self, val: int) -> None:
+        curMin = min(val, self.data[-1][1]) if self.data else val
+        self.data.append((val, curMin))
+        
+    def pop(self) -> None:
+        self.data.pop() if self.data else None
+
+    def top(self) -> int:
+        return self.data[-1][0] if self.data else None
+
+    def getMin(self) -> int:
+        if not self.data:
+            return float('-inf')
+        return self.data[-1][1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
