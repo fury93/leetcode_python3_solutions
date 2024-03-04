fe2c6fc10be3bf55c0940e99eacc8e79bb168850
fury93 <www.fanreala@gmail.com>
2024-03-04 22:34:00 +0300

    Time: 39 ms (33.21%) | Memory: 17 MB (29.64%) - LeetSync

--- 355-design-twitter/design-twitter.py
+++ 355-design-twitter/design-twitter.py
@@ -15,7 +15,7 @@ class Twitter:
         # unique id for tweet according time, here also can be used timestamp or more advanced UUID https://docs.python.org/3/library/uuid.html
         self.uuid = itertools.count()                                     # it's just a number 1,2,3....
         self.subscribers = defaultdict(set)                               # userId => set(subscribers1, subscriber2, ...)
-        self.subscriptions = defaultdict(set)                             #userId => set(subscription1, subscription2, ...)
+        self.subscriptions = defaultdict(set)                             # userId => set(subscription1, subscription2, ...)
         self.feed = defaultdict(lambda: deque(maxlen = self.__feedLimit)) # userId => deque(feed)
         self.posts = defaultdict(deque)                                   # userId => deque(user posts, newest from the left), or DoubleLinkelList if needed deletion support
 
@@ -32,8 +32,8 @@ class Twitter:
         if followerId in self.subscribers[followeeId]: return
         self.subscribers[followeeId].add(followerId)
         self.subscriptions[followerId].add(followeeId)
-        self.__updateUserFeed(followerId)
-        #self.__addUserToFeed(followerId, followeeId)
+        #self.__updateUserFeed(followerId)
+        self.__addUserToFeed(followerId, followeeId) # here you can choose this special method or __updateUserFeed()
         
     def unfollow(self, followerId: int, followeeId: int) -> None:
         if followerId not in self.subscribers[followeeId]: return
@@ -50,21 +50,20 @@ class Twitter:
     def __clearFeed(self, userId: int) -> None:
         self.feed[userId].clear()
 
+    # it's max 20 tweets, so instead of calling __updateUserFeed() can be used simple sorting of current feed and new user feed
     def __addUserToFeed(self, userId, addUserId):
-        # it's max 20 tweets, so instead of calling __updateUserFeed() can be used simple sorting
         feed = sorted(list(self.feed[userId]) + list(self.posts[addUserId]), reverse = True)[:self.__feedLimit]
         self.__clearFeed(userId)
         for tweet in feed:
             self.__addToFeed(userId, tweet, False)
         
+        # An alternative approach with eliminating old tweets over limit using automatic feed slicing (deque max len)
         #feed = sorted(list(self.feed[userId]) + list(self.posts[addUserId]))
         #self.__clearFeed(userId)
-        # here newFeed can be sliced to maxLimit size(10) manually or automatically by queue using maxLimit size
         #list(map(lambda tweet: self.__addToFeed(userId, tweet), feed))
 
     def __updateUserFeed(self, userId) -> None:
         self.__clearFeed(userId)
-        
         tweets = heapq.merge(*(self.posts[idx] for idx in self.subscriptions[userId] | {userId}), reverse = True)
         limitTweets = islice(tweets, self.__feedLimit)
 
