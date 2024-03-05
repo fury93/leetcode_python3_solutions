ed8627bbefc6db7570f9543474c59c6d7de02ec8
fury93 <www.fanreala@gmail.com>
2024-03-06 01:46:51 +0300

    Time: 173 ms (90.47%) | Memory: 18.6 MB (60.59%) - LeetSync

--- 353-design-snake-game/design-snake-game.py
+++ 353-design-snake-game/design-snake-game.py
@@ -1,13 +1,14 @@
 class SnakeGame:
 
     def __init__(self, width: int, height: int, food: List[List[int]]):
-        self.snake = deque([(0, 0)]) # head - left, tail - right
-        self.food = deque((x, y) for x, y in food)
-        self.snakeCells = set((0, 0))
+        self.snake = deque([(0, 0)]) # head - left, tail - right, used for the order
+        self.snakeCells = set((0, 0)) # use for fast search if current cell busy with the snake body
+        self.food = deque((x, y) for x, y in food) # I prefere to use tuples for coordinats
         self.w = width
         self.h = height
 
     def move(self, direction: str) -> int:
+        # 1. Make new move, it's new head position
         row, col = self.snake[0]
         match direction:
             case 'R': col += 1
@@ -15,21 +16,25 @@ class SnakeGame:
             case 'U': row -= 1
             case 'D': row += 1
 
-        if not (0 <= row < self.h and 0 <= col < self.w):
-            return -1 # border
+        # 2. Check if head doesn't go out of bounds (hits a wall)
+        if not (0 <= row < self.h and 0 <= col < self.w): return -1 
 
         head = (row, col)
+        # 3. If a food in the head cell => remove this food and leave old tail
+        # 4. Else remove old tail, it will be moved to new head cell later
         if self.food and self.food[0] == head:
             self.food.popleft()
         else:
             self.snakeCells.discard(self.snake.pop())
         
-        if head in self.snakeCells:
-            return -1 # snake body already in this cell
+        # 5. check if new head don't cross the snake body
+        if head in self.snakeCells: return -1
         
+        # 6. Add new head 
         self.snake.appendleft(head)
         self.snakeCells.add(head)
 
+        # 7. Score equal to the snake size without the head
         return len(self.snake) - 1
 
 # Your SnakeGame object will be instantiated and called as such:
