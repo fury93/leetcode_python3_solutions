8524588d00dbdc1b8cccfa3b7c3bd37b8c6b155b
fury93 <www.fanreala@gmail.com>
2024-03-06 01:37:22 +0300

    Time: 176 ms (86.44%) | Memory: 18.4 MB (93.01%) - LeetSync

--- 353-design-snake-game/design-snake-game.py
+++ 353-design-snake-game/design-snake-game.py
@@ -1,10 +1,11 @@
 class SnakeGame:
 
     def __init__(self, width: int, height: int, food: List[List[int]]):
-        self.grid = [[None] * width for _ in range(height)]
-        self.snake = deque([(0, 0)]) # head - most left, tail - most right
-        self.food = deque(food)
-        self.grid[0][0] = 1 # there is snake inumn this cell
+        self.snake = deque([(0, 0)]) # head - left, tail - right
+        self.food = deque((x, y) for x, y in food)
+        self.snakeCells = set((0, 0))
+        self.w = width
+        self.h = height
 
     def move(self, direction: str) -> int:
         row, col = self.snake[0]
@@ -14,28 +15,22 @@ class SnakeGame:
             case 'U': row -= 1
             case 'D': row += 1
 
-        if not (0 <= row < len(self.grid) and 0 <= col < len(self.grid[0])):
+        if not (0 <= row < self.h and 0 <= col < self.w):
             return -1 # border
 
-        head = [row, col]
-        if self.food and head == self.food[0]:
+        head = (row, col)
+        if self.food and self.food[0] == head:
             self.food.popleft()
         else:
-            row1, col1 = self.snake.pop() # remove tail
-            self.grid[row1][col1] = None
+            self.snakeCells.discard(self.snake.pop())
         
-        if self.grid[row][col] == 1:
+        if head in self.snakeCells:
             return -1 # snake body already in this cell
         
         self.snake.appendleft(head)
-        self.grid[row][col] = 1
+        self.snakeCells.add(head)
 
         return len(self.snake) - 1
-
-
-        
-        
-
 
 # Your SnakeGame object will be instantiated and called as such:
 # obj = SnakeGame(width, height, food)
