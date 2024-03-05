5460a726794e10819a312adcbaee787ef7a2fc0b
fury93 <www.fanreala@gmail.com>
2024-03-05 03:28:44 +0300

    Time: 130 ms (10.90%) | Memory: 24.1 MB (5.21%) - LeetSync

---
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        res = len(s)
+        s = [(k, len(list(g))) for k, g in groupby(s)]
+        
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l][0] == s[r][0]:
+                res -= s[l][1] + s[r][1]
+                l, r = l + 1, r - 1
+            else:
+                return res
+
+        return 0 if s[l][1] > 1 else 1
