e36db9df219c12ed094d5bd08a8ea68465272570
fury93 <www.fanreala@gmail.com>
2024-03-06 14:49:55 +0300

    Time: 505 ms (20.19%) | Memory: 30.9 MB (47.52%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,6 +1,42 @@
 class Solution:
-    # BS solution
+    # 1 Sort
     def findDuplicate(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return nums[i]
+
+    # 2 Set
+    def findDuplicate2(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
+
+    # Negative Marking
+    def findDuplicate3(self, nums: List[int]) -> int:
+        for num in nums:
+            cur = abs(num)
+            if nums[cur] < 0:
+                duplicate = cur
+                break
+            nums[cur] = -nums[cur]
+
+        # Restore numbers
+        for i in range(len(nums)):
+            nums[i] = abs(nums[i])
+
+        return duplicate
+
+    # 4. Cyclic sort
+    def findDuplicate4(self, nums: List[int]) -> int:
+        while nums[0] != nums[nums[0]]:
+            nums[nums[0]], nums[0] = nums[0], nums[nums[0]]
+        return nums[0]
+
+    # 5. BS 
+    def findDuplicate5(self, nums: List[int]) -> int:
         low, high = 1, len(nums) - 1
         
         while low <= high:
@@ -14,3 +50,46 @@ class Solution:
                 low = cur + 1
                 
         return duplicate
+
+    # 6. Sum of Set Bits
+    def findDuplicate6(self, nums: List[int]) -> int:
+        duplicate = 0
+        n = len(nums) - 1
+        bits = n.bit_length()
+        for bit in range(bits):
+            mask = 1 << bit
+            base_count = 0
+            nums_count = 0
+            for i in range(n + 1):
+                # If bit is set in number (i) then add 1 to base_count
+                if i & mask:
+                    base_count += 1
+                    
+                # If bit is set in nums[i] then add 1 to nums_count
+                if nums[i] & mask:
+                    nums_count += 1
+                    
+            # If the current bit is more frequently set in nums than it is in 
+            # the range [1, 2, ..., n] then it must also be set in the duplicate number.
+            if nums_count - base_count > 0:
+                duplicate |= mask
+                
+        return duplicate
+
+        # 7. Floyd's Tortoise and Hare (Cycle Detection)
+    def findDuplicate7(self, nums):
+        # Find the intersection point of the two runners.
+        tortoise = hare = nums[0]
+        while True:
+            tortoise = nums[tortoise]
+            hare = nums[nums[hare]]
+            if tortoise == hare:
+                break
+        
+        # Find the "entrance" to the cycle.
+        tortoise = nums[0]
+        while tortoise != hare:
+            tortoise = nums[tortoise]
+            hare = nums[hare]
+        
+        return hare
