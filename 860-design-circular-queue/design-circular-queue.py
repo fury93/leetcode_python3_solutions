fbb56b2aafec97c584cd05f9dd23f15641025c2b
fury93 <www.fanreala@gmail.com>
2024-03-07 20:03:48 +0300

    Time: 63 ms (32.79%) | Memory: 17.7 MB (18.95%) - LeetSync

--- 860-design-circular-queue/design-circular-queue.py
+++ 860-design-circular-queue/design-circular-queue.py
@@ -1,4 +1,96 @@
+class Node:
+    def __init__(self, val = -1):
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class DLL:
+    def __init__(self):
+        self.size = 0
+        self.dummy_start = Node()
+        self.dummy_end = Node()
+        self.dummy_start.next = self.dummy_end
+        self.dummy_end.prev = self.dummy_start
+
+    def append(self, node) -> Node:
+        left, right = self.dummy_end.prev, self.dummy_end
+        node.next = right
+        node.prev = left
+        left.next = node
+        right.prev = node
+        self.size += 1
+        
+        return node
+
+    def popLeft(self) -> bool:
+        if self.size == 0: return False
+        firstNode = self.dummy_start.next
+        self.dummy_start.next = firstNode.next
+        firstNode.next.prev = self.dummy_start
+        del firstNode
+        self.size -= 1
+        return True
+
+    def peekLeft(self) -> Node:
+        return self.dummy_start.next
+
+    def peekRight(self) -> Node:
+        return self.dummy_end.prev
+    
+    def getSize(self) -> int:
+        return self.size
+
 class MyCircularQueue:
+
+    def __init__(self, k: int):
+        """
+        Initialize your data structure here. Set the size of the queue to be k.
+        """
+        self.maxSize = k
+        self.q = DLL()
+        
+
+    def enQueue(self, value: int) -> bool:
+        """
+        Insert an element into the circular queue. Return true if the operation is successful.
+        """
+        if self.isFull(): return False 
+        self.q.append(Node(value))
+        return True
+
+    def deQueue(self) -> bool:
+        """
+        Delete an element from the circular queue. Return true if the operation is successful.
+        """
+        return self.q.popLeft()
+
+    def Front(self) -> int:
+        """
+        Get the front item from the queue.
+        """
+        print(self.q.dummy_start.next, self.q.dummy_end.prev)
+        return self.q.peekLeft().val
+
+    def Rear(self) -> int:
+        """
+        Get the last item from the queue.
+        """
+        return self.q.peekRight().val
+
+    def isEmpty(self) -> bool:
+        """
+        Checks whether the circular queue is empty or not.
+        """
+        return self.q.getSize() == 0
+
+    def isFull(self) -> bool:
+        """
+        Checks whether the circular queue is full or not.
+        """
+        return self.maxSize == self.q.getSize()
+
+
+class MyCircularQueue2:
 
     def __init__(self, k: int):
         """
