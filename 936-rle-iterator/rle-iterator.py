d633b7fff3bdf15a9c2cb5dcbe47e35996846fea
fury93 <www.fanreala@gmail.com>
2024-03-08 15:37:40 +0300

    Time: 46 ms (25.45%) | Memory: 17 MB (99.52%) - LeetSync

---
+++ 936-rle-iterator/rle-iterator.py
@@ -0,0 +1,21 @@
+class RLEIterator:
+
+    def __init__(self, encoding: List[int]):
+        self.s = encoding
+        self.p = 0
+
+    def next(self, n: int) -> int:
+        while self.p < len(self.s):
+            if self.s[self.p] >= n:
+                self.s[self.p] -= n
+                return  self.s[self.p+1]
+            n -= self.s[self.p]
+            self.p += 2
+        
+        return -1
+        
+
+
+# Your RLEIterator object will be instantiated and called as such:
+# obj = RLEIterator(encoding)
+# param_1 = obj.next(n)
