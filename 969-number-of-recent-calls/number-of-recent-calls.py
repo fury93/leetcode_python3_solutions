3c9625281116ec2a7b9d1e989840f119f7f3a470
fury93 <www.fanreala@gmail.com>
2024-03-08 21:35:25 +0300

    Time: 203 ms (37.25%) | Memory: 22.4 MB (26.75%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.py
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -6,6 +6,17 @@ class RecentCounter:
 
     def ping(self, t: int) -> int:
         self.data.append(t)
+        
+        return len(self.data) - bisect_left(self.data, t - self.k)
+
+class RecentCounter3:
+
+    def __init__(self):
+        self.data = deque()
+        self.k = 3000
+
+    def ping(self, t: int) -> int:
+        self.data.append(t)
 
         while self.data[0] < t - self.k:
             self.data.popleft()
