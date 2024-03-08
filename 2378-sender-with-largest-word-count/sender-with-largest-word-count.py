a0d003ef1bfdfb16830ba3ddfc92fd04cd2609e5
fury93 <www.fanreala@gmail.com>
2024-03-09 01:09:39 +0300

    Time: 326 ms (90.03%) | Memory: 23.9 MB (89.10%) - LeetSync

--- 2378-sender-with-largest-word-count/sender-with-largest-word-count.py
+++ 2378-sender-with-largest-word-count/sender-with-largest-word-count.py
@@ -1,5 +1,17 @@
 class Solution:
     def largestWordCount(self, messages: List[str], senders: List[str]) -> str:
+        d, maxSenderName= defaultdict(int), '@'
+        for mes, name in zip(messages, senders):
+            d[name] += len(mes.split())
+            if d[name] > d[maxSenderName]:
+                maxSenderName = name
+            elif d[name] == d[maxSenderName] and name > maxSenderName:
+                maxSenderName = name
+        
+        return maxSenderName
+
+class Solution2:
+    def largestWordCount(self, messages: List[str], senders: List[str]) -> str:
         d = defaultdict(int)
         for m, n in zip(messages, senders):
             d[n] += len(m.split())
