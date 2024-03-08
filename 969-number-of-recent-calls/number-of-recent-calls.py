b54b15c3297b8c247a7a774ada63a59cc7bf4afb
fury93 <www.fanreala@gmail.com>
2024-03-08 21:55:12 +0300

    Time: 187 ms (83.91%) | Memory: 22.2 MB (34.41%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.py
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -3,6 +3,26 @@ class RecentCounter:
     def __init__(self):
         self.data = deque()
         self.k = 3000
+        self.calls = 0
+
+    def ping(self, t: int) -> int:
+        self.calls += 1
+        if not self.data or self.data[-1][0] < t:
+            self.data.append([t, 1])
+        else:
+            self.data[-1][0] += 1
+        
+        while self.data[0][0] < t - self.k:
+            self.calls -= self.data.popleft()[1]
+        
+        return self.calls
+
+
+class RecentCounter2:
+
+    def __init__(self):
+        self.data = deque()
+        self.k = 3000
 
     def ping(self, t: int) -> int:
         self.data.append(t)
@@ -23,24 +43,21 @@ class RecentCounter3:
         
         return len(self.data)
 
-class RecentCounter2:
+class RecentCounter4:
 
     def __init__(self):
-        self.data = deque()
-        self.k = 3000
-        self.calls = 0
+        self.k = 3001
+        self.data = [[0, 0]] * (self.k)
 
     def ping(self, t: int) -> int:
-        self.calls += 1
-        if not self.data or self.data[-1][0] < t:
-            self.data.append([t, 1])
+        key = t % self.k
+        if self.data[key][0] != t:
+            self.data[key] = [t, 1]
         else:
-            self.data[-1][0] += 1
+            self.data[key][1] += 1
+        minTime = t - self.k
         
-        while self.data[0][0] < t - self.k:
-            self.calls -= self.data.popleft()[1]
-        
-        return self.calls
+        return sum(val for time, val in self.data if time > minTime)
 
 
 # Your RecentCounter object will be instantiated and called as such:
