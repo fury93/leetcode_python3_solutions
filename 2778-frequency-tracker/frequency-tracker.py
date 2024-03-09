c8ed090cb2cf654696dc02c6eec7b79d8eb236b7
fury93 <www.fanreala@gmail.com>
2024-03-09 14:14:08 +0300

    Time: 473 ms (51.48%) | Memory: 72.3 MB (86.92%) - LeetSync

--- 2778-frequency-tracker/frequency-tracker.py
+++ 2778-frequency-tracker/frequency-tracker.py
@@ -1,24 +1,24 @@
 class FrequencyTracker:
 
     def __init__(self):
-        self.nums = [0] * (10**5 + 1) # frequency for the num
-        self.freq = [0] * (10**5 + 1) # count of nums with such freq
+        #self.nums = [0] * (10**5 + 1) # frequency for the num
+        #self.freq = [0] * (10**5 + 1) # count of nums with such freq
+        self.nums = defaultdict(int)
+        self.freq = defaultdict(int)
 
     def add(self, number: int) -> None:
-        numFreq = self.nums[number]
-        if numFreq > 0:
-            self.freq[numFreq] -= 1
+        oldFreq = self.nums[number]
+        if oldFreq > 0:
+            self.freq[oldFreq] -= 1
         self.nums[number] += 1
-        self.freq[numFreq + 1] += 1
+        self.freq[oldFreq + 1] += 1
 
     def deleteOne(self, number: int) -> None:
-        numFreq = self.nums[number]
-        if not numFreq: return
-        self.freq[numFreq] -= 1
+        oldFreq = self.nums[number]
+        if oldFreq == 0: return
+        self.freq[oldFreq] -= 1
         self.nums[number] -= 1
-        self.freq[numFreq - 1] += 1
-
-        
+        self.freq[oldFreq - 1] += 1
 
     def hasFrequency(self, frequency: int) -> bool:
         return self.freq[frequency] > 0
