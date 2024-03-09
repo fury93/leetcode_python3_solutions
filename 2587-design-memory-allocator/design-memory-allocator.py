68fdbf91aec1403cc18f0e3f5dffb6d38fc857ea
fury93 <www.fanreala@gmail.com>
2024-03-09 21:33:43 +0300

    Time: 134 ms (77.13%) | Memory: 17.8 MB (32.17%) - LeetSync

--- 2587-design-memory-allocator/design-memory-allocator.py
+++ 2587-design-memory-allocator/design-memory-allocator.py
@@ -7,22 +7,18 @@ class Allocator:
         self.busyBlocks = defaultdict(list)
 
     def allocate(self, size: int, mID: int) -> int:
-        #print('allocate', size, mID)
         for start, end in self.freeBlocks:
             if (blockEnd:=start+size-1) > end: continue # block size is not enough
             self.busyBlocks[mID].append((start, blockEnd))
             self.freeBlocks.remove((start, end))
             if blockEnd < end: # have free size in the block, return new free size to freeBlocks list
                 self.freeBlocks.add((blockEnd+1, end))
-            #print(start, end, 'busy=>', self.busyBlocks, 'free=>', self.freeBlocks)
             return start
 
-        #print('allocate not possible')
         return -1 # no avaialbe blocks
 
 
     def free(self, mID: int) -> int:
-        #print('free', mID)
         freeBlocksCnt = 0
         for start, end in self.busyBlocks[mID]:
             freeBlocksCnt += end - start + 1
@@ -36,17 +32,16 @@ class Allocator:
                     end = nextEnd
                     self.freeBlocks.remove((nextStart, nextEnd))
 
-            if idx > 0: # check previous free block if it does exist
+            # check left (previous) free block if it does exist
+            if idx > 0: 
                 prevStart, prevEnd = self.freeBlocks[idx - 1]
                 if prevEnd + 1 == start:
                     start = prevStart
                     self.freeBlocks.remove((prevStart, prevEnd))
-                
 
-            
             self.freeBlocks.add((start, end))
         del self.busyBlocks[mID] # remove all released blocks
-        #print('busy=>', self.busyBlocks, 'free=>', self.freeBlocks)
+        
         return freeBlocksCnt
 
 
