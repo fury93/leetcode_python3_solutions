1d5cd856d2a60c13e11f0319c6a3d2633ce234ed
fury93 <www.fanreala@gmail.com>
2024-03-09 22:07:05 +0300

    Time: 354 ms (100.00%) | Memory: 71.8 MB (85.58%) - LeetSync

--- 2512-longest-uploaded-prefix/longest-uploaded-prefix.py
+++ 2512-longest-uploaded-prefix/longest-uploaded-prefix.py
@@ -1,16 +1,16 @@
 class LUPrefix:
 
     def __init__(self, n: int):
-        self.videos = [False] * n
+        self.videos = [False] * (n + 1)
         self.prefix = 0
 
     def upload(self, video: int) -> None:
         self.videos[video-1] = True
-        #while self.prefix < len(self.videos) and self.videos[self.prefix]:
+        #while self.videos[self.prefix]:
         #    self.prefix += 1
 
     def longest(self) -> int:
-        while self.prefix < len(self.videos) and self.videos[self.prefix]:
+        while self.videos[self.prefix]:
             self.prefix += 1
         return self.prefix
         
