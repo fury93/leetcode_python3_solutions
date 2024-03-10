3cf9cd0de8f65f8c325353de67de15681638b657
fury93 <www.fanreala@gmail.com>
2024-03-10 19:42:02 +0300

    Time: 559 ms (59.52%) | Memory: 63.7 MB (57.14%) - LeetSync

--- 2161-stock-price-fluctuation/stock-price-fluctuation.py
+++ 2161-stock-price-fluctuation/stock-price-fluctuation.py
@@ -1,5 +1,40 @@
-from sortedcontainers import SortedList
+#from sortedcontainers import SortedList
 class StockPrice:
+
+    def __init__(self):
+        self.timeToPrice = defaultdict(int)
+        self.minPrices = []
+        self.maxPrices = []
+        self.maxTime = 0
+
+    def update(self, timestamp: int, price: int) -> None:
+        heappush(self.minPrices, (price, timestamp))
+        heappush(self.maxPrices, (-price, timestamp))
+        self.timeToPrice[timestamp] = price
+        self.maxTime = max(self.maxTime, timestamp)
+
+    def current(self) -> int:
+        return self.timeToPrice[self.maxTime]
+
+    def maximum(self) -> int:
+        while self.maxPrices:
+            price, timestamp = self.maxPrices[0]
+            if self.timeToPrice[timestamp] != -price:
+                heappop(self.maxPrices)
+            else: break
+        
+        return -self.maxPrices[0][0] if self.maxPrices else -1
+
+    def minimum(self) -> int:
+        while self.minPrices:
+            price, timestamp = self.minPrices[0]
+            if self.timeToPrice[timestamp] != price:
+                heappop(self.minPrices)
+            else: break
+        
+        return self.minPrices[0][0] if self.minPrices else -1
+
+class StockPrice2:
 
     def __init__(self):
         self.timeToPrice = defaultdict(int)
