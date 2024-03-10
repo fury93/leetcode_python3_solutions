bed61f0cd596398d84f5a9a6e9ac0d376082fc36
fury93 <www.fanreala@gmail.com>
2024-03-11 01:19:06 +0300

    Time: 489 ms (85.62%) | Memory: 48.9 MB (50.98%) - LeetSync

--- 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.py
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.py
@@ -1,9 +1,9 @@
 class FindSumPairs:
 
     def __init__(self, nums1: List[int], nums2: List[int]):
-        self.nums1 = nums1
-        self.nums2 = nums2
+        self.nums1Cnt = Counter(nums1)
         self.nums2Cnt = Counter(nums2)
+        self.nums2 = nums2
 
     def add(self, index: int, val: int) -> None:
         oldVal = self.nums2[index]
@@ -12,10 +12,10 @@ class FindSumPairs:
 
     def count(self, tot: int) -> int:
         res = 0
-        for n1 in self.nums1:
+        for n1, n1Cnt in self.nums1Cnt.items():
             n2 = tot - n1
             if n2 in self.nums2Cnt:
-                res += self.nums2Cnt[n2]
+                res += self.nums2Cnt[n2] * n1Cnt
         return res
         
 # Your FindSumPairs object will be instantiated and called as such:
