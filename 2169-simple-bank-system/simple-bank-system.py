7f777fdf154ff8449d6eee6cd66112611e3807b8
fury93 <www.fanreala@gmail.com>
2024-03-10 18:31:56 +0300

    Time: 545 ms (20.72%) | Memory: 46.3 MB (40.99%) - LeetSync

--- 2169-simple-bank-system/simple-bank-system.py
+++ 2169-simple-bank-system/simple-bank-system.py
@@ -4,19 +4,23 @@ class Bank:
         self.balance = balance
 
     def transfer(self, account1: int, account2: int, money: int) -> bool:
-        if self.withdraw(account1, money): 
-            if self.deposit(account2, money): return True 
-            self.deposit(account1, money)
+        if self.isValidAccount(account1) and self.isValidAccount(account2):
+            if self.withdraw(account1, money): 
+                self.deposit(account2, money)
+                return True
         return False
 
     def deposit(self, account: int, money: int) -> bool:
-        if 1 <= account <= len(self.balance): 
+        if self.isValidAccount(account): 
             self.balance[account-1] += money
             return True 
         return False
         
     def withdraw(self, account: int, money: int) -> bool:
-        if 1 <= account <= len(self.balance) and self.balance[account-1] >= money: 
+        if self.isValidAccount(account) and self.balance[account-1] >= money: 
             self.balance[account-1] -= money
             return True 
         return False
+
+    def isValidAccount(self, account):
+        return 1 <= account <= len(self.balance)
