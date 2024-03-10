143fde72291e58255490cb021877d73aa8161408
fury93 <www.fanreala@gmail.com>
2024-03-10 14:47:16 +0300

    Time: 267 ms (97.41%) | Memory: 23.3 MB (71.48%) - LeetSync

--- 906-walking-robot-simulation/walking-robot-simulation.py
+++ 906-walking-robot-simulation/walking-robot-simulation.py
@@ -1,5 +1,26 @@
 class Solution:
     def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+        obstacles = set(tuple(_) for _ in obstacles)
+        res, curX, curY, diffX, diffY  = 0, 0, 0, 0, 1
+
+        for command in commands:
+            if command < 0: #change direction 
+                match command:
+                    case -1: diffX, diffY = diffY, -diffX # clockwise
+                    case -2: diffX, diffY = -diffY, diffX # anti-clockwise
+                continue
+            
+            for _ in range(command):
+                nextX, nextY = curX + diffX, curY + diffY
+                if (nextX, nextY) in obstacles: break
+                curX, curY = nextX, nextY
+            
+            res = max(res, curX*curX + curY*curY)
+
+        return res
+
+class Solution2:
+    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
         obstacles = set((x, y) for x, y in obstacles)
         res, curPosition, curDirection  = 0, (0, 0), 0
         #if (0, 0) in obstacles: return res # this test-case doesn't work
@@ -14,8 +35,8 @@ class Solution:
                 continue
             
             for _ in range(command):
-                nextPosition = tuple(pos + diff for pos, diff in zip(curPosition, direction[curDirection]))
-                #nextPosition = (curPosition[0] + direction[curDirection][0], curPosition[1] + direction[curDirection][1])
+                #nextPosition = tuple(pos + diff for pos, diff in zip(curPosition, direction[curDirection]))
+                nextPosition = (curPosition[0] + direction[curDirection][0], curPosition[1] + direction[curDirection][1])
                 if nextPosition in obstacles:
                     break
                 curPosition = nextPosition
