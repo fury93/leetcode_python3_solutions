d422894d1ad810aa30fdf19e71a2af204f9bf3c3
fury93 <www.fanreala@gmail.com>
2024-03-10 11:22:23 +0300

    Time: 43 ms (100.00%) | Memory: 16.5 MB (100.00%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.py
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.py
@@ -1,7 +1,7 @@
 class Solution:
     def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
         volume = sum(apple)
-        for i, boxCapacity in enumerate(sorted(capacity, reverse= True)):
+        for i, boxCapacity in enumerate(sorted(capacity, reverse= True), start=1):
             volume -= boxCapacity
-            if volume <= 0: return i + 1
+            if volume <= 0: break
         return i
