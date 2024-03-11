ebbb60134826d8c844448358305483b123fbb866
fury93 <www.fanreala@gmail.com>
2024-03-11 12:16:11 +0300

    Time: 43 ms (8.63%) | Memory: 16.7 MB (26.18%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,4 +1,10 @@
 class Solution:
-    def customSortString(self, order: str, s: str) -> str:
+    def customSortString2(self, order: str, s: str) -> str:
         d = {v:k for k, v in enumerate(order)}
         return ''.join(sorted(s, key=lambda ch: d[ch] if ch in d else len(s)))
+
+    def customSortString(self, order: str, s: str) -> str:
+        cnt = Counter(s)
+        chars1 = [ch * cnt[ch] for ch in order]
+        chars2 = [ch * cnt for ch, cnt in cnt.items() if ch not in order]
+        return ''.join(chain(chars1, chars2))
