3c9bf1a0f37d52f9dacfa444fdfd82f931e33fb9
fury93 <www.fanreala@gmail.com>
2024-03-11 12:50:10 +0300

    Time: 37 ms (46.56%) | Memory: 16.6 MB (61.09%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,10 +1,25 @@
 from itertools import repeat
 class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        freq = defaultdict(int)
+        for ch in s:
+            freq[ch] += 1
+        
+        chars = []
+        for ch in order:
+            chars.append(ch * freq[ch])
+            freq[ch] = 0
+
+        for ch, cnt in freq.items():
+            chars.append(ch * cnt)
+
+        return ''.join(chars)
+
     def customSortString2(self, order: str, s: str) -> str:
         d = {v:k for k, v in enumerate(order)}
         return ''.join(sorted(s, key=lambda ch: d[ch] if ch in d else len(s)))
 
-    def customSortString(self, order: str, s: str) -> str:
+    def customSortString3(self, order: str, s: str) -> str:
         cnt = Counter(s)
         chars1 = [repeat(ch, cnt[ch]) for ch in order]
         chars2 = [repeat(ch, cnt) for ch, cnt in cnt.items() if ch not in order]
