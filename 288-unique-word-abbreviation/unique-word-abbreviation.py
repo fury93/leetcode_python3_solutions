179f89cb2ce75383f906d2d1aeb0405d42fcf83d
fury93 <www.fanreala@gmail.com>
2024-03-13 01:43:14 +0300

    Time: 177 ms (90.35%) | Memory: 26.1 MB (98.84%) - LeetSync

--- 288-unique-word-abbreviation/unique-word-abbreviation.py
+++ 288-unique-word-abbreviation/unique-word-abbreviation.py
@@ -15,3 +15,17 @@ class ValidWordAbbr:
 
     def convertToAbbr(self, w):
         return w if (ln:=len(w)) < 2 else f'{w[0]}{ln-2}{w[-1]}'
+
+class ValidWordAbbr2:
+
+    def __init__(self, dictionary: List[str]):
+        self.abbr = defaultdict(set)
+        for w in dictionary:
+            self.abbr[self.convertToAbbr(w)].add(w)
+
+    def isUnique(self, word: str) -> bool:
+        key = self.convertToAbbr(word)
+        return not self.abbr[key] or len(self.abbr[key]) == 1 and word in self.abbr[key]
+
+    def convertToAbbr(self, w):
+        return f'{w[0]}{ln-2 if (ln:=len(w)) > 2 else ""}{w[-1]}'
