447b472fe1e987a39fde12bb898d8384bb7fe0ed
fury93 <www.fanreala@gmail.com>
2024-03-15 13:28:28 +0300

    Time: 38 ms (31.87%) | Memory: 16.5 MB (65.35%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,18 +1,26 @@
 class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
+    # bijection
+    def wordPattern3(self, pattern: str, s: str) -> bool:
         s = s.split()
         return (len(set(pattern)) ==
                 len(set(s)) ==
                 len(set(zip_longest(pattern,s))))
 
-    def wordPattern2(self, pattern: str, s: str) -> bool:
-        dic1, dic2 = {}, {}
+    def wordPattern(self, pattern: str, s: str) -> bool:
         words = s.split()
-
         if len(pattern) != len(words):
             return False
 
-        for c, word in zip(pattern, words):
+        charToWord, wordToChar = {}, {}
+        for char, word in zip(pattern, words):
+            if char in charToWord and charToWord[char] != word:
+                return False
+            if word in wordToChar and wordToChar[word] != char:
+                return False
+            charToWord[char] = word
+            wordToChar[word] = char
+            continue
+            # old approach
             if c not in dic1 and word not in dic2:
                 dic1[c] = word
                 dic2[word] = c
