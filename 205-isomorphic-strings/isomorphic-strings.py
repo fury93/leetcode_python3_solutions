fa18cda8b246eba96984a282160daf87db7d488c
fury93 <www.fanreala@gmail.com>
2024-03-15 13:44:22 +0300

    Time: 42 ms (61.55%) | Memory: 16.7 MB (67.28%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,5 +1,9 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        return len(set(s)) == len(set(set(t))) == len(set(zip(s, t)))
+    
+    
+    def isIsomorphic2(self, s: str, t: str) -> bool:
         if (len(s) != len(t)):
             return False
         
@@ -13,6 +17,21 @@ class Solution:
                 return False
                 
         return True
+
+    #  First occurence transformation
+    def transformString(self, s: str) -> str:
+        index_mapping = {}
+        new_str = []
+        
+        for i, c in enumerate(s):
+            if c not in index_mapping:
+                index_mapping[c] = i
+            new_str.append(str(index_mapping[c]))
+        
+        return " ".join(new_str)
+    
+    def isIsomorphic3(self, s: str, t: str) -> bool:
+        return self.transformString(s) == self.transformString(t)
             
             
             
