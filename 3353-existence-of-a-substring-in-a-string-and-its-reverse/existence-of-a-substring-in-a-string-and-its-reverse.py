d2cbd68911ec3a4401738e73ee897ef343e82b00
fury93 <www.fanreala@gmail.com>
2024-03-17 19:17:19 +0300

    Time: 30 ms (50.00%) | Memory: 16.5 MB (100.00%) - LeetSync

--- 3353-existence-of-a-substring-in-a-string-and-its-reverse/existence-of-a-substring-in-a-string-and-its-reverse.py
+++ 3353-existence-of-a-substring-in-a-string-and-its-reverse/existence-of-a-substring-in-a-string-and-its-reverse.py
@@ -1,4 +1,11 @@
 class Solution:
+    def isSubstringPresent4(self, s: str) -> bool:
+        s += ' '
+        for i in range(1, len(s)-1):
+            if s[i] == s[i-1] or s[i-1] == s[i+1]:
+                return True
+        return False
+
     def isSubstringPresent3(self, s: str) -> bool:
         s += ' '
         for prev, cur, nxt in zip(s, s[1:], s[2:]):
@@ -13,8 +20,10 @@ class Solution:
         return False
 
     def isSubstringPresent(self, s: str) -> bool:
-        s += ' '
-        for i in range(1, len(s)-1):
-            if s[i] == s[i-1] or s[i-1] == s[i+1]:
-                return True
-        return False
+        def getPairs(s):
+            pairs = set()
+            for i in range(1, len(s)):
+                pairs.add(s[i-1:i+1])
+            return pairs
+        
+        return getPairs(s) & getPairs(s[::-1])
