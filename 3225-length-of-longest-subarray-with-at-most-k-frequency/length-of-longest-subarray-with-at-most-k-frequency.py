c2e34ee645b8fa92e6c04996c972e91b4fb1f013
fury93 <www.fanreala@gmail.com>
2024-03-28 18:10:04 +0300

    Time: 1059 ms (98.09%) | Memory: 30.9 MB (99.20%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+    def maxSubarrayLength2(self, nums: List[int], k: int) -> int:
         res, l, freq = 0, 0, defaultdict(int)
 
         for r, num in enumerate(nums):
@@ -10,3 +10,21 @@ class Solution:
             res = max(res, r - l + 1)
 
         return res 
+
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        moreThanK, l, freq = 0, 0, defaultdict(int)
+
+        for r, num in enumerate(nums):
+            freq[num] += 1
+            
+            if freq[num] == k + 1:
+                moreThanK += 1
+            
+            if moreThanK > 0:
+                leftBoundNum = nums[l]
+                if freq[leftBoundNum] == k + 1:
+                    moreThanK -= 1
+                freq[leftBoundNum] -= 1
+                l += 1
+
+        return r - l + 1 
