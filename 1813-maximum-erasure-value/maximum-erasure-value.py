57c2f57954442a819eaa21266fa7c0e7642b9ef1
fury93 <www.fanreala@gmail.com>
2024-04-14 08:18:31 +0300

    Time: 825 ms (95.32%) | Memory: 30 MB (53.61%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.py
+++ 1813-maximum-erasure-value/maximum-erasure-value.py
@@ -1,9 +1,9 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
         res, l = 0, 0
-        # prefix = list(itertools.accumulate(nums, initial = 0))
         pos = [-1] * (10**4 + 1)
         prefix = [0] * (len(nums) + 1)
+        
         for r, n in enumerate(nums):
             prefix[r+1] = prefix[r] + n
             if pos[n] >= l:
