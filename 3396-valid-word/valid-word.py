dd6a6be88dd77a2bd6fd42909f1526af8b087dbb
fury93 <www.fanreala@gmail.com>
2024-05-21 01:40:15 +0300

    Time: 39 ms (38.10%) | Memory: 16.6 MB (12.83%) - LeetSync

--- 3396-valid-word/valid-word.py
+++ 3396-valid-word/valid-word.py
@@ -1,11 +1,12 @@
 class Solution:
     def isValid(self, word: str) -> bool:
-        if len(word) < 3 or not word.isalnum(): return False
+        if len(word) < 3: return False
         
         isVowel, isConsonant = False, False,
         vowels = set(['a', 'e', 'i', 'o', 'u'])
 
         for ch in word:
+            if not ch.isalnum(): return False
             if ch.isalpha():
                 if ch.lower() in vowels:
                     isVowel = True
