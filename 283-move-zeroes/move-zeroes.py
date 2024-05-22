d9bd7a550a1cb626c0078c0bf3177a70a7349b2d
fury93 <www.fanreala@gmail.com>
2024-05-22 21:26:20 +0300

    Time: 130 ms (42.46%) | Memory: 18.1 MB (78.51%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,13 +1,13 @@
 class Solution:
-    def moveZeroes2(self, nums: List[int]) -> None:
+    def moveZeroes(self, nums: List[int]) -> None:
         snawBall = 0
         for i, n in enumerate(nums):
             if n == 0:
-                snawBall +=1
+                snawBall += 1
             elif snawBall > 0:
                 nums[i], nums[i - snawBall] =  nums[i - snawBall], nums[i]
 
-    def moveZeroes(self, nums: List[int]) -> None:
+    def moveZeroes2(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
@@ -15,4 +15,4 @@ class Solution:
         for r in range(len(nums)):
             if nums[r] != 0:
                 nums[l], nums[r] = nums[r], nums[l]
-                l +=1
+                l += 1
