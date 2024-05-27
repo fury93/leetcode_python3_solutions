b7c6c4c9350c942f98cadba2c2dc9047c4c9aeb6
fury93 <www.fanreala@gmail.com>
2024-05-27 22:44:05 +0300

    Time: 78 ms (97.21%) | Memory: 18.1 MB (95.40%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,5 +1,6 @@
 class Solution:
-    def canFinish(self, n, prerequisites):
+    # Approach 1: Topological Sort Using Kahn's Algorithm
+    def canFinish2(self, n, prerequisites):
         adj, rank = [[] for _ in range(n)], [0] * n
         
         for child, parent in prerequisites:
@@ -19,3 +20,31 @@ class Solution:
                     q.append(nextCourse)
 
         return visited == n
+
+     # Approach 2: Depth First Search
+    def canFinish(self, n, prerequisites):
+        adj = [[] for _ in range(n)]
+        for child, parent in prerequisites:
+            adj[parent].append(child)
+
+        visited = [False] * n
+        stack = set()
+
+        def dfs(i, visited, stack):
+            if i in stack: return False
+            if visited[i]: return True
+            visited[i] = True
+            stack.add(i)
+
+            for child in adj[i]:
+                if not dfs(child, visited, stack): return False
+
+            stack.remove(i)
+            return True
+
+
+        for i in range(n):
+            if not dfs(i, visited, stack): return False
+
+        return True
+
