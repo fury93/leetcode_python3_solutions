0cdc52731b0b9bc9268bded27b9a8cd6c7f8dbaf
fury93 <www.fanreala@gmail.com>
2024-06-04 04:40:41 +0300

    Time: 38 ms (49.08%) | Memory: 16.6 MB (80.97%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,7 +1,11 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> int:
+    def longestPalindrome2(self, s: str) -> int:
         res = 0
         for n in Counter(s).values():
             res += n if not n & 1 or not res & 1 else n-1
 
         return res
+
+    def longestPalindrome(self, s):
+        odds = sum(v & 1 for v in collections.Counter(s).values())
+        return len(s) - odds + bool(odds)
