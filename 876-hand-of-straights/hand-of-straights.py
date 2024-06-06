ba72d54c654390b1c4a9db170ccb7f0b94c3850c
fury93 <www.fanreala@gmail.com>
2024-06-06 09:26:58 +0300

    Time: 153 ms (88.84%) | Memory: 18.1 MB (86.00%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -1,10 +1,8 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         cnt = Counter(hand)
-        values = sorted(cnt.keys())
 
-        for i in range(len(values)):
-            val = values[i]
+        for val in sorted(cnt):
             if not cnt[val]: continue
             diff = cnt[val]
             for j in range(val, val + groupSize):
