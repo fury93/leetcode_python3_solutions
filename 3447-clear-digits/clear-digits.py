0b0677a57e26a06b96dd83bc0ba502965d990c2b
fury93 <www.fanreala@gmail.com>
2024-06-09 21:36:00 +0300

    Time: 20 ms (100.00%) | Memory: 16.6 MB (100.00%) - LeetSync

--- 3447-clear-digits/clear-digits.py
+++ 3447-clear-digits/clear-digits.py
@@ -1,5 +1,6 @@
 class Solution:
-    def clearDigits(self, s: str) -> str:
+    # Stack
+    def clearDigits2(self, s: str) -> str:
         stack = []
         for ch in s:
             if ch.isalpha():
@@ -8,3 +9,15 @@ class Solution:
                 stack.pop()
 
         return ''.join(stack)
+
+    # Two pointers
+    def clearDigits(self, s: str) -> str:
+        l, chars = 0, list(s)
+        for r, ch in enumerate(chars):
+            if ch.isalpha():
+                chars[l] = ch
+                l += 1
+            else:
+                l -= 1
+
+        return ''.join(chars[:l])
