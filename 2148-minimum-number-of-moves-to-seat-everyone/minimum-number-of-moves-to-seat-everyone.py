69d94da9d17c39a9e2ebdeb610d34aa2f791204e
fury93 <www.fanreala@gmail.com>
2024-06-14 11:46:02 +0300

    Time: 48 ms (97.75%) | Memory: 16.4 MB (82.07%) - LeetSync

---
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        res = 0
+        for seat, student in zip(sorted(seats), sorted(students)):
+            res += abs(seat - student)
+        return res
+
+    def minMovesToSeat2(self, seats: List[int], students: List[int]) -> int:
+        max_position = max(max(seats), max(students))
+        differences = [0] * (max_position)
+
+        # Count the available seats at each position
+        for position in seats:
+            differences[position - 1] += 1
+
+        # Remove a seat for each student at that position
+        for position in students:
+            differences[position - 1] -= 1
+
+        moves = 0
+        unmatched = 0
+        for difference in differences:
+            moves += abs(unmatched)
+            unmatched += difference
+
+        return moves
