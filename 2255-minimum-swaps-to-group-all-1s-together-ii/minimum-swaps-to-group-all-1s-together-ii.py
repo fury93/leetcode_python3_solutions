862c322aa1caaf56c5a79dcca528d586b1887847
fury93 <www.fanreala@gmail.com>
2024-06-27 17:19:15 +0300

    Time: 671 ms (82.09%) | Memory: 19.6 MB (96.52%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.py
@@ -1,5 +1,5 @@
 class Solution:
-    def minSwaps(self, nums: List[int]) -> int:
+    def minSwaps2(self, nums: List[int]) -> int:
         ones = sum(nums)
         if ones <= 1: return 0
         
@@ -12,4 +12,14 @@ class Solution:
             zeros -= nums[(i + 1 - ones) % len(nums)] == 0
         
         return minSwaps
+
+    def minSwaps(self, nums: List[int]) -> int:
+        ones = sum(nums)
+        curOnes = maxOnes = sum(nums[:ones])
         
+        for i in range(len(nums)):
+            curOnes -= nums[i]
+            curOnes += nums[(i + ones) % len(nums)]
+            maxOnes = max(maxOnes, curOnes)
+        
+        return ones - maxOnes
