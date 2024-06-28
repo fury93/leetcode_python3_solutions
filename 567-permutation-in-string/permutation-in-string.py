37db212994ff85c78f797e89d80fe1f9ccd1ddb3
fury93 <www.fanreala@gmail.com>
2024-06-28 03:04:36 +0300

    Time: 60 ms (61.68%) | Memory: 16.7 MB (57.81%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -2,6 +2,27 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s2) < len(s1): return False
         
+        need, have, left = Counter(s1), Counter(), 0
+
+        for right, ch in enumerate(s2):
+            if ch not in need:
+                have.clear()
+                left = right + 1
+                continue
+            
+            have[ch] += 1
+            # window can be only with required symbols with count not more than needed
+            while have[ch] > need[ch]:
+                have[s2[left]] -= 1
+                left += 1
+                
+            if right - left + 1 == len(s1): return True
+
+        return False
+
+    def checkInclusion3(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1): return False
+        
         need, matched = Counter(s1), 0
 
         for i, ch in enumerate(s2, start = 1):
