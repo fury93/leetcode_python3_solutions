a9d4bdfd9c6daf231efdce02e2dba3226e1d0720
fury93 <www.fanreala@gmail.com>
2024-07-04 03:09:33 +0300

    Time: 958 ms (53.11%) | Memory: 75.2 MB (8.20%) - LeetSync

---
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        backbone = ListNode()
+        prefix, prev = 0, backbone
+        while head:
+            if head.val == 0:
+                if prefix > 0:
+                    prev.next = ListNode(prefix)
+                    prefix = 0
+                    prev = prev.next
+            else:
+                prefix += head.val
+            head = head.next
+
+        return backbone.next
