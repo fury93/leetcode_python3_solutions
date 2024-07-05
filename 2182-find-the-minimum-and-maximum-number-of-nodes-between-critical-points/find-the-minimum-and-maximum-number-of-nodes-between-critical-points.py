6637fec857646c6fcbc0bf79e67b16154001d575
fury93 <www.fanreala@gmail.com>
2024-07-05 03:45:43 +0300

    Time: 364 ms (38.78%) | Memory: 44.2 MB (82.89%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -1,10 +1,25 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
+        prev, cur  = head, head.next
+        pos, firstPos, prevPos, minDist = 0, None, None, math.inf
+        
+        while cur.next:
+            pos += 1
+            if prev.val < cur.val > cur.next.val or prev.val > cur.val < cur.next.val:
+                if prevPos:
+                    minDist = min(minDist, pos - prevPos)
+                    prevPos = pos
+                else:
+                    firstPos = prevPos = pos
+                
+            cur, prev = cur.next, cur
+
+        if minDist == math.inf:
+            return [-1, -1]
+
+        return [minDist, prevPos - firstPos]
+    
+    def nodesBetweenCriticalPoints2(self, head: Optional[ListNode]) -> List[int]:
         prev, cur, idx, i = head, head.next, [], 1
         while cur and cur.next:
             if prev.val < cur.val > cur.next.val or prev.val > cur.val < cur.next.val:
