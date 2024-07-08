f1611b339ce8f0fa684d27c9862f29329a301478
fury93 <www.fanreala@gmail.com>
2024-07-08 14:56:39 +0300

    Time: 476 ms (67.02%) | Memory: 46.9 MB (36.52%) - LeetSync

---
+++ 1982-remove-duplicates-from-an-unsorted-linked-list/remove-duplicates-from-an-unsorted-linked-list.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicatesUnsorted(self, head: ListNode) -> ListNode:
+        dummy, d, cur = ListNode(0, head), defaultdict(int), head
+        while cur:
+            d[cur.val] += 1
+            cur = cur.next
+        
+        cur, prev = head, dummy
+        while cur:
+            if d[cur.val] > 1:
+                prev.next = cur.next
+            else:
+                prev = prev.next
+            cur = cur.next
+        return dummy.next
+        
+        
