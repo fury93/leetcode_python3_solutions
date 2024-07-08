154bff2e01754b74a2ae9e2b38644774a78405a6
fury93 <www.fanreala@gmail.com>
2024-07-08 19:56:15 +0300

    Time: 566 ms (8.59%) | Memory: 21.9 MB (49.88%) - LeetSync

--- 305-number-of-islands-ii/number-of-islands-ii.py
+++ 305-number-of-islands-ii/number-of-islands-ii.py
@@ -1,31 +1,35 @@
 class Solution:
     def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
-        res, ds = [], UnionFind()
-        for x, y in positions:
-            ds.add((x, y))
-            for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-                xx, yy = x + dx, y + dy
-                if ds.isExist((xx, yy)):
-                    ds.union((xx, yy), (x, y))
-            res.append(ds.getCnt())
+        res, uf = [], UnionFind(m , n)
+        for row, col in positions:
+            uf.add(row, col)
+            for rowDiff, colDiff in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                adjacentRow, adjacentCol = row + rowDiff, col + colDiff
+                if 0 <= adjacentRow < m and 0 <= adjacentCol < n:
+                    uf.union(row, col, adjacentRow, adjacentCol)
+            res.append(uf.count())
                     
         return res
 
 class UnionFind:
-    def __init__(self):
-        self.root = dict()
-        self.rank = dict()
+    def __init__(self, rows, cols):
+        size = rows * cols
+        self.root = [None for i in range(size)]
+        self.rank = [0] * size
         self.cnt = 0
+        self.rowSize = cols
 
     def find(self, x):
-        if x == self.root[x]:
-            return x
-        self.root[x] = self.find(self.root[x])
+        if x != self.root[x]:
+            self.root[x] = self.find(self.root[x])
         return self.root[x]
 
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
+    def union(self, row1, col1, row2, col2):
+        idx1, idx2 = self.getIdx(row1, col1), self.getIdx(row2, col2)
+        if not self.exist(idx1) or not self.exist(idx2): return
+
+        rootX = self.find(idx1)
+        rootY = self.find(idx2)
         if rootX != rootY:
             if self.rank[rootX] > self.rank[rootY]:
                 self.root[rootY] = rootX
@@ -36,14 +40,19 @@ class UnionFind:
                 self.rank[rootX] += 1
             self.cnt -= 1
 
-    def add(self, key):
-        if key in self.root: return
-        self.root[key] = key
-        self.rank[key] = 1
-        self.cnt += 1
+    def getIdx(self, row, col):
+        return row * self.rowSize + col
 
-    def getCnt(self):
+    def add(self, row, col):
+        idx = self.getIdx(row, col)
+        if not self.exist(idx):
+            self.root[idx] = idx
+            self.rank[idx] = 1
+            self.cnt += 1
+
+    def count(self):
         return self.cnt
 
-    def isExist(self, key):
-        return key in self.root
+    def exist(self, idx):
+        print(idx)
+        return self.root[idx] is not None
