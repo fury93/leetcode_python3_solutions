a73838b84b942e0e86a1402dec7094a2e416cca9
fury93 <www.fanreala@gmail.com>
2024-07-08 18:21:58 +0300

    Time: 234 ms (71.86%) | Memory: 19.3 MB (44.13%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,21 +1,141 @@
-from itertools import product
 class Solution:
-    L = '1'
-    W = '0'
-
     def numIslands(self, grid: List[List[str]]) -> int:
-        res, R, C = 0, len(grid), len(grid[0])
+        res, ROWS, COLS, LAND, WATER = 0, len(grid), len(grid[0]), '1', '0'
 
-        def dfs(x, y):
-            if 0 <= x < R and 0 <= y < C and grid[x][y] == self.L:
-                grid[x][y] = self.W
-                for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-                    dfs(x + dx, y + dy)
+        def dfs(row, col):
+            if 0 <= row < ROWS and 0 <= col < COLS and grid[row][col] == LAND:
+                grid[row][col] = WATER
+                for diffRow, diffCol in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                    dfs(row + diffRow, col + diffCol)
 
-        for i, j in product(range(R), range(C)):
-                if grid[i][j] == self.L:
-                    res += 1
-                    dfs(i, j)
+        for row, col in product(range(ROWS), range(COLS)):
+            if grid[row][col] == LAND:
+                res += 1
+                dfs(row, col)
 
         return res
 
+# BFS official solution
+class Solution2:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
+        nr = len(grid)
+        nc = len(grid[0])
+        num_islands = 0
+
+        for r in range(nr):
+            for c in range(nc):
+                if grid[r][c] == "1":
+                    num_islands += 1
+                    grid[r][c] = 0  # mark as visited
+                    neighbors = []
+                    neighbors.append((r, c))
+                    while neighbors:
+                        row, col = neighbors.pop(0)
+                        if row - 1 >= 0 and grid[row - 1][col] == "1":
+                            neighbors.append((row - 1, col))
+                            grid[row - 1][col] = "0"
+                        if row + 1 < nr and grid[row + 1][col] == "1":
+                            neighbors.append((row + 1, col))
+                            grid[row + 1][col] = "0"
+                        if col - 1 >= 0 and grid[row][col - 1] == "1":
+                            neighbors.append((row, col - 1))
+                            grid[row][col - 1] = "0"
+                        if col + 1 < nc and grid[row][col + 1] == "1":
+                            neighbors.append((row, col + 1))
+                            grid[row][col + 1] = "0"
+        return num_islands
+
+# DFS official solution
+class Solution3:
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+
+        num_islands = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    self.dfs(grid, i, j)
+                    num_islands += 1
+
+        return num_islands
+
+    def dfs(self, grid, r, c):
+        if (
+            r < 0
+            or c < 0
+            or r >= len(grid)
+            or c >= len(grid[0])
+            or grid[r][c] != "1"
+        ):
+            return
+        grid[r][c] = "0"
+
+        self.dfs(grid, r - 1, c)
+        self.dfs(grid, r + 1, c)
+        self.dfs(grid, r, c - 1)
+        self.dfs(grid, r, c + 1)
+
+class UnionFind:
+    def __init__(self, grid):
+        self.count = 0
+        m, n = len(grid), len(grid[0])
+        self.parent = []
+        self.rank = []
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    self.parent.append(i * n + j)
+                    self.count += 1
+                else:
+                    self.parent.append(-1)
+                self.rank.append(0)
+
+    def find(self, i):
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, x, y):
+        rootx = self.find(x)
+        rooty = self.find(y)
+        if rootx != rooty:
+            if self.rank[rootx] > self.rank[rooty]:
+                self.parent[rooty] = rootx
+            elif self.rank[rootx] < self.rank[rooty]:
+                self.parent[rootx] = rooty
+            else:
+                self.parent[rooty] = rootx
+                self.rank[rootx] += 1
+            self.count -= 1
+
+    def getCount(self):
+        return self.count
+
+# Union-Find official solution
+class Solution3:
+    def numIslands(self, grid):
+        if not grid or not grid[0]:
+            return 0
+
+        nr = len(grid)
+        nc = len(grid[0])
+        uf = UnionFind(grid)
+
+        for r in range(nr):
+            for c in range(nc):
+                if grid[r][c] == "1":
+                    grid[r][c] = "0"
+                    if r - 1 >= 0 and grid[r - 1][c] == "1":
+                        uf.union(r * nc + c, (r - 1) * nc + c)
+                    if r + 1 < nr and grid[r + 1][c] == "1":
+                        uf.union(r * nc + c, (r + 1) * nc + c)
+                    if c - 1 >= 0 and grid[r][c - 1] == "1":
+                        uf.union(r * nc + c, r * nc + c - 1)
+                    if c + 1 < nc and grid[r][c + 1] == "1":
+                        uf.union(r * nc + c, r * nc + c + 1)
+
+        return uf.getCount()
