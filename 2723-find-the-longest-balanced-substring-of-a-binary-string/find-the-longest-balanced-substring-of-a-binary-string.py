079b11eed875b8c931dfa500619b1d135d9563eb
fury93 <www.fanreala@gmail.com>
2024-08-01 23:27:58 +0300

    Time: 33 ms (93.97%) | Memory: 16.6 MB (9.57%) - LeetSync

--- 2723-find-the-longest-balanced-substring-of-a-binary-string/find-the-longest-balanced-substring-of-a-binary-string.py
+++ 2723-find-the-longest-balanced-substring-of-a-binary-string/find-the-longest-balanced-substring-of-a-binary-string.py
@@ -1,16 +1,16 @@
 class Solution:
     def findTheLongestBalancedSubstring(self, s: str) -> int:
-        minConsequtiveSequence, zeros, ones = 0, 0, 0
+        longestSubstring, zeros, ones = 0, 0, 0
         for c in chain(s, '0'):
             if c == '0':
                 if ones:
-                    minConsequtiveSequence = max(minConsequtiveSequence, min(zeros, ones))
+                    longestSubstring = max(longestSubstring, min(zeros, ones))
                     zeros = ones = 0
                 zeros += 1
             else:
                 ones += 1
 
-        return minConsequtiveSequence * 2
+        return longestSubstring * 2
 
     def findTheLongestBalancedSubstring2(self, s: str) -> int:
         res, zeros, ones = 0, 0, 0
