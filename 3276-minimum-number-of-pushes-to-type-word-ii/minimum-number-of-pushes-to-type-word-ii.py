99892a1263c68a4467200c695d29e6eb90b852f4
fury93 <www.fanreala@gmail.com>
2024-08-06 22:33:23 +0300

    Time: 147 ms (73.03%) | Memory: 17.7 MB (36.75%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -1,20 +1,4 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        # Frequency list to store count of each letter
-        frequency = [0] * 26
-
-        # Count occurrences of each letter
-        for c in word:
-            frequency[ord(c) - ord("a")] += 1
-        # Sort frequencies in descending order
-        frequency.sort(reverse=True)
-
-        total_pushes = 0
-
-        # Calculate total number of presses
-        for i in range(26):
-            if frequency[i] == 0:
-                break
-            total_pushes += (i // 8 + 1) * frequency[i]
-
-        return total_pushes
+        freq = sorted(Counter(word).values(), reverse=True)
+        return sum((i // 8 + 1) * f for i, f in enumerate(freq))
