28e789ba241a5ec549c2c7ea9eef333da7b97d11
fury93 <www.fanreala@gmail.com>
2024-08-19 21:26:21 +0300

    Time: 153 ms (97.03%) | Memory: 16.9 MB (41.67%) - LeetSync

---
+++ 3516-design-neighbor-sum-service/design-neighbor-sum-service.py
@@ -0,0 +1,38 @@
+class NeighborSum:
+
+    def __init__(self, grid: List[List[int]]):
+        self.grid = grid
+        self.size = len(grid)
+        self.positions = [None] * self.size ** 2
+        
+        for i, j in product(range(self.size), range(self.size)):
+            self.positions[grid[i][j]] = i, j
+
+    def adjacentSum(self, value: int) -> int:
+        i, j = self.positions[value]
+        sm = 0
+        sm += self.grid[i-1][j] if i >= 1 else 0 # top
+        sm += self.grid[i+1][j] if i+1 < self.size else 0 # bottom
+        sm += self.grid[i][j-1] if j >= 1 else 0 # left
+        sm += self.grid[i][j+1] if j+1 < self.size else 0 # right
+
+        return sm
+
+        
+
+    def diagonalSum(self, value: int) -> int:
+        i, j = self.positions[value]
+        sm = 0
+        sm += self.grid[i-1][j-1] if i >= 1 and j >= 1 else 0 # top left
+        sm += self.grid[i+1][j+1] if i+1 < self.size and j+1 < self.size else 0 # bottom right
+        sm += self.grid[i-1][j+1] if i >= 1 and j+1 < self.size else 0 # top right
+        sm += self.grid[i+1][j-1] if i+1 < self.size and j >= 1 else 0 # bottome left
+
+        return sm
+        
+
+
+# Your NeighborSum object will be instantiated and called as such:
+# obj = NeighborSum(grid)
+# param_1 = obj.adjacentSum(value)
+# param_2 = obj.diagonalSum(value)
