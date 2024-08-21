8104b270eb848b9a802ff602eda3c6edf31b618e
fury93 <www.fanreala@gmail.com>
2024-08-22 00:37:06 +0300

    Time: 47 ms (100.00%) | Memory: 16.8 MB (100.00%) - LeetSync

--- 3577-convert-doubly-linked-list-to-array-i/convert-doubly-linked-list-to-array-i.py
+++ 3577-convert-doubly-linked-list-to-array-i/convert-doubly-linked-list-to-array-i.py
@@ -1,8 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev=None, next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+"""
 class Solution:
-    def toArray(self, root: 'Optional[Node]') -> List[int]:
+    def toArray2(self, root: 'Optional[Node]') -> List[int]:
         res, cur = [], root
         while cur:
             res.append(cur.val)
             cur = cur.next
         return res
+
+    def toArray(self, root: 'Optional[Node]') -> List[int]:
+        def convert(node, depth):
+            if not node: 
+                res = [None] * depth
+                return res
+            
+            res = convert(node.next, depth + 1)
+            res[depth] = node.val
+            return res
+
+        return convert(root, 0)
         
