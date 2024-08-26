54887ad8569a0963434a13dcb009c3284b096ef9
fury93 <www.fanreala@gmail.com>
2024-08-26 10:29:16 +0300

    Time: 41 ms (82.54%) | Memory: 18.1 MB (77.39%) - LeetSync

---
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        stack, res = [root], []
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                stack.extend(node.children)
+        
+        return res[::-1]
