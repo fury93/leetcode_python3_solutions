c83c9d2412b1014ab9635de61b45a46ec756434a
fury93 <www.fanreala@gmail.com>
2024-08-28 16:41:51 +0300

    Time: 39 ms (33.32%) | Memory: 16.6 MB (27.30%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,5 +1,6 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
+     # Bottom-up with a space optimization
+    def rob3(self, nums: List[int]) -> int:
         beforePrev = prev = 0
         for money in nums:
             prev, beforePrev = max(prev, beforePrev + money), prev
@@ -18,3 +19,19 @@ class Solution:
             money[i] = max(money[i-1], money[i-2] + nums[i])
 
         return money[-1]
+
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1: return nums[0]
+
+        memo = [None] * len(nums)
+        memo[0] = nums[0]
+        memo[1] = max(nums[0], nums[1])
+
+        def robToHouse(i):
+            if i < 0: return 0
+            if memo[i] is None:
+                memo[i] = max(robToHouse(i-1), robToHouse(i-2) + nums[i])
+
+            return memo[i]
+
+        return robToHouse(len(nums)-1)
