775a59b349fe03636c7f32b8832d5d2db0251ff0
fury93 <www.fanreala@gmail.com>
2024-08-28 21:55:27 +0300

    Time: 647 ms (91.18%) | Memory: 30.9 MB (83.44%) - LeetSync

--- 2725-mice-and-cheese/mice-and-cheese.py
+++ 2725-mice-and-cheese/mice-and-cheese.py
@@ -1,7 +1,7 @@
 class Solution:
     def miceAndCheese(self, reward1: List[int], reward2: List[int], k: int) -> int:
         points = sum(reward2)
-        heap = [] #minHeap, top k cheeses with best profit when replace reward2 to reward1
+        heap = [] #minHeap, top k cheeses with best profit when replace reward2 with reward1
 
         for r1, r2 in zip(reward1, reward2):
             profit = r1 - r2
