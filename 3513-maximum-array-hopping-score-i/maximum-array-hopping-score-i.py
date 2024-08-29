a5dbac2405a2aa37dc03f71b5043325934cc26e0
fury93 <www.fanreala@gmail.com>
2024-08-29 20:08:12 +0300

    Time: 131 ms (68.15%) | Memory: 16.6 MB (94.85%) - LeetSync

--- 3513-maximum-array-hopping-score-i/maximum-array-hopping-score-i.py
+++ 3513-maximum-array-hopping-score-i/maximum-array-hopping-score-i.py
@@ -1,5 +1,14 @@
 class Solution:
     def maxScore(self, nums: List[int]) -> int:
+        res, maxN = 0, 0
+        for i in range(len(nums)-1, 0, -1):
+            maxN = max(maxN, nums[i])
+            res += maxN
+
+        return res
+
+    # Stack
+    def maxScore2(self, nums: List[int]) -> int:
         stack = []
         for i, n in enumerate(nums):
             while stack and nums[stack[-1]] < n:
