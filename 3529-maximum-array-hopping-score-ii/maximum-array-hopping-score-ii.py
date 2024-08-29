2de0670cd677e1f2585b52b5e088d1e04417f91a
fury93 <www.fanreala@gmail.com>
2024-08-29 20:14:46 +0300

    Time: 865 ms (50.36%) | Memory: 31.1 MB (79.14%) - LeetSync

--- 3529-maximum-array-hopping-score-ii/maximum-array-hopping-score-ii.py
+++ 3529-maximum-array-hopping-score-ii/maximum-array-hopping-score-ii.py
@@ -1,9 +1,9 @@
 # the same https://leetcode.com/problems/maximum-array-hopping-score-i/
 class Solution:
     def maxScore(self, nums: List[int]) -> int:
-        res, maxN = 0, 0
+        res, maxHeight = 0, 0
         for i in range(len(nums)-1, 0, -1):
-            maxN = max(maxN, nums[i])
-            res += maxN
+            maxHeight = max(maxHeight, nums[i])
+            res += maxHeight
 
         return res
