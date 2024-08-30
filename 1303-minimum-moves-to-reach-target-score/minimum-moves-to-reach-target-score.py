276c7f5939e887bd2875315da36f329562737937
fury93 <www.fanreala@gmail.com>
2024-08-31 01:38:40 +0300

    Time: 30 ms (88.76%) | Memory: 16.5 MB (82.11%) - LeetSync

--- 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.py
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.py
@@ -1,16 +1,11 @@
+# similar problem https://leetcode.com/problems/broken-calculator/description/
 class Solution:
     def minMoves(self, target: int, maxDoubles: int) -> int:
         moves = 0
-
-        while target > 1:
-            if maxDoubles == 0: break
-
-            if target & 1 == 0:
-                target //= 2
-                maxDoubles -= 1
-            else:
-                target -= 1
-            moves += 1
+        while target > 1 and maxDoubles > 0:
+            moves += 1 + (target & 1)
+            maxDoubles -= 1
+            target >>= 1
 
         return moves + target - 1
             
