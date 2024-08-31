dd292ec6a80d3c7385e701c36f6871fc4cd63b60
fury93 <www.fanreala@gmail.com>
2024-08-31 22:26:24 +0300

    Time: 36 ms (65.70%) | Memory: 16.4 MB (58.03%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,26 @@
+class Solution:
+    # Top-down
+    def fib2(self, n: int) -> int:
+        if n < 2: return n
+        d = [None] * (n+1)
+        d[0] = 0
+        d[1] = 1
+        
+        def f(n):
+            if n in d:
+                return d[n]
+            d[n] = f(n-1) + f(n-2)
+            return d[n]
+        
+        return f(n)
+
+    def fib(self, n: int) -> int:
+        if n < 2: return n
+
+        prev1, prev2 = 0, 1
+
+        for i in range(2, n+1):
+            cur = prev1 + prev2
+            prev1, prev2 = prev2, cur
+
+        return prev2
