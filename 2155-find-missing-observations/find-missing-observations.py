dd9810a00c5fb0104085e70c43e1fd501dce4719
fury93 <www.fanreala@gmail.com>
2024-09-05 12:05:02 +0300

    Time: 1003 ms (44.12%) | Memory: 26.8 MB (57.22%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.py
+++ 2155-find-missing-observations/find-missing-observations.py
@@ -1,8 +1,19 @@
 class Solution:
-    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+    def missingRolls2(self, rolls: List[int], mean: int, n: int) -> List[int]:
         expectedSum = (len(rolls) + n) * mean - sum(rolls)
         if expectedSum < n or expectedSum > n * 6:
             return []
 
         quotient, remainder  = divmod(expectedSum, n)
         return [quotient + 1] * remainder + [quotient] * (n - remainder)
+    
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        expectedSum = (len(rolls) + n) * mean - sum(rolls)
+        if expectedSum < n or expectedSum > n * 6:
+            return []
+
+        quotient, remainder  = divmod(expectedSum, n)
+        res = [quotient] * n
+        for i in range(remainder):
+            res[i] += 1
+        return res
