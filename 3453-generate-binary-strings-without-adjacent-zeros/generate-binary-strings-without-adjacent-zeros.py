87b0ae3e4fb5b2cd5210587ceba1841a1b24fbc2
fury93 <www.fanreala@gmail.com>
2024-09-06 00:14:32 +0300

    Time: 69 ms (18.74%) | Memory: 18.8 MB (15.87%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.py
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.py
@@ -8,15 +8,8 @@ class Solution:
             if digits[i-1] != 0:
                 digits[i] = 0
                 generate(i+1)
-                #addDigit(digits, '0', i)
             digits[i] = 1
             generate(i+1)
-            #addDigit(digits, '1', i)
-
-        def addDigit(digits, addDigit, i):
-            digits.append(addDigit)
-            generate(digits, i-1)
-            digits.pop()
             
         res, digits = [], [None] * n
         generate(0)
