4a99efc936d4c67fdd62669811798727d88da261
fury93 <www.fanreala@gmail.com>
2024-09-06 00:15:15 +0300

    Time: 47 ms (85.96%) | Memory: 18.8 MB (15.87%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.py
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.py
@@ -2,13 +2,13 @@ class Solution:
     def validStrings(self, n: int) -> List[str]:
         def generate(i):
             if i == n:
-                res.append(''.join(map(str,digits)))
+                res.append(''.join(digits))
                 return
             
-            if digits[i-1] != 0:
-                digits[i] = 0
+            if digits[i-1] != '0':
+                digits[i] = '0'
                 generate(i+1)
-            digits[i] = 1
+            digits[i] = '1'
             generate(i+1)
             
         res, digits = [], [None] * n
