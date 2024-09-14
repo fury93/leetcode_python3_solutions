53cf039816095b531384e03180ae548b2be4ba76
fury93 <www.fanreala@gmail.com>
2024-09-14 17:16:29 +0300

    Time: 48 ms (89.83%) | Memory: 16.8 MB (22.26%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,5 +1,5 @@
 class Solution:
-    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+    def smallerNumbersThanCurrent2(self, nums: List[int]) -> List[int]:
         cnt = [0] * 101
         for n in nums:
             cnt[n] += 1
@@ -7,8 +7,11 @@ class Solution:
 
         return [cnt[n] for n in nums]
             
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        sorted_nums = sorted(nums)
+        return [bisect_left(sorted_nums, n) for n in nums]
 
-    def smallerNumbersThanCurrent2(self, nums: List[int]) -> List[int]:
+    def smallerNumbersThanCurrent3(self, nums: List[int]) -> List[int]:
         d = {}
         for id, n in enumerate(sorted(nums)):
             d.setdefault(n, id) #if n not in d
