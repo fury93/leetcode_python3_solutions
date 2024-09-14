7169e8d4343646bf10842799e144984d8dd230c8
fury93 <www.fanreala@gmail.com>
2024-09-15 01:55:43 +0300

    Time: 496 ms (73.63%) | Memory: 26.6 MB (54.39%) - LeetSync

--- 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.py
+++ 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.py
@@ -1,9 +1,8 @@
 class Solution:
     def maximumSetSize(self, nums1: List[int], nums2: List[int]) -> int:
         set1, set2, maxLen = set(nums1), set(nums2), len(nums1) // 2
-        uniqSet1 = set1 - set2
-        uniqSet2 = set2 - set1
-        intersectSet = set1 & set2
-        uniqSet1Len = min(maxLen, len(uniqSet1))
-        uniqSet2Len = min(maxLen, len(uniqSet2))
-        return min(len(nums1), uniqSet1Len + uniqSet2Len + len(intersectSet) )
+        duplicateLen = len(set1 & set2)
+        uniqSet1Len = min(maxLen, len(set1) - duplicateLen)
+        uniqSet2Len = min(maxLen, len(set2) - duplicateLen)
+        
+        return min(len(nums1), uniqSet1Len + uniqSet2Len + duplicateLen)
