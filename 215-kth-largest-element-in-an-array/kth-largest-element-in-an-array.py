4582c37d4e7aaf28a62fa62d2f19e0a115ca45ab
fury93 <www.fanreala@gmail.com>
2024-09-19 00:20:59 +0300

    Time: 434 ms (93.84%) | Memory: 29.8 MB (21.58%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,6 +1,6 @@
 class Solution:
-    # QuickSelect
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    # Approach 3: QuickSelect
+    def findKthLargest3(self, nums: List[int], k: int) -> int:
         if len(nums) == 1: return nums[0]
 
         def findKthLargest(nums, k):
@@ -19,3 +19,35 @@ class Solution:
                 return mid[0]
 
         return findKthLargest(nums, k)
+
+    # Approach 4: Counting Sort O(N + M)
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        min_value = min(nums)
+        max_value = max(nums)
+        count = [0] * (max_value - min_value + 1)
+
+        for num in nums:
+            count[num - min_value] += 1
+        
+        remain = k
+        for num in range(len(count) -1, -1, -1):
+            remain -= count[num]
+            if remain <= 0:
+                return num + min_value
+
+        return -1
+
+    # Approach 2: Min-Heap O(N*logK)
+    def findKthLargest2(self, nums, k):
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, num)
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        return heap[0]
+
+    # Approach 1: Sort O(N*LogN)
+    def findKthLargest1(self, nums, k):
+        nums.sort(reverse=True)
+        return nums[k - 1]
