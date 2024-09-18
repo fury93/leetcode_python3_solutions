2de39c398f614662afe465a63531c372d7f03157
fury93 <www.fanreala@gmail.com>
2024-09-19 01:26:52 +0300

    Time: 68 ms (76.96%) | Memory: 18.2 MB (5.82%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Kadane
+    def maxProduct(self, nums: List[int]) -> int:
+        prefix, suffix, max_so_far = 0, 0, float('-inf')
+        for i in range(len(nums)):
+            prefix = (prefix or 1) * nums[i]
+            suffix = (suffix or 1) * nums[~i]
+            max_so_far = max(max_so_far, prefix, suffix)
+        return max_so_far
+    
+    # Lee approach
+    def maxProduct(self, A):
+        B = A[::-1]
+        for i in range(1, len(A)):
+            A[i] *= A[i - 1] or 1
+            B[i] *= B[i - 1] or 1
+        return max(A + B)
