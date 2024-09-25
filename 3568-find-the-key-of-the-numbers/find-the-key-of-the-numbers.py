07fab4fa21ab987a79f7e938c150f7685959a3e2
fury93 <www.fanreala@gmail.com>
2024-09-26 02:53:27 +0300

    Time: 29 ms (92.81%) | Memory: 16.6 MB (47.85%) - LeetSync

--- 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.py
+++ 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.py
@@ -1,5 +1,6 @@
 class Solution:
-    def generateKey(self, num1: int, num2: int, num3: int) -> int:
+    # Math
+    def generateKey2(self, num1: int, num2: int, num3: int) -> int:
         res = 0
         for i in range(4):
             num1, rem1 = divmod(num1, 10)
@@ -9,3 +10,8 @@ class Solution:
             res += min(rem1, rem2, rem3) * decimalPlace
 
         return res
+
+    # Convert to string, ye15
+    def generateKey(self, num1: int, num2: int, num3: int) -> int:
+        vals = [str(x).zfill(4) for x in (num1, num2, num3)]
+        return int("".join(map(min, zip(*vals))))
