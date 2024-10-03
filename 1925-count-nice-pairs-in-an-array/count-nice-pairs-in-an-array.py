716ee70a91b32f4c91a82561bbeec5b5877ce9b8
fury93 <www.fanreala@gmail.com>
2024-10-03 18:19:36 +0300

    Time: 498 ms (88.14%) | Memory: 27 MB (81.21%) - LeetSync

--- 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.py
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.py
@@ -5,3 +5,12 @@ class Solution:
         for n in Counter(nums).values():
             res += n*(n-1)//2 
         return res % (10**9 + 7)
+
+    
+    def countNicePairs2(self, nums: List[int]) -> int:
+        res, d = 0, defaultdict(int)
+        for i, n in enumerate(nums):
+            k = n - int(str(n)[::-1])
+            res += d[k]
+            d[k] += 1
+        return res % (10**9 + 7)
