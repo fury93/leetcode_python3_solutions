edaa398abb75497e145591226714884cb9685a1f
fury93 <www.fanreala@gmail.com>
2024-10-06 13:22:04 +0300

    Time: 37 ms (43.79%) | Memory: 16.7 MB (13.04%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.py
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.py
@@ -1,23 +1,18 @@
 class Solution:
     def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
-        if len(sentence1) > len(sentence2):
-            return self.areSentencesSimilar(sentence2, sentence1)
-        
         s1 = sentence1.split()
         s2 = sentence2.split()
+
+        if len(s1) > len(s2):
+            s1, s2 = s2, s1
         
-        i = 0
-        while i < len(s1) and s1[i] == s2[i]:
-            i += 1
+        l = 0
+        while l < len(s1) and s1[l] == s2[l]:
+            l += 1
         
-        if i == len(s1): return True
+        r1, r2 = len(s1) - 1, len(s2) - 1
+        while r1 >= l and s1[r1] == s2[r2]:
+            r1 -= 1
+            r2 -= 1
         
-        j = len(s2) - 1
-        while j > i and s1[i] != s2[j]:
-            j -= 1
-        
-        while i < len(s1) and j < len(s2) and s1[i] == s2[j]:
-            i += 1
-            j += 1
-        
-        return i == len(s1) and j == len(s2)
+        return l > r1
