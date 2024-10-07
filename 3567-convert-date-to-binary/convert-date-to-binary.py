42bfe608d6dbf80108cb0fc92b3f723a18ea7c28
fury93 <www.fanreala@gmail.com>
2024-10-07 10:31:55 +0300

    Time: 29 ms (90.63%) | Memory: 16.6 MB (26.48%) - LeetSync

--- 3567-convert-date-to-binary/convert-date-to-binary.py
+++ 3567-convert-date-to-binary/convert-date-to-binary.py
@@ -2,3 +2,7 @@ class Solution:
     def convertDateToBinary(self, date: str) -> str:
         year, month, day = map(int, date.split('-'))
         return f'{year:b}-{month:b}-{day:b}'
+        
+        # return "-".join(f"{int(d):b}" for d in date.split("-"))
+        
+        # return  '-'.join([bin(int(num))[2:] for num in date.split('-')])
