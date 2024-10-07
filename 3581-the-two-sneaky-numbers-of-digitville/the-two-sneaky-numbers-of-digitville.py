5b93698bdebc03abb5415a1f6908a7c5fb081727
fury93 <www.fanreala@gmail.com>
2024-10-08 02:24:53 +0300

    Time: 55 ms (22.37%) | Memory: 16.7 MB (6.83%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
@@ -0,0 +1,17 @@
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        uniqNumsCnt = len(nums)-2
+        xorNums = reduce(xor, nums)
+        xorKeys = reduce(xor, range(uniqNumsCnt))
+        xorDuplicates = xorNums ^ xorKeys
+        firstDiffBit = xorDuplicates & -xorDuplicates # read more about negative bit representations
+
+        group1Nums = reduce(xor, (n for n in nums if n & firstDiffBit))
+        group1Keys = reduce(xor, (i for i in range(uniqNumsCnt) if i & firstDiffBit))
+
+        group2Nums = reduce(xor, (n for n in nums if not n & firstDiffBit))
+        group2Keys = reduce(xor, (i for i in range(uniqNumsCnt) if not i & firstDiffBit))
+
+        return [group1Nums ^ group1Keys, group2Nums ^ group2Keys]
+
+        
