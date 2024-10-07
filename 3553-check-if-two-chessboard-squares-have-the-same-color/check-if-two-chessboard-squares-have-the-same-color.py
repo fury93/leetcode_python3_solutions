10528366b9397ee59e09dc38f1fbef69a3904636
fury93 <www.fanreala@gmail.com>
2024-10-07 10:42:18 +0300

    Time: 36 ms (50.19%) | Memory: 16.4 MB (99.22%) - LeetSync

--- 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.py
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.py
@@ -1,6 +1,8 @@
 class Solution:
     def checkTwoChessboards(self, c1: str, c2: str) -> bool:
-        def colToNumber(ch):
-            return ord(ch) - 96
+        def getColor(pos):
+            col = ord(pos[0]) - 96
+            row = int(pos[1])
+            return (row + col) & 1
         
-        return (colToNumber(c1[0]) + int(c1[1])) & 1 == colToNumber(c2[0]) + int(c2[1]) & 1
+        return getColor(c1) == getColor(c2)
