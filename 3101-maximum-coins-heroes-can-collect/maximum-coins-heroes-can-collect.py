8ca3e1695e43c4deabeb00895b0f4f547544c6b6
fury93 <www.fanreala@gmail.com>
2024-10-09 00:01:16 +0300

    Time: 588 ms (100.00%) | Memory: 40.2 MB (35.95%) - LeetSync

--- 3101-maximum-coins-heroes-can-collect/maximum-coins-heroes-can-collect.py
+++ 3101-maximum-coins-heroes-can-collect/maximum-coins-heroes-can-collect.py
@@ -3,10 +3,3 @@ class Solution:
         monsters, coins = zip(*sorted(zip(monsters, coins), key = lambda x: x[0]))
         coins = list(accumulate(coins, initial = 0))
         return [coins[bisect_right(monsters, power)] for power in heroes]
-        res = [0] * len(heroes)
-        for i, power in enumerate(heroes):
-            pos = bisect_right(monsters, power)
-            if pos > 0:
-                res[i] = coins[pos-1]
-        
-        return res
