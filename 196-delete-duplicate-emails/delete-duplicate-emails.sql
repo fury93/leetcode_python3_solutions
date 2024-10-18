25f6fdb5f9acdb4019721d32d9ffe9bb7d32aed1
fury93 <www.fanreala@gmail.com>
2024-10-19 02:59:38 +0300

    Time: 700 ms (79.93%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,5 +1,13 @@
 # Please write a DELETE statement and DO NOT write a SELECT statement.
 # Write your MySQL query statement below
-delete p1
-from person p1, person p2
-where p1.id > p2.id and p1.email = p2.email
+-- delete p1
+-- from person p1, person p2
+-- where p1.id > p2.id and p1.email = p2.email
+
+DELETE FROM `Person`
+WHERE `id` NOT IN (
+    SELECT * FROM (
+        SELECT MIN(`id`) AS `id` FROM `Person` #store the minimum id's so that one stays
+        GROUP BY `email`
+    ) AS `keep_ids`
+);
