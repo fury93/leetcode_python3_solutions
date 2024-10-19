9e657a57055afa16e5b46a7a2688ce2d79212523
fury93 <www.fanreala@gmail.com>
2024-10-19 03:00:12 +0300

    Time: 1105 ms (32.19%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,17 +1,31 @@
 # Write your MySQL query statement below
-SELECT
-    d.Name AS 'Department', e1.Name AS 'Employee', e1.Salary
-FROM
-    Employee e1
-        JOIN
-    Department d ON e1.DepartmentId = d.Id
-WHERE
-    3 > (SELECT
-            COUNT(DISTINCT e2.Salary)
-        FROM
-            Employee e2
-        WHERE
-            e2.Salary > e1.Salary
-                AND e1.DepartmentId = e2.DepartmentId
-        )
-;
+-- SELECT
+--     d.Name AS 'Department', e1.Name AS 'Employee', e1.Salary
+-- FROM
+--     Employee e1
+--         JOIN
+--     Department d ON e1.DepartmentId = d.Id
+-- WHERE
+--     3 > (SELECT
+--             COUNT(DISTINCT e2.Salary)
+--         FROM
+--             Employee e2
+--         WHERE
+--             e2.Salary > e1.Salary
+--                 AND e1.DepartmentId = e2.DepartmentId
+--         )
+-- ;
+
+select Department, Employee, Salary 
+    from (
+    select 
+    dense_rank() over w as top,
+    e.name as Employee, salary as Salary, departmentId, d.name as Department from Employee e
+    join Department d
+    on e.departmentId = d.id 
+    window w as (
+    partition by departmentId
+    order by salary desc
+    )
+) x
+where top < 4
