e13570b44e7ddf1991405346a93f94ba5c74bdce
fury93 <www.fanreala@gmail.com>
2024-11-11 17:41:33 +0300

    Time: 677 ms (20.94%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,24 +1,29 @@
 # Write your MySQL query statement below
-SELECT
-  X.player_id,
-  X.event_date AS first_login
-FROM
-  (
-    SELECT
-      A.player_id,
-      A.event_date,
-      RANK() OVER (
-        PARTITION BY
-          A.player_id
-        ORDER BY
-          A.event_date
-      ) AS rnk
-    FROM
-      Activity A
-  ) X
-WHERE
-  X.rnk = 1;
-  
+SELECT player_id,
+       Min(event_date) first_login
+FROM   activity
+GROUP  BY player_id  
+
+-- SELECT
+--   X.player_id,
+--   X.event_date AS first_login
+-- FROM
+--   (
+--     SELECT
+--       A.player_id,
+--       A.event_date,
+--       RANK() OVER (
+--         PARTITION BY
+--           A.player_id
+--         ORDER BY
+--           A.event_date
+--       ) AS rnk
+--     FROM
+--       Activity A
+--   ) X
+-- WHERE
+--   X.rnk = 1;
+
 -- select player_id, device_id
 -- from `activity`
 -- group by player_id
