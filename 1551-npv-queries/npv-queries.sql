119da1fc0659c6e2837b9eed9cd18a464a8dfd84
fury93 <www.fanreala@gmail.com>
2024-11-11 20:44:46 +0300

    Time: 1062 ms (69.23%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1551-npv-queries/npv-queries.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+-- SELECT 
+--   Q.id, 
+--   Q.year, 
+--   IFNULL(N.npv, 0) AS npv 
+-- FROM 
+--   Queries Q 
+--   LEFT JOIN NPV N ON Q.id = N.id 
+--   AND Q.year = N.year;
+SELECT 
+  Q.id, 
+  Q.year, 
+  COALESCE(N.npv, 0) AS npv 
+FROM 
+  Queries Q 
+  LEFT JOIN NPV N ON Q.id = N.id 
+  AND Q.year = N.year;
