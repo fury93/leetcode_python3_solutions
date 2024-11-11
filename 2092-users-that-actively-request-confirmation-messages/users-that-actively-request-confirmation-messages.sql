ae83d04932ec09147fab84e41eb4131517c2b5a6
fury93 <www.fanreala@gmail.com>
2024-11-11 22:43:45 +0300

    Time: 701 ms (89.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2092-users-that-actively-request-confirmation-messages/users-that-actively-request-confirmation-messages.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select
+distinct c.user_id
+from confirmations c
+join confirmations d
+on c.user_id=d.user_id and c.time_stamp>d.time_stamp and 
+timestampdiff(second, d.time_stamp,c.time_stamp)<=86400
