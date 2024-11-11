1b8d7f7708dac6cdaa48d82600394c89fc0552c5
fury93 <www.fanreala@gmail.com>
2024-11-11 21:27:54 +0300

    Time: 1859 ms (16.22%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,30 @@
+# Write your MySQL query statement below
+
+# Approach 1: Removing Records Using NOT IN/EXISTS
+-- SELECT 
+--   customer_id, 
+--   COUNT(visit_id) AS count_no_trans 
+-- FROM 
+--   Visits 
+-- WHERE 
+--   visit_id NOT IN (
+--     SELECT 
+--       visit_id 
+--     FROM 
+--       Transactions
+--   ) 
+-- GROUP BY 
+--   customer_id
+
+# Approach 2: Removing Records Using LEFT JOIN and IS NULL
+SELECT 
+  customer_id, 
+  COUNT(*) AS count_no_trans 
+FROM 
+  Visits AS v 
+  LEFT JOIN Transactions AS t ON v.visit_id = t.visit_id 
+WHERE 
+  t.visit_id IS NULL 
+GROUP BY 
+  customer_id
+
