df9eb7d61fe5db626cc1a93e7a0b868500eeebca
fury93 <www.fanreala@gmail.com>
2024-11-11 20:09:57 +0300

    Time: 1893 ms (15.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1495-number-of-trusted-contacts-of-a-customer/number-of-trusted-contacts-of-a-customer.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT 
+  I.invoice_id, 
+  Cust.customer_name, 
+  I.price, 
+  COUNT(DISTINCT C.contact_name) AS contacts_cnt, 
+  COUNT(DISTINCT Nme.customer_name) AS trusted_contacts_cnt 
+FROM 
+  Invoices I 
+  LEFT JOIN Customers Cust ON I.user_id = Cust.customer_id 
+  LEFT JOIN Contacts C ON C.user_id = Cust.customer_id 
+  LEFT JOIN Customers Nme ON Nme.customer_name = C.contact_name 
+GROUP BY 
+  I.invoice_id
