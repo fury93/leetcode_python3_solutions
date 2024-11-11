67144f11812a8aaac67e8f410939cf3dabe98f2b
fury93 <www.fanreala@gmail.com>
2024-11-11 23:47:23 +0300

    Time: 1491 ms (15.94%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2515-calculate-the-influence-of-each-salesperson/calculate-the-influence-of-each-salesperson.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select s.salesperson_id, name, ifnull(sum(price),0) total
+from salesperson s left join customer c
+on s.salesperson_id = c.salesperson_id
+left join sales
+on c.customer_id = sales.customer_Id
+group by salesperson_Id
