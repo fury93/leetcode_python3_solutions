4fdcbf5f779d2cf4d09c821403163f0295f1d77e
fury93 <www.fanreala@gmail.com>
2024-11-11 21:36:50 +0300

    Time: 423 ms (81.42%) | Memory: 0B (100.00%) - LeetSync

--- 1759-find-the-missing-ids/find-the-missing-ids.sql
+++ 1759-find-the-missing-ids/find-the-missing-ids.sql
@@ -1,16 +1,31 @@
 # Write your MySQL query statement below
-WITH RECURSIVE id_seq AS (
-    SELECT 1 as continued_id
-    UNION 
-    SELECT continued_id + 1
-    FROM id_seq
-    WHERE continued_id < (
-        SELECT MAX(customer_id) 
-        FROM Customers
-    ) 
+with recursive all_id as
+(
+    select 
+        1 as ids
+    union all
+    select 
+        ids+1 
+    from all_id 
+        where ids<(select max(customer_id) from Customers)
 )
+select 
+ids
+from all_id
+where ids not in (select customer_id  from Customers)
+order by 1
 
-SELECT continued_id AS ids
-FROM id_seq
-WHERE continued_id NOT IN (SELECT customer_id FROM Customers)  
+-- WITH RECURSIVE id_seq AS (
+--     SELECT 1 as continued_id
+--     UNION 
+--     SELECT continued_id + 1
+--     FROM id_seq
+--     WHERE continued_id < (
+--         SELECT MAX(customer_id) 
+--         FROM Customers
+--     ) 
+-- )
 
+-- SELECT continued_id AS ids
+-- FROM id_seq
+-- WHERE continued_id NOT IN (SELECT customer_id FROM Customers)  
