96cb5199c85ff49a9b897e280353eca77d8fe0b7
fury93 <www.fanreala@gmail.com>
2024-11-11 20:10:18 +0300

    Time: 1252 ms (93.33%) | Memory: 0B (100.00%) - LeetSync

--- 1495-number-of-trusted-contacts-of-a-customer/number-of-trusted-contacts-of-a-customer.sql
+++ 1495-number-of-trusted-contacts-of-a-customer/number-of-trusted-contacts-of-a-customer.sql
@@ -1,14 +1,27 @@
 # Write your MySQL query statement below
-SELECT 
-  I.invoice_id, 
-  Cust.customer_name, 
-  I.price, 
-  COUNT(DISTINCT C.contact_name) AS contacts_cnt, 
-  COUNT(DISTINCT Nme.customer_name) AS trusted_contacts_cnt 
-FROM 
-  Invoices I 
-  LEFT JOIN Customers Cust ON I.user_id = Cust.customer_id 
-  LEFT JOIN Contacts C ON C.user_id = Cust.customer_id 
-  LEFT JOIN Customers Nme ON Nme.customer_name = C.contact_name 
-GROUP BY 
-  I.invoice_id
+WITH CTE AS(
+    SELECT user_id, COUNT(contact_email) AS contacts_cnt,
+            SUM(IF(contact_email IN (SELECT email FROM Customers), 1, 0)) AS trusted_contacts_cnt
+    FROM Contacts
+    GROUP BY user_id)
+
+SELECT invoice_id, cs.customer_name, price, 
+        IFNULL(contacts_cnt, 0) AS contacts_cnt, 
+        IFNULL(trusted_contacts_cnt, 0) AS trusted_contacts_cnt
+FROM Invoices i 
+LEFT JOIN CTE c ON i.user_id = c.user_id
+LEFT JOIN Customers cs ON i.user_id = cs.customer_id
+ORDER BY invoice_id
+-- SELECT 
+--   I.invoice_id, 
+--   Cust.customer_name, 
+--   I.price, 
+--   COUNT(DISTINCT C.contact_name) AS contacts_cnt, 
+--   COUNT(DISTINCT Nme.customer_name) AS trusted_contacts_cnt 
+-- FROM 
+--   Invoices I 
+--   LEFT JOIN Customers Cust ON I.user_id = Cust.customer_id 
+--   LEFT JOIN Contacts C ON C.user_id = Cust.customer_id 
+--   LEFT JOIN Customers Nme ON Nme.customer_name = C.contact_name 
+-- GROUP BY 
+--   I.invoice_id
