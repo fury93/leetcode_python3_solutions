9e51221ed99330d172cead1322680749021b6524
fury93 <www.fanreala@gmail.com>
2024-11-11 19:21:19 +0300

    Time: 465 ms (85.88%) | Memory: 0B (100.00%) - LeetSync

--- 1246-user-activity-for-the-past-30-days-ii/user-activity-for-the-past-30-days-ii.sql
+++ 1246-user-activity-for-the-past-30-days-ii/user-activity-for-the-past-30-days-ii.sql
@@ -1,5 +1,12 @@
 # Write your MySQL query statement below
-SELECT ifnull(ROUND(COUNT(DISTINCT session_id)/COUNT(DISTINCT user_id), 2),0.00) 
-AS average_sessions_per_user
-FROM Activity 
-WHERE activity_date >= '2019-06-28' and activity_date <= '2019-07-27';  
+-- SELECT ifnull(ROUND(COUNT(DISTINCT session_id)/COUNT(DISTINCT user_id), 2),0.00) 
+-- AS average_sessions_per_user
+-- FROM Activity 
+-- WHERE activity_date >= '2019-06-28' and activity_date <= '2019-07-27';  
+
+select ifnull(round(avg(cnt),2),0) as average_sessions_per_user
+from( 
+    select user_id, count(distinct session_id) as cnt 
+    from activity 
+    where activity_date between date_sub('2019-07-27', interval 29 day) and '2019-07-27'
+    group by user_id) count_session
