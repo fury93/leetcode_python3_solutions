d2131a74ffe00c26743a1cd1a3752207a93bd316
fury93 <www.fanreala@gmail.com>
2024-11-12 00:17:38 +0300

    Time: 309 ms (69.09%) | Memory: 0B (100.00%) - LeetSync

--- 3256-find-candidates-for-data-scientist-position/find-candidates-for-data-scientist-position.sql
+++ 3256-find-candidates-for-data-scientist-position/find-candidates-for-data-scientist-position.sql
@@ -1,6 +1,14 @@
 # Write your MySQL query statement below
-SELECT candidate_id
-FROM Candidates
-GROUP BY candidate_id
-HAVING SUM(IF(skill = 'Python', 1, 0)) + SUM(IF(skill = 'Tableau', 1, 0)) + SUM(IF(skill = 'PostgreSQL', 1, 0)) = 3
-ORDER BY candidate_id ASC
+-- SELECT candidate_id
+-- FROM Candidates
+-- GROUP BY candidate_id
+-- HAVING SUM(IF(skill = 'Python', 1, 0)) + SUM(IF(skill = 'Tableau', 1, 0)) + SUM(IF(skill = 'PostgreSQL', 1, 0)) = 3
+-- ORDER BY candidate_id ASC
+
+# Write your MySQL query statement below
+select candidate_id
+from candidates
+where skill IN ('Python','Tableau','PostgreSQL')
+group by 1
+having count(skill)=3
+order by 1 asc
