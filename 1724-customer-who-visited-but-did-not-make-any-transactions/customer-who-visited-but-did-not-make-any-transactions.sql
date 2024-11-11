c7595c8519c6d5c4849245ad0971843b1df58cf0
fury93 <www.fanreala@gmail.com>
2024-11-11 21:28:31 +0300

    Time: 1342 ms (82.43%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
-
+ SELECT v.customer_id, COUNT(DISTINCT v.visit_id) as count_no_trans 
+ FROM Visits v LEFT JOIN Transactions t ON v.visit_id = t.visit_id
+ WHERE t.transaction_id IS NULL
+ GROUP BY v.customer_id ;
+ 
 # Approach 1: Removing Records Using NOT IN/EXISTS
 -- SELECT 
 --   customer_id, 
@@ -17,14 +21,14 @@
 --   customer_id
 
 # Approach 2: Removing Records Using LEFT JOIN and IS NULL
-SELECT 
-  customer_id, 
-  COUNT(*) AS count_no_trans 
-FROM 
-  Visits AS v 
-  LEFT JOIN Transactions AS t ON v.visit_id = t.visit_id 
-WHERE 
-  t.visit_id IS NULL 
-GROUP BY 
-  customer_id
+-- SELECT 
+--   customer_id, 
+--   COUNT(*) AS count_no_trans 
+-- FROM 
+--   Visits AS v 
+--   LEFT JOIN Transactions AS t ON v.visit_id = t.visit_id 
+-- WHERE 
+--   t.visit_id IS NULL 
+-- GROUP BY 
+--   customer_id
 
