2d732ccbd06e10c34d78cfb78040412e1d2bf690
fury93 <www.fanreala@gmail.com>
2024-11-11 22:35:51 +0300

    Time: 547 ms (89.77%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2030-group-employees-of-the-same-salary/group-employees-of-the-same-salary.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT *,
+    DENSE_RANK() OVER(ORDER BY salary) team_id
+FROM Employees
+WHERE salary NOT IN(
+			SELECT salary
+			FROM Employees
+			GROUP BY salary
+			HAVING COUNT(*) = 1)
+ORDER BY team_id, employee_id
