3be2a4feba6e5f9de8c065802caecba16c6435f9
fury93 <www.fanreala@gmail.com>
2024-11-12 00:03:19 +0300

    Time: 381 ms (46.74%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3155-highest-salaries-difference/highest-salaries-difference.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT ABS(
+(SELECT MAX(salary) as salary
+    FROM Salaries 
+    WHERE department = "Engineering"
+) 
+- (SELECT MAX(salary) as salary
+ FROM Salaries 
+ WHERE department = "Marketing")
+ ) AS salary_difference 
