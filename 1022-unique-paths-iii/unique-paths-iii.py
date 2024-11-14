fa383a1d9a89703e05ee37c3b575d2e0a2cf167c
fury93 <www.fanreala@gmail.com>
2024-11-15 01:00:38 +0300

    Time: 7 ms (96.91%) | Memory: 16.8 MB (9.91%) - LeetSync

---
+++ 1022-unique-paths-iii/unique-paths-iii.py
@@ -0,0 +1,79 @@
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        self.res = 0
+        m, n, empty = len(grid), len(grid[0]), 1
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    x, y = (i, j)
+                elif grid[i][j] == 0:
+                    empty += 1
+
+        def dfs(x, y, empty):
+            if not (0 <= x < m and 0 <= y < n and grid[x][y] >= 0): return
+            if grid[x][y] == 2:
+                self.res += empty == 0
+                return
+            grid[x][y] = -2
+            dfs(x + 1, y, empty - 1)
+            dfs(x - 1, y, empty - 1)
+            dfs(x, y + 1, empty - 1)
+            dfs(x, y - 1, empty - 1)
+            grid[x][y] = 0
+        dfs(x, y, empty)
+        return self.res
+
+class Solution2:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+
+        # step 1). initialize the conditions for backtracking
+        #   i.e. initial state and final state
+        non_obstacles = 0
+        start_row, start_col = 0, 0
+        for row in range(0, rows):
+            for col in range(0, cols):
+                cell = grid[row][col] 
+                if  cell >= 0:
+                    non_obstacles += 1
+                if cell == 1:
+                    start_row, start_col = row, col
+
+        # count of paths as the final result
+        path_count = 0
+
+        # step 2). backtrack on the grid
+        def backtrack(row, col, remain):
+            # we need to modify this external variable
+            nonlocal path_count
+
+            # base case for the termination of backtracking
+            if grid[row][col] == 2 and remain == 1:
+                # reach the destination
+                path_count += 1
+                return
+
+            # mark the square as visited. case: 0, 1, 2 
+            temp = grid[row][col] 
+            grid[row][col] = -4
+            remain -= 1   # we now have one less square to visit
+
+            # explore the 4 potential directions around
+            for ro, co in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                next_row, next_col = row + ro, col + co
+
+                if not (0 <= next_row < rows and 0 <= next_col < cols):
+                    # invalid coordinate
+                    continue
+                if grid[next_row][next_col] < 0:
+                    # either obstacle or visited square
+                    continue
+
+                backtrack(next_row, next_col, remain)
+
+            # unmark the square after the visit
+            grid[row][col] = temp
+
+        backtrack(start_row, start_col, non_obstacles)
+
+        return path_count
