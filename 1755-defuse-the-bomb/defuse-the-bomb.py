b70ba12f2a9ec7fe7c44e88ecb31131ffd23eaef
fury93 <www.fanreala@gmail.com>
2024-11-18 03:25:12 +0300

    Time: 0 ms (100.00%) | Memory: 16.5 MB (91.75%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.py
@@ -0,0 +1,5 @@
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        if k < 0: return self.decrypt(code[::-1], -k)[::-1]
+        p = list(accumulate(code + code))
+        return [p[i+k] - p[i] for i in range(len(code))]
