f0e18d4ed55fed3fbd07d8b6fe0c386238734472
fury93 <www.fanreala@gmail.com>
2024-11-19 22:55:34 +0300

    Time: 145 ms (95.48%) | Memory: 17.2 MB (56.90%) - LeetSync

--- 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
+++ 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
@@ -22,3 +22,30 @@ class Solution:
                 l += 1
         
         return r - l + 1
+
+class Solution2:
+    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+        n = len(s)
+        if n < 3:
+            return n
+
+        left, right = 0, 0
+        hashmap = defaultdict()
+        max_len = 2
+
+        while right < n:
+            # when the slidewindow contains less than 3 characters
+            hashmap[s[right]] = right
+            right += 1
+
+            # slidewindow contains 3 characters
+            if len(hashmap) == 3:
+                # delete the leftmost character
+                del_idx = min(hashmap.values())
+                del hashmap[s[del_idx]]
+                # move left pointer of the slidewindow
+                left = del_idx + 1
+
+            max_len = max(max_len, right - left)
+
+        return max_len
