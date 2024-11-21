07084458a6868ead1fc774f776e7fc80de30b950
fury93 <www.fanreala@gmail.com>
2024-11-22 01:06:17 +0300

    Time: 19 ms (67.25%) | Memory: 26.4 MB (89.32%) - LeetSync

--- 247-strobogrammatic-number-ii/strobogrammatic-number-ii.py
+++ 247-strobogrammatic-number-ii/strobogrammatic-number-ii.py
@@ -50,3 +50,25 @@ class Solution2:
             q = next_level
             
         return q
+
+class Solution3:
+    def findStrobogrammatic(self, n: int) -> List[str]:
+        def helper(m, n):
+            if m == 0:
+                return [""]
+            if m == 1:
+                return ["0", "1", "8"]
+            
+            prev = helper(m-2, n)
+            result = []
+
+            for num in prev:
+                if m != n:
+                    result.append("0" + num + "0")
+                result.append("1" + num + "1")
+                result.append("8" + num + "8")
+                result.append("6" + num + "9")
+                result.append("9" + num + "6")
+            
+            return result
+        return helper(n, n)
