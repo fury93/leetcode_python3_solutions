d60e4a1842e8379cd9e60245492cbb5844d908e8
fury93 <www.fanreala@gmail.com>
2024-11-22 00:36:33 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (67.51%) - LeetSync

--- 156-binary-tree-upside-down/binary-tree-upside-down.py
+++ 156-binary-tree-upside-down/binary-tree-upside-down.py
@@ -6,13 +6,23 @@
 #         self.right = right
 class Solution:
     def upsideDownBinaryTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root and root.left :
+        if root and root.left:
             res = self.upsideDownBinaryTree(root.left)
-            root.left.right=root
-            if root.right :
-                root.left.left=root.right
-            root.left =None
-            root.right=None
+            root.left.right = root
+            if root.right:
+                root.left.left = root.right
+            root.left = None
+            root.right = None
             return res
         else:
             return root
+
+    def upsideDownBinaryTree2(self, root):
+        if not root or not root.left:
+            return root
+        lRoot = self.upsideDownBinaryTree(root.left)
+        rMost = lRoot
+        while rMost.right:
+            rMost = rMost.right
+        root, rMost.left, rMost.right = lRoot, root.right, TreeNode(root.val)
+        return root
