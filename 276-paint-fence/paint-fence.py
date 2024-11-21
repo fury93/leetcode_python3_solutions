8247b530f70c2313f5086303d2490ea9896c5d0d
fury93 <www.fanreala@gmail.com>
2024-11-22 02:19:35 +0300

    Time: 0 ms (100.00%) | Memory: 16.5 MB (70.49%) - LeetSync

---
+++ 276-paint-fence/paint-fence.py
@@ -0,0 +1,33 @@
+class Solution:
+    def numWays(self, n: int, k: int) -> int:
+        if n == 1:
+            return k
+        
+        two_posts_back = k
+        one_post_back = k * k
+        
+        for i in range(3, n + 1):
+            curr = (k - 1) * (one_post_back + two_posts_back)
+            two_posts_back = one_post_back
+            one_post_back = curr
+
+        return one_post_back
+
+class Solution2:
+    def numWays(self, n: int, k: int) -> int:
+        def total_ways(i):
+            if i == 1:
+                return k
+            if i == 2:
+                return k * k
+            
+            # Check if we have already calculated totalWays(i)
+            if i in memo:
+                return memo[i]
+            
+            # Use the recurrence relation to calculate total_ways(i)
+            memo[i] = (k - 1) * (total_ways(i - 1) + total_ways(i - 2))
+            return memo[i]
+
+        memo = {}
+        return total_ways(n)
