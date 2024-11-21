4bdee77a2d1772c2e9a3af59d7ce90b3655bb59e
fury93 <www.fanreala@gmail.com>
2024-11-22 02:44:54 +0300

    Time: 531 ms (10.81%) | Memory: 25.6 MB (7.25%) - LeetSync

--- 308-range-sum-query-2d-mutable/range-sum-query-2d-mutable.py
+++ 308-range-sum-query-2d-mutable/range-sum-query-2d-mutable.py
@@ -1,4 +1,45 @@
-class NumMatrix(object):
+class BIT:
+    def __init__(self,  nums: List[int]) -> None:
+        self.nums = nums
+        self.n = len(nums) + 1
+        self.tree = [0] + nums
+        for i in range(1, self.n):
+            p = i + (i & -i)
+            if p < self.n:
+                self.tree[p] += self.tree[i]
+    
+    def query(self, i: int) -> int:
+        i += 1
+        total = 0
+        while i > 0:
+            total += self.tree[i]
+            i -= i & -i
+        return total
+
+    def update(self, i: int, v: int) -> None:
+        d = v - self.nums[i]
+        self.nums[i] = v
+        i += 1
+        while i < self.n:
+            self.tree[i] += d
+            i += i & -i
+        
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        self.m, self.n = len(matrix), len(matrix[0])
+        self.bits = [BIT(row) for row in matrix]
+
+    def update(self, row: int, col: int, val: int) -> None:
+        self.bits[row].update(col, val)
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        total = 0
+        for r in range(row1, row2 + 1):
+            total += self.bits[r].query(col2) - self.bits[r].query(col1 - 1)
+        return total
+        
+class NumMatrix3(object):
     def __init__(self, matrix):
         if not matrix:
             return
