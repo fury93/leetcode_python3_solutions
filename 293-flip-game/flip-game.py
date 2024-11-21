2f5461f621c0bff921f0d9815033d5b3bc73264e
fury93 <www.fanreala@gmail.com>
2024-11-22 02:31:04 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (90.35%) - LeetSync

---
+++ 293-flip-game/flip-game.py
@@ -0,0 +1,7 @@
+class Solution:
+    def generatePossibleNextMoves(self, states: str) -> List[str]:
+        res = []
+        for i in range(1, len(states)):
+            if states[i] == states[i-1] == '+':
+                res.append(states[:i-1] + '--' + states[i+1:])
+        return res
