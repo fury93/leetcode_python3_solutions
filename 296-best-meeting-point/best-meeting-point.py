1a01d9f46c771f736479a432c1fd6021792ee8af
fury93 <www.fanreala@gmail.com>
2024-11-22 02:34:35 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (92.96%) - LeetSync

---
+++ 296-best-meeting-point/best-meeting-point.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minTotalDistance(self, grid):
+        row_sum = list(map(sum, grid))
+        col_sum = list(map(sum, zip(*grid)))
+
+        def minTotalDistance1D(vec):
+            i, j = -1, len(vec)
+            d = left = right = 0
+            while i != j:
+                if left < right:
+                    d += left
+                    i += 1
+                    left += vec[i]
+                else:
+                    d += right
+                    j -= 1
+                    right += vec[j]
+            return d
+
+        return minTotalDistance1D(row_sum) + minTotalDistance1D(col_sum)
+        
