73027a3397fb3ac2eca683c4eb4f92d5cc6196e3
fury93 <www.fanreala@gmail.com>
2024-11-22 00:46:28 +0300

    Time: 28 ms (90.52%) | Memory: 16.5 MB (78.99%) - LeetSync

--- 158-read-n-characters-given-read4-ii-call-multiple-times/read-n-characters-given-read4-ii-call-multiple-times.py
+++ 158-read-n-characters-given-read4-ii-call-multiple-times/read-n-characters-given-read4-ii-call-multiple-times.py
@@ -16,3 +16,26 @@ class Solution:
             self.i4 += 1
             i += 1
         return i
+
+class Solution2:
+    def __init__(self):
+        self.buf4 = [''] * 4
+        self.buf_index = 0
+        self.buf_size = 0
+
+
+    def read(self, buf: List[str], n: int) -> int:
+        index = 0
+        while index < n:
+            if self.buf_index == self.buf_size: # read new buf4
+                self.buf_size = read4(self.buf4)
+                self.buf_index = 0
+                if self.buf_size == 0: # end of file
+                    break
+            
+            while index < n and self.buf_index < self.buf_size:
+                buf[index] = self.buf4[self.buf_index]
+                self.buf_index += 1
+                index += 1
+            
+        return index
