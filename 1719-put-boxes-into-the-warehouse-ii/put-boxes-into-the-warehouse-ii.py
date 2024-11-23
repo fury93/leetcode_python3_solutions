b5b5ad56322effd6cd8b14f1f1a24d53e3af7896
fury93 <www.fanreala@gmail.com>
2024-11-24 02:26:13 +0300

    Time: 105 ms (40.91%) | Memory: 35.9 MB (19.51%) - LeetSync

--- 1719-put-boxes-into-the-warehouse-ii/put-boxes-into-the-warehouse-ii.py
+++ 1719-put-boxes-into-the-warehouse-ii/put-boxes-into-the-warehouse-ii.py
@@ -38,3 +38,35 @@ class Solution:
                 count += 1
                 boxIndex += 1
         return count
+
+class Solution2:
+    def maxBoxesInWarehouse(
+        self, boxes: List[int], warehouse: List[int]
+    ) -> int:
+        # Sort the boxes by height
+
+        boxes.sort()
+
+        warehouse_size = len(warehouse)
+        left_index = 0
+        right_index = warehouse_size - 1
+        box_count = 0
+        box_index = len(boxes) - 1
+
+        # Iterate through the boxes from largest to smallest
+
+        while left_index <= right_index and box_index >= 0:
+            # Check if the current box can fit in the
+            # leftmost available room
+
+            if boxes[box_index] <= warehouse[left_index]:
+                box_count += 1
+                left_index += 1
+            # Check if the current box can fit in the
+            # rightmost available room
+
+            elif boxes[box_index] <= warehouse[right_index]:
+                box_count += 1
+                right_index -= 1
+            box_index -= 1
+        return box_count
