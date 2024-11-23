db488674d447835aecd94cea59a0eda0157de90d
fury93 <www.fanreala@gmail.com>
2024-11-24 01:33:48 +0300

    Time: 0 ms (100.00%) | Memory: 16.5 MB (99.56%) - LeetSync

---
+++ 1164-missing-number-in-arithmetic-progression/missing-number-in-arithmetic-progression.py
@@ -0,0 +1,9 @@
+class Solution:
+    def missingNumber(self, arr: List[int]) -> int:
+        minVal, maxVal, L = min(arr), max(arr), len(arr)
+        if minVal == maxVal: return minVal
+        step = (maxVal - minVal) // L
+        arrSet = set(arr)
+        for n in range(minVal, maxVal+1, step):
+            if n not in arrSet:
+                return n
