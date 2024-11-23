84face6da58bdbad49a6af006329fc128668b32e
fury93 <www.fanreala@gmail.com>
2024-11-24 00:33:56 +0300

    Time: 1 ms (49.35%) | Memory: 16.9 MB (46.42%) - LeetSync

---
+++ 1074-high-five/high-five.py
@@ -0,0 +1,11 @@
+class Solution:
+    def highFive(self, items: List[List[int]]) -> List[List[int]]:
+        students = [None] * 1001
+        for idx, score in items:
+            if not students[idx]: students[idx] = []
+            if len(students[idx]) < 5:
+                heappush(students[idx], score)
+            else:
+                heappushpop(students[idx], score)
+               
+        return [[key, sum(val)//5] for key, val in enumerate(students) if val]
