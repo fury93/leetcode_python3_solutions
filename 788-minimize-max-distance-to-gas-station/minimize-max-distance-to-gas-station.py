7005b0aabffaea64eb5dc828b38da04c5eccfa36
fury93 <www.fanreala@gmail.com>
2024-11-23 23:49:55 +0300

    Time: 31 ms (100.00%) | Memory: 17.3 MB (5.59%) - LeetSync

--- 788-minimize-max-distance-to-gas-station/minimize-max-distance-to-gas-station.py
+++ 788-minimize-max-distance-to-gas-station/minimize-max-distance-to-gas-station.py
@@ -1,4 +1,24 @@
-class Solution(object):
+class Solution:
+    def minmaxGasDist(self, stations: List[int], k: int) -> float:
+        # priority queue
+        # the lower bound of the solution is the entire length divided by (K+1)
+        bound = (stations[-1] - stations[0]) / (k + 1)
+        added = 0  # keeps track of the stations already built
+        max_heap = []
+        for i in range(1, len(stations)):
+            needed = math.ceil((stations[i] - stations[i-1]) / bound) - 1
+            heapq.heappush(max_heap, (-(stations[i] - stations[i-1])/(needed+1), i, needed))
+            added += needed
+        
+        while added < k:
+            cur, i, needed = heapq.heappop(max_heap)
+            needed += 1
+            heapq.heappush(max_heap, (-(stations[i] - stations[i-1])/(needed+1), i, needed))
+            added += 1
+        
+        return -max_heap[0][0]
+        
+class Solution2:
     def minmaxGasDist(self, stations, K):
         def possible(D):
             return sum(int((stations[i+1] - stations[i]) / D)
