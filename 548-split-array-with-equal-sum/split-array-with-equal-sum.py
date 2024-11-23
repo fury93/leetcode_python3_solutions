86226cc75a8a44f4857ea26866b279980c7cbb43
fury93 <www.fanreala@gmail.com>
2024-11-23 20:31:16 +0300

    Time: 859 ms (81.82%) | Memory: 16.8 MB (53.85%) - LeetSync

--- 548-split-array-with-equal-sum/split-array-with-equal-sum.py
+++ 548-split-array-with-equal-sum/split-array-with-equal-sum.py
@@ -1,4 +1,20 @@
-class Solution(object):
+class Solution:
+    def splitArray(self, nums: List[int]) -> bool:
+        n = len(nums)
+        s = [0] * (n + 1)
+        for i, v in enumerate(nums):
+            s[i + 1] = s[i] + v
+        for j in range(3, n - 3):
+            seen = set()
+            for i in range(1, j - 1):
+                if s[i] == s[j] - s[i + 1]:
+                    seen.add(s[i])
+            for k in range(j + 2, n - 1):
+                if s[n] - s[k + 1] == s[k] - s[j + 1] and s[n] - s[k + 1] in seen:
+                    return True
+        return False
+        
+class Solution3(object):
     def splitArray(self, nums):
         """
         :type nums: List[int]
@@ -11,7 +27,7 @@ class Solution(object):
             
         return len(nums) > 6 and any(split(nums[:j]) & split(nums[j+1:]) \
                              for j in range(3, len(nums)-3))
-                             
+
 class Solution2:
     def splitArray(self, A):
         P = [0]
