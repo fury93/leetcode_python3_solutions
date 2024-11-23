5861f049e7a2ecaf51423431d047a0f672b2b726
fury93 <www.fanreala@gmail.com>
2024-11-24 01:08:07 +0300

    Time: 0 ms (100.00%) | Memory: 16.5 MB (88.14%) - LeetSync

--- 1131-count-substrings-with-only-one-distinct-letter/count-substrings-with-only-one-distinct-letter.py
+++ 1131-count-substrings-with-only-one-distinct-letter/count-substrings-with-only-one-distinct-letter.py
@@ -1,5 +1,15 @@
 class Solution:
     def countLetters(self, s: str) -> int:
+        l = 0
+        count = 0
+        for r, c in enumerate(s):
+            while s[l] != c:
+                l += 1
+            count += r - l + 1
+        return count
+        
+class Solution2:
+    def countLetters(self, s: str) -> int:
         sm = 0
         for _, g in groupby(s):
             ln = len(list(g))
