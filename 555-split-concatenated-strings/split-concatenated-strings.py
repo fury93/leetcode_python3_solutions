f1404f5fdf18feef7451f6c69cb697e762ad6b4c
fury93 <www.fanreala@gmail.com>
2024-11-23 20:33:49 +0300

    Time: 27 ms (85.71%) | Memory: 16.8 MB (34.78%) - LeetSync

--- 555-split-concatenated-strings/split-concatenated-strings.py
+++ 555-split-concatenated-strings/split-concatenated-strings.py
@@ -1,5 +1,18 @@
 class Solution:
     def splitLoopedString(self, strs: List[str]) -> str:
+        strs = [s[::-1] if s[::-1] > s else s for s in strs]
+        ans = ''.join(strs)
+        for i, s in enumerate(strs):
+            t = ''.join(strs[i + 1 :]) + ''.join(strs[:i])
+            for j in range(len(s)):
+                a = s[j:]
+                b = s[:j]
+                ans = max(ans, a + t + b)
+                ans = max(ans, b[::-1] + t + a[::-1])
+        return ans
+        
+class Solution2:
+    def splitLoopedString(self, strs: List[str]) -> str:
         arr=[s if s>=s[::-1] else s[::-1] for s in strs]
         res=''
         for i,s in enumerate(arr):
