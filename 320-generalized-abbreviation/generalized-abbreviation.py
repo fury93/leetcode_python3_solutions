c1c87745fbb78d20040d3ad1b98d3af7e56f328e
fury93 <www.fanreala@gmail.com>
2024-11-23 17:04:07 +0300

    Time: 15 ms (98.80%) | Memory: 19.5 MB (91.78%) - LeetSync

--- 320-generalized-abbreviation/generalized-abbreviation.py
+++ 320-generalized-abbreviation/generalized-abbreviation.py
@@ -29,3 +29,30 @@ class Solution:
         abbreviations = []
         self.store_abbreviations(abbreviations, word, "", 0, 0)
         return abbreviations
+
+class Solution2:
+    def generateAbbreviations(self, word):
+        N = len(word)
+        abbreviations = []
+
+        for mask in range(1 << N):
+            curr_word = []
+            abbreviated_count = 0
+
+            for index in range(N):
+                # If the bit at the position index is 1, increment the abbreviated substring.
+                if mask & (1 << index):
+                    abbreviated_count += 1
+                else:
+                    # Append the last substring and then the current character.
+                    if abbreviated_count > 0:
+                        curr_word.append(str(abbreviated_count))
+                        abbreviated_count = 0
+                    curr_word.append(word[index])
+
+            if abbreviated_count > 0:
+                curr_word.append(str(abbreviated_count))
+
+            abbreviations.append("".join(curr_word))
+
+        return abbreviations
