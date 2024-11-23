8ff40282c094f12a0cdc50db08dbf9ca81b18405
fury93 <www.fanreala@gmail.com>
2024-11-23 20:41:53 +0300

    Time: 11 ms (46.99%) | Memory: 17.5 MB (71.94%) - LeetSync

---
+++ 573-squirrel-simulation/squirrel-simulation.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minDistance(self, height, width, tree, squirrel, nuts):
+        def taxi(p, q):
+            return abs(p[0] - q[0]) + abs(p[1] - q[1])
+        
+        S = sum(2 * taxi(tree, nut) for nut in nuts)
+        return min(S + taxi(squirrel, nut) - taxi(nut, tree) for nut in nuts)
