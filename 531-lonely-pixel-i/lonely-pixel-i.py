b1e6ad358e20cb27f23aa3227254a29aa9b02fe6
fury93 <www.fanreala@gmail.com>
2024-11-23 20:20:24 +0300

    Time: 11 ms (89.72%) | Memory: 21.9 MB (33.00%) - LeetSync

---
+++ 531-lonely-pixel-i/lonely-pixel-i.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findLonelyPixel(self, picture: List[List[str]]) -> int:
+        n,m=len(picture),len(picture[0])
+        res=0
+        for j in range(m):
+            found=False
+            for i in range(n):
+                if picture[i][j]=='B':
+                    if found or picture[i].count('B')!=1:
+                        found=False
+                        break
+                    found=True
+            if found:
+                res+=1
+        return res
