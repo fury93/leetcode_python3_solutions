c67d32046ad9da94cbea6efaa48357e2425742e4
fury93 <www.fanreala@gmail.com>
2024-11-24 00:27:49 +0300

    Time: 29 ms (100.00%) | Memory: 20.3 MB (90.91%) - LeetSync

--- 1061-number-of-valid-subarrays/number-of-valid-subarrays.py
+++ 1061-number-of-valid-subarrays/number-of-valid-subarrays.py
@@ -1,5 +1,19 @@
 class Solution:
     def validSubarrays(self, nums: List[int]) -> int:
+        ans = 0
+        stack = []
+
+        for num in nums:
+            while stack and stack[-1] > num:
+                stack.pop()
+            stack.append(num)
+
+            ans += len(stack)
+
+        return ans
+        
+class Solution2:
+    def validSubarrays(self, nums: List[int]) -> int:
         stack = []
         nextSmaller = [len(nums)] * len(nums)
         for i, v in enumerate(nums):
