cb6ac6486090877aedb99f645d8d4a355082fa17
fury93 <www.fanreala@gmail.com>
2024-11-23 23:44:54 +0300

    Time: 3 ms (100.00%) | Memory: 16.7 MB (44.09%) - LeetSync

--- 760-bold-words-in-string/bold-words-in-string.py
+++ 760-bold-words-in-string/bold-words-in-string.py
@@ -1,4 +1,30 @@
 class Solution:
+    def boldWords(self, words: List[str], s: str) -> str:
+        n = len(s)
+        bold = [False for _ in range(n)] 
+    
+        for word in words:
+            start = s.find(word)
+            while start != -1:
+                end = start + len(word)
+                for i in range(start, end):
+                    bold[i] = True
+                
+                start = s.find(word, start + 1)
+
+        ans = []
+        for idx, c in enumerate(s):
+            if bold[idx] and (idx == 0 or not bold[idx - 1]):
+                ans.append("<b>")
+
+            ans.append(c)
+
+            if bold[idx] and (idx == n - 1 or not bold[idx + 1]):
+                ans.append("</b>")
+
+        return "".join(ans)
+        
+class Solution2:
     def boldWords(self, words, S):
         n=len(S)
         b=[False]*n
