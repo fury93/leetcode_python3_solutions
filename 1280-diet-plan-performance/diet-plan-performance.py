7ee4c69a594c48545c8ca4d24b26c2efa9f37d21
fury93 <www.fanreala@gmail.com>
2024-11-24 01:07:20 +0300

    Time: 7 ms (79.35%) | Memory: 24 MB (92.31%) - LeetSync

--- 1280-diet-plan-performance/diet-plan-performance.py
+++ 1280-diet-plan-performance/diet-plan-performance.py
@@ -1,5 +1,25 @@
 class Solution:
     def dietPlanPerformance(self, calories: List[int], k: int, lower: int, upper: int) -> int:
+        score = 0
+        total = sum(calories[:k])
+        if total < lower:
+            score -= 1
+        if total > upper:
+            score += 1
+        
+        for right in range(k, len(calories)):
+            total = total + calories[right] - calories[right - k]
+            if total < lower:
+                score -= 1
+            if total > upper:
+                score += 1
+        
+        return score
+        
+            
+        
+class Solution2:
+    def dietPlanPerformance(self, calories: List[int], k: int, lower: int, upper: int) -> int:
         res, sm, k = 0, 0, k - 1
         for r, cal in enumerate(calories):
             sm += cal
