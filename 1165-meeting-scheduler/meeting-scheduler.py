9e9398d189fe9710c4aee73ed4f14c378857fe8a
fury93 <www.fanreala@gmail.com>
2024-11-24 01:35:08 +0300

    Time: 77 ms (21.46%) | Memory: 22.8 MB (45.05%) - LeetSync

--- 1165-meeting-scheduler/meeting-scheduler.py
+++ 1165-meeting-scheduler/meeting-scheduler.py
@@ -14,3 +14,16 @@ class Solution:
                 j += 1
 
         return []
+
+class Solution2:
+    def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
+        # build up a heap containing time slots last longer than duration
+        timeslots = list(filter(lambda x: x[1] - x[0] >= duration, slots1 + slots2))
+        heapq.heapify(timeslots)
+
+        while len(timeslots) > 1:
+            start1, end1 = heapq.heappop(timeslots)
+            start2, end2 = timeslots[0]
+            if end1 >= start2 + duration:
+                return [start2, start2 + duration]
+        return []
