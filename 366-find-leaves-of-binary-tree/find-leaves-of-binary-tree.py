e7e710f30a8d2fdba5f120e91c181d092ad1d7cd
fury93 <www.fanreala@gmail.com>
2024-11-23 17:56:07 +0300

    Time: 0 ms (100.00%) | Memory: 16.7 MB (44.77%) - LeetSync

---
+++ 366-find-leaves-of-binary-tree/find-leaves-of-binary-tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = defaultdict(list)
+
+        def dfs(node):
+            if not node: return 0
+            left = dfs(node.left)
+            right = dfs(node.right)
+            curLevel = max(left, right) + 1
+            res[curLevel].append(node.val)
+            return curLevel
+        
+        dfs(root)
+
+        return list(res.values())
+        
