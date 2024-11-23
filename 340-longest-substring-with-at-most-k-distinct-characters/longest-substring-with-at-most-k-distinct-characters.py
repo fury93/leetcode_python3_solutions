07eee0967394e4db59df36b363ea8bc9ff54c9eb
fury93 <www.fanreala@gmail.com>
2024-11-23 17:14:06 +0300

    Time: 30 ms (63.62%) | Memory: 16.9 MB (26.30%) - LeetSync

--- 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.py
+++ 340-longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.py
@@ -11,3 +11,73 @@ class Solution:
                 l += 1
         
         return r - l + 1
+
+# Approach 1: Binary Search + Fixed Size Sliding Window
+class Solution1:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        n = len(s)
+        if k >= n:
+            return n
+        left, right = k, n
+        
+        def isValid(size):
+            counter = collections.Counter(s[:size])
+            if len(counter) <= k:
+                return True
+            for i in range(size, n):
+                counter[s[i]] += 1
+                counter[s[i - size]] -= 1
+                if counter[s[i - size]] == 0:
+                    del counter[s[i - size]]
+                if len(counter) <= k:
+                    return True
+            return False
+        
+        while left < right:
+            mid = (left + right + 1) // 2
+            
+            if isValid(mid):
+                left = mid
+            else:
+                right = mid - 1
+        
+        return left
+
+# Approach 2: Sliding Window
+class Solution2:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        n = len(s)
+        max_size = 0
+        counter = collections.Counter()
+        
+        left = 0
+        for right in range(n):
+            counter[s[right]] += 1
+            
+            while len(counter) > k: 
+                counter[s[left]] -= 1
+                if counter[s[left]] == 0:
+                    del counter[s[left]]
+                left += 1
+            
+            max_size = max(max_size, right - left + 1)
+                    
+        return max_size
+
+# Approach 3: Sliding Window II
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        max_size = 0
+        counter = collections.Counter()
+        
+        for right in range(len(s)):
+            counter[s[right]] += 1
+            
+            if len(counter) <= k:
+                max_size += 1
+            else:
+                counter[s[right - max_size]] -= 1
+                if counter[s[right - max_size]] == 0:
+                    del counter[s[right - max_size]]
+
+        return max_size
