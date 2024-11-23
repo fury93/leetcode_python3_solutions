6054556a2cd6eb9caa81677de393e6bfd66451ac
fury93 <www.fanreala@gmail.com>
2024-11-24 01:24:56 +0300

    Time: 3 ms (90.11%) | Memory: 38.9 MB (55.99%) - LeetSync

--- 1143-find-smallest-common-element-in-all-rows/find-smallest-common-element-in-all-rows.py
+++ 1143-find-smallest-common-element-in-all-rows/find-smallest-common-element-in-all-rows.py
@@ -1,4 +1,29 @@
 class Solution:
+    def smallestCommonElement(self, mat: List[List[int]]) -> int:
+        for i in mat[0]:
+            target = i
+            for j in range(1, len(mat)):
+                if not self.search(target, mat[j]):
+                    break
+                if j == len(mat) - 1:
+                    return target
+        return -1
+    
+    def search(self, target, nums):
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            mid = left + (right-left)//2
+            midV = nums[mid]
+            if midV == target:
+                return True
+            elif midV > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return False
+        
+class Solution2:
     def smallestCommonElement(self, M):
         c = collections.Counter()
         for row in M:
