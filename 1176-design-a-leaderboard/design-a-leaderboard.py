2ee0f84bed72bffdc1169ab227076df3a57a1c37
fury93 <www.fanreala@gmail.com>
2024-11-24 01:42:58 +0300

    Time: 34 ms (13.86%) | Memory: 17.6 MB (6.91%) - LeetSync

--- 1176-design-a-leaderboard/design-a-leaderboard.py
+++ 1176-design-a-leaderboard/design-a-leaderboard.py
@@ -12,7 +12,110 @@ class Leaderboard:
     def reset(self, playerId: int) -> None:
         self.playersScore[playerId] = 0
         
+class Leaderboard1:
 
+    def __init__(self):
+        self.scores = defaultdict()
+
+    def addScore(self, playerId: int, score: int) -> None:
+        if playerId not in self.scores:
+            self.scores[playerId] = 0
+        self.scores[playerId] += score
+
+    def top(self, K: int) -> int:
+        values = [v for _, v in sorted(self.scores.items(), key=lambda item: item[1])]
+        values.sort(reverse=True)
+        total, i = 0, 0
+        while i < K:
+            total += values[i]
+            i += 1
+        
+        return total
+
+    def reset(self, playerId: int) -> None:
+        self.scores[playerId] = 0
+
+class Leaderboard2:
+
+    def __init__(self):
+        self.scores = {}
+
+    def addScore(self, playerId: int, score: int) -> None:
+        if playerId not in self.scores:
+            self.scores[playerId] = 0
+        self.scores[playerId] += score
+
+    def top(self, K: int) -> int:
+    
+        # This is a min-heap by default in Python.
+        heap = []
+        for x in self.scores.values():
+            heapq.heappush(heap, x)
+            if len(heap) > K:
+                heapq.heappop(heap)
+        res = 0
+        while heap:
+            res += heapq.heappop(heap)
+        return res
+
+    def reset(self, playerId: int) -> None:
+        self.scores[playerId] = 0
+
+from sortedcontainers import SortedDict
+
+class Leaderboard3:
+
+    def __init__(self):
+        self.scores = {}
+        self.sortedScores = SortedDict()
+
+    def addScore(self, playerId: int, score: int) -> None:
+
+        # The scores dictionary simply contains the mapping from the
+        # playerId to their score. The sortedScores contain a BST with 
+        # key as the score and value as the number of players that have
+        # that score.     
+        if playerId not in self.scores:
+            self.scores[playerId] = score
+            self.sortedScores[-score] = self.sortedScores.get(-score, 0) + 1
+        else:
+            preScore = self.scores[playerId]
+            val = self.sortedScores.get(-preScore)
+            if val == 1:
+                del self.sortedScores[-preScore]
+            else:
+                self.sortedScores[-preScore] = val - 1    
+            
+            newScore = preScore + score;
+            self.scores[playerId] = newScore
+            self.sortedScores[-newScore] = self.sortedScores.get(-newScore, 0) + 1
+        
+    def top(self, K: int) -> int:
+        count, total = 0, 0;
+
+        for key, value in self.sortedScores.items():
+            times = self.sortedScores.get(key)
+            for _ in range(times): 
+                total += -key;
+                count += 1;
+                
+                # Found top-K scores, break.
+                if count == K:
+                    break;
+                
+            # Found top-K scores, break.
+            if count == K:
+                break;
+        
+        return total;
+
+    def reset(self, playerId: int) -> None:
+        preScore = self.scores[playerId]
+        if self.sortedScores[-preScore] == 1:
+            del self.sortedScores[-preScore]
+        else:
+            self.sortedScores[-preScore] -= 1
+        del self.scores[playerId];
 
 # Your Leaderboard object will be instantiated and called as such:
 # obj = Leaderboard()
