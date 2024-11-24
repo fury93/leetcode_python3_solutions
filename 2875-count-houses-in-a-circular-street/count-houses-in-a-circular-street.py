e613044bb0083a788800d1ab86f8cad58e8b20fb
fury93 <www.fanreala@gmail.com>
2024-11-24 21:30:14 +0300

    Time: 176 ms (6.25%) | Memory: 17.7 MB (5.77%) - LeetSync

--- 2875-count-houses-in-a-circular-street/count-houses-in-a-circular-street.py
+++ 2875-count-houses-in-a-circular-street/count-houses-in-a-circular-street.py
@@ -23,3 +23,15 @@ class Solution:
             res += 1
             street.moveRight()
         return res
+
+class Solution2:
+    def houseCount(self, s: Optional['Street'], k: int) -> int:
+        a=0
+        s.moveLeft()
+        s.openDoor()
+
+        for i in range(1,k+1):
+            s.moveRight()
+            if s.isDoorOpen():a=i
+            s.closeDoor()
+        return a
