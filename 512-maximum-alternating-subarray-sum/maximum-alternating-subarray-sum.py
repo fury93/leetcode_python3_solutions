1eba23f5f2c128f5a97d5442738c77334848ef3c
fury93 <www.fanreala@gmail.com>
2024-11-24 19:32:40 +0300

    Time: 95 ms (100.00%) | Memory: 30.1 MB (45.59%) - LeetSync

--- 512-maximum-alternating-subarray-sum/maximum-alternating-subarray-sum.py
+++ 512-maximum-alternating-subarray-sum/maximum-alternating-subarray-sum.py
@@ -1,5 +1,17 @@
 class Solution:
     def maximumAlternatingSubarraySum(self, nums: List[int]) -> int:
+        add = nums[0]
+        sub = float('-inf')
+        rst = add
+
+        for num in nums[1:]:
+            add, sub = max(sub + num, num), add - num
+            rst = max(rst, add, sub)
+        
+        return rst
+        
+class Solution2:
+    def maximumAlternatingSubarraySum(self, nums: List[int]) -> int:
         max_sum = pos = nes = float('-inf')
         for i, num in enumerate(nums):
             pos, nes = max(nes + num, num), pos - num
