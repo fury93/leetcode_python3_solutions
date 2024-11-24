af5c738f85a73c633c4342e45323dfab8c3c3cd8
fury93 <www.fanreala@gmail.com>
2024-11-24 19:56:25 +0300

    Time: 36 ms (31.34%) | Memory: 27.6 MB (55.40%) - LeetSync

--- 2354-minimum-health-to-beat-game/minimum-health-to-beat-game.py
+++ 2354-minimum-health-to-beat-game/minimum-health-to-beat-game.py
@@ -6,3 +6,9 @@ class Solution:
             maxLevelDamage = max(maxLevelDamage, levelDamage)
         
         return allDamage - min(armor, maxLevelDamage) + 1
+
+class Solution2:
+    def minimumHealth(self, damage: List[int], armor: int) -> int:
+        tot = sum(damage)
+        tot -= min(armor, max(damage))
+        return tot + 1
