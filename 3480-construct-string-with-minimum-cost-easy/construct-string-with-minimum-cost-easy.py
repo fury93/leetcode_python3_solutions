6a61b8bf39b9b840fc6a70b90ea039d7ed9d99bd
fury93 <www.fanreala@gmail.com>
2024-11-24 22:28:12 +0300

    Time: 36 ms (92.86%) | Memory: 19.9 MB (29.63%) - LeetSync

--- 3480-construct-string-with-minimum-cost-easy/construct-string-with-minimum-cost-easy.py
+++ 3480-construct-string-with-minimum-cost-easy/construct-string-with-minimum-cost-easy.py
@@ -1,5 +1,22 @@
 class Solution:
     def minimumCost(self, target: str, words: List[str], costs: List[int]) -> int:
+        
+        @cache
+        def construct_string(i):
+            if i == len(target):
+                return 0
+            min_cost = float('inf')
+            for j, word in enumerate(words):
+                if target.startswith(word, i):
+                    min_cost = min(min_cost, costs[j] + construct_string(i + len(word)))
+            return min_cost
+        
+        min_cost = construct_string(0)
+        return min_cost if min_cost != float('inf') else -1
+        
+        
+class Solution2:
+    def minimumCost(self, target: str, words: List[str], costs: List[int]) -> int:
         n = len(target)
         dp = [0] + [inf] * n
         for i in range(n):
