19067053072e6376e84843990a81af7e75c56e76
fury93 <www.fanreala@gmail.com>
2024-11-24 22:15:16 +0300

    Time: 127 ms (97.98%) | Memory: 18.9 MB (25.21%) - LeetSync

--- 3425-maximum-number-of-potholes-that-can-be-fixed/maximum-number-of-potholes-that-can-be-fixed.py
+++ 3425-maximum-number-of-potholes-that-can-be-fixed/maximum-number-of-potholes-that-can-be-fixed.py
@@ -1,5 +1,25 @@
 class Solution:
     def maxPotholes(self, road: str, budget: int) -> int:
+        groups = [len(x) for x in road.split(".")if x]
+        groups.sort(reverse = True) 
+        total_fixed = 0    
+        remaning_budget = budget
+    
+        for size in groups:
+            cost = size + 1 
+            if remaning_budget >= cost:
+                total_fixed += size
+                remaning_budget -= cost
+            else:
+                if remaning_budget > 1:  
+                    total_fixed += remaning_budget - 1 
+                break
+
+        return total_fixed
+        
+
+class Solution2:
+    def maxPotholes(self, road: str, budget: int) -> int:
         res = 0
         for holes in sorted(map(len, road.split('.')), reverse=True):
             res += min(holes, max(0, budget - 1))
