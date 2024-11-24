f4c7124ea3a8e722dc6c96de5a4bcb6b30b4a598
fury93 <www.fanreala@gmail.com>
2024-11-24 18:57:02 +0300

    Time: 389 ms (100.00%) | Memory: 31.2 MB (5.26%) - LeetSync

--- 1937-maximize-the-beauty-of-the-garden/maximize-the-beauty-of-the-garden.py
+++ 1937-maximize-the-beauty-of-the-garden/maximize-the-beauty-of-the-garden.py
@@ -1,5 +1,26 @@
 class Solution:
     def maximumBeauty(self, flowers: List[int]) -> int:
+        prefix = [0]
+
+        indexes = defaultdict(list)
+
+        for i, flower in enumerate(flowers):
+            val = 0 if flower < 0 else flower
+            prefix.append(prefix[-1] + val)
+            indexes[flower].append(i)
+
+        ans = float("-inf")
+        for k, v in indexes.items():
+            if len(v) < 2:
+                continue
+            first, last = v[0], v[-1]
+            add = 2 * k if k < 0 else 0
+            ans = max(ans, add + prefix[last + 1] - prefix[first])
+        return ans
+
+        
+class Solution2:
+    def maximumBeauty(self, flowers: List[int]) -> int:
         positive_prefix_sum = [0]
         positions = {}
         maxsum = -inf
