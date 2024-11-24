4b5c0125b84ef64518103690f1a233b56b870db6
fury93 <www.fanreala@gmail.com>
2024-11-24 19:49:58 +0300

    Time: 3 ms (71.43%) | Memory: 16.8 MB (25.00%) - LeetSync

--- 2314-remove-all-ones-with-row-and-column-flips-ii/remove-all-ones-with-row-and-column-flips-ii.py
+++ 2314-remove-all-ones-with-row-and-column-flips-ii/remove-all-ones-with-row-and-column-flips-ii.py
@@ -1,4 +1,22 @@
-class Solution(object):
+class Solution:
+    def removeOnes(self, grid: List[List[int]]) -> int:
+        num_rows, num_cols, seen, res = len(grid), len(grid[0]), set(), inf
+
+        def backtrack(num_flips: int)-> None:
+            nonlocal res, seen
+            cells_of_one = [(row_idx, col_idx) for row_idx, col_idx in product(range(num_rows), range(num_cols)) if grid[row_idx][col_idx] and not {row_idx, col_idx+15} & seen]
+            if not cells_of_one: res = min(res, num_flips)
+
+            for row_idx, col_idx in cells_of_one:
+                seen |= {row_idx, col_idx+15}
+                backtrack(num_flips+1)
+                seen -= {row_idx, col_idx+15}
+            return
+
+        backtrack(0)
+        return res
+        
+class Solution2:
     def removeOnes(self, grid):
         m = len(grid)
         n = len(grid[0])
