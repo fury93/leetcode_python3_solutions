882719ef78000dc8fe7de90775bef5bbc0c5f7cf
fury93 <www.fanreala@gmail.com>
2024-11-24 22:12:13 +0300

    Time: 112 ms (100.00%) | Memory: 16.6 MB (73.91%) - LeetSync

--- 3401-guess-the-number-using-bitwise-questions-ii/guess-the-number-using-bitwise-questions-ii.py
+++ 3401-guess-the-number-using-bitwise-questions-ii/guess-the-number-using-bitwise-questions-ii.py
@@ -2,5 +2,17 @@
 # def commonBits(num: int) -> int:
 
 class Solution:
+    def findNumber(self) -> int:
+        ans = 0
+        baseline = commonBits(0)
+        for i in range(30):
+            b = 1 << i
+            res = commonBits(b)
+            if res > baseline:
+                ans |= b
+            baseline = res
+        return ans
+
+class Solution2:
     def findNumber(self):
         return reduce(lambda a, b: a | b,[1 << i  for i in range(30) if commonBits(1 << i) > commonBits(1 << i)])        
