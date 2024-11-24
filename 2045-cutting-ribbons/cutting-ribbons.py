18a668f5015315c7d26bc66580e90e6a8f900716
fury93 <www.fanreala@gmail.com>
2024-11-24 19:12:08 +0300

    Time: 167 ms (98.23%) | Memory: 29.3 MB (84.53%) - LeetSync

---
+++ 2045-cutting-ribbons/cutting-ribbons.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxLength(self, ribbons: List[int], k: int) -> int:
+        sm, mx = sum(ribbons), max(ribbons)
+        l, r = mx // k, min(mx, sm //k)
+
+        def isEnough(cutSize, k):
+            for size in ribbons:
+                k -= size // cutSize
+                if k <= 0: return True
+            return False
+                
+        while l < r:
+            m = (l + r + 1) // 2
+            if isEnough(m, k): l = m
+            else: r = m - 1
+        
+        return l
