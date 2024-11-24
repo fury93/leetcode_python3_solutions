f8e7a36373cb405021f80acfc8dab6c3922539f8
fury93 <www.fanreala@gmail.com>
2024-11-24 21:57:16 +0300

    Time: 178 ms (99.04%) | Memory: 17.2 MB (49.84%) - LeetSync

--- 3254-number-of-divisible-triplet-sums/number-of-divisible-triplet-sums.py
+++ 3254-number-of-divisible-triplet-sums/number-of-divisible-triplet-sums.py
@@ -1,4 +1,22 @@
 class Solution:
+    def divisibleTripletCount(self, nums: List[int], d: int) -> int:
+        n = len(nums)
+        if n < 3:
+            return 0
+        ans = 0
+        map_ = [0] * d
+        map_[ (nums[0] + nums[1]) % d ] = 1
+        for k in range(2, n):
+            nums[k] %= d
+            x = (d - nums[k]) % d
+            ans += map_[x]
+ 
+            for l in range(k - 1, -1, -1):
+                x = (nums[l] + nums[k]) % d
+                map_[x] += 1
+        return ans 
+        
+class Solution2:
     def divisibleTripletCount(self, A: List[int], d: int) -> int:
         ans = 0
         for i in range(len(A)):
