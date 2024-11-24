fd6130a7d66061b5f298ab390798644762ac8bb6
fury93 <www.fanreala@gmail.com>
2024-11-24 19:01:28 +0300

    Time: 602 ms (91.67%) | Memory: 20.7 MB (92.90%) - LeetSync

--- 1959-minimum-path-cost-in-a-hidden-grid/minimum-path-cost-in-a-hidden-grid.py
+++ 1959-minimum-path-cost-in-a-hidden-grid/minimum-path-cost-in-a-hidden-grid.py
@@ -13,7 +13,48 @@
 #        
 #
 
-class Solution(object):
+class Solution:
+    DIR = ['U', 'R', 'D', 'L']
+    STEPS = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+    def __init__(self):
+        self.map = {}
+        self.target = None
+
+    def findShortestPath(self, master):
+        # DFS to map out the grid
+        self.dfs(master, 0, 0)
+        
+        if self.target is None:
+            return -1
+
+        # Priority Queue for shortest path
+        pq = [(0, 0, 0)]  # (cost, x, y)
+        while pq:
+            cur_cost, x, y = heapq.heappop(pq)
+            if (x, y) == self.target:
+                return cur_cost
+            for step in self.STEPS:
+                x1, y1 = x + step[0], y + step[1]
+                if (x1, y1) not in self.map:
+                    continue
+                val = self.map.pop((x1, y1))
+                heapq.heappush(pq, (cur_cost + val, x1, y1))
+
+        return -1
+
+    def dfs(self, master, x, y):
+        if master.isTarget():
+            self.target = (x, y)
+        for i in range(4):
+            x1, y1 = x + self.STEPS[i][0], y + self.STEPS[i][1]
+            if (x1, y1) not in self.map and master.canMove(self.DIR[i]):
+                val = master.move(self.DIR[i])
+                self.map[(x1, y1)] = val
+                self.dfs(master, x1, y1)
+                master.move(self.DIR[(i + 2) % 4])
+
+class Solution2:
     def findShortestPath(self, master: 'GridMaster') -> int:
         opposite = {'D': 'U', 'U': 'D', 'L': 'R', 'R': 'L'}       # opposite direction to move back and force
         cost_dict = collections.defaultdict(lambda: sys.maxsize)  # save cost for each location (x, y)
