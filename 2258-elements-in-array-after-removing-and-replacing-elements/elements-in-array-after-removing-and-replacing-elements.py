48b77085296c54fd58a7379f4dc124f98f130e6d
fury93 <www.fanreala@gmail.com>
2024-11-24 19:41:53 +0300

    Time: 26 ms (100.00%) | Memory: 50.7 MB (37.93%) - LeetSync

--- 2258-elements-in-array-after-removing-and-replacing-elements/elements-in-array-after-removing-and-replacing-elements.py
+++ 2258-elements-in-array-after-removing-and-replacing-elements/elements-in-array-after-removing-and-replacing-elements.py
@@ -1,5 +1,23 @@
 class Solution:
     def elementInNums(self, nums: List[int], queries: List[List[int]]) -> List[int]:
+        # time, index
+        n = len(nums)
+        
+        ans = []
+        cached = []
+        for i in range(n):
+            cached.append(nums[i:])
+        for i in range(n):
+            cached.append(nums[:i])
+        cycle = len(cached)
+        # print(cached)
+        for time, index in queries:
+            t = time%cycle
+            ans.append(cached[t][index] if index<len(cached[t]) else -1)
+        return ans 
+        
+class Solution2:
+    def elementInNums(self, nums: List[int], queries: List[List[int]]) -> List[int]:
         def answerQuery(query):
             time, idx = query
             time %= len(nums)*2
