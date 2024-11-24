4a0f476d1312014c37d44eabe25d7d05ae42be09
fury93 <www.fanreala@gmail.com>
2024-11-24 21:28:51 +0300

    Time: 303 ms (100.00%) | Memory: 30.9 MB (59.40%) - LeetSync

--- 2729-minimum-operations-to-make-numbers-non-positive/minimum-operations-to-make-numbers-non-positive.py
+++ 2729-minimum-operations-to-make-numbers-non-positive/minimum-operations-to-make-numbers-non-positive.py
@@ -1,4 +1,27 @@
 class Solution:
+    def minOperations(self, nums: List[int], x: int, y: int) -> int:
+        nums.sort()
+        d = x - y
+        def check(steps):
+            sub = steps * y
+            idx = bisect.bisect_right(nums, sub)
+            cnt = steps
+            for i in range(idx, len(nums)):
+                cnt -= (nums[i] - sub - 1) // d + 1
+                if cnt < 0:
+                    return False 
+            return True
+        l = nums[-1] // x - 1
+        r = nums[-1] // y + 1
+        while (r - l)>1:
+            m = (r+l) //2 
+            if check(m):
+                r = m 
+            else:
+                l = m 
+        return r
+        
+class Solution2:
     def minOperations(self, nums: list[int], x: int, y: int) -> int:
         def predicate(ops: int) -> bool:
             return sum(max(0, math.ceil((num - ops * y) / (x - y)))
