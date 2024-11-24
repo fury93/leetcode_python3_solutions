16d86a722d8b0fa4ec28841d078f397e626b32ea
fury93 <www.fanreala@gmail.com>
2024-11-24 21:10:00 +0300

    Time: 30 ms (60.00%) | Memory: 26.8 MB (85.29%) - LeetSync

--- 2644-bitwise-or-of-all-subsequence-sums/bitwise-or-of-all-subsequence-sums.py
+++ 2644-bitwise-or-of-all-subsequence-sums/bitwise-or-of-all-subsequence-sums.py
@@ -11,3 +11,11 @@ class Solution:
             result |= num | prefix_sum
 
         return result
+
+class Solution:
+    def subsequenceSumOr(self, nums: List[int]) -> int:
+        ans = prefix = 0 
+        for x in nums: 
+            prefix += x
+            ans |= x | prefix 
+        return ans 
