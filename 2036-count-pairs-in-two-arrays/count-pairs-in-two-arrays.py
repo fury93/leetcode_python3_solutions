7ba0b3dd1293a33262ac2e4ec4c2764ea85a45c9
fury93 <www.fanreala@gmail.com>
2024-11-24 19:11:04 +0300

    Time: 275 ms (24.45%) | Memory: 32.9 MB (97.83%) - LeetSync

--- 2036-count-pairs-in-two-arrays/count-pairs-in-two-arrays.py
+++ 2036-count-pairs-in-two-arrays/count-pairs-in-two-arrays.py
@@ -32,3 +32,26 @@ class Solution:
                 result += N - left
 
         return result
+
+class Solution2:
+    def countPairs(self, nums1, nums2):
+        N = len(nums1)  # nums2 is the same length
+
+        # Difference[i] stores nums1[i] - nums2[i]
+        difference = [nums1[i] - nums2[i] for i in range(N)]
+        difference.sort()
+
+        # Count the number of valid pairs
+        result = 0
+        left = 0
+        right = N - 1
+        while left < right:
+            # Left makes a valid pair with right
+            # Right also makes a valid pair with the indices between the pointers
+            if difference[left] + difference[right] > 0:
+                result += right - left
+                right -= 1
+            # Left and right are not a valid pair
+            else:
+                left += 1
+        return result
