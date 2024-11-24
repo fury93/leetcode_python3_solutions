813b3d8ed27d4a238540a5ae0b8797165daf75f6
fury93 <www.fanreala@gmail.com>
2024-11-24 20:12:12 +0300

    Time: 0 ms (100.00%) | Memory: 16.9 MB (81.31%) - LeetSync

---
+++ 2468-valid-palindrome-iv/valid-palindrome-iv.py
@@ -0,0 +1,12 @@
+class Solution:
+    def makePalindrome(self, s: str) -> bool:
+        swapsAllowed, l, r = 2, 0, len(s) - 1
+        while l < r:
+            if s[l] == s[r]:
+                l, r = l + 1, r - 1
+            elif swapsAllowed > 0:
+                swapsAllowed -= 1
+                l, r = l + 1, r - 1
+            else: return False
+
+        return True
