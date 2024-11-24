781f7014116f93c4595f19102578f089797e1f37
fury93 <www.fanreala@gmail.com>
2024-11-24 20:08:54 +0300

    Time: 156 ms (64.76%) | Memory: 34.1 MB (74.35%) - LeetSync

--- 2056-jump-game-viii/jump-game-viii.py
+++ 2056-jump-game-viii/jump-game-viii.py
@@ -1,6 +1,26 @@
 class Solution:
     def minCost(self, nums: List[int], costs: List[int]) -> int:
         n = len(nums)
+        dp = [math.inf] * n
+        dp[0] = 0
+
+        increase_stack, decrease_stack = [] * n, [] * n
+
+        for i in range(n):
+            while decrease_stack and nums[decrease_stack[-1]] <= nums[i]:
+                dp[i] = min(dp[i], dp[decrease_stack.pop()] + costs[i])
+
+            while increase_stack and nums[increase_stack[-1]] > nums[i]:
+                dp[i] = min(dp[i], dp[increase_stack.pop()] + costs[i])
+
+            increase_stack.append(i)
+            decrease_stack.append(i)
+
+        return dp[-1]
+        
+class Solution2:
+    def minCost(self, nums: List[int], costs: List[int]) -> int:
+        n = len(nums)
         nse, nge = [n] * n, [n] * n
         maxmono, minmono = [], []
         for index in range(n):
