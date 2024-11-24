2b69ea8020027f910534411ef07bc1b2485d58ed
fury93 <www.fanreala@gmail.com>
2024-11-24 20:07:54 +0300

    Time: 977 ms (75.79%) | Memory: 16.9 MB (53.31%) - LeetSync

--- 2426-maximum-profit-from-trading-stocks/maximum-profit-from-trading-stocks.py
+++ 2426-maximum-profit-from-trading-stocks/maximum-profit-from-trading-stocks.py
@@ -5,3 +5,20 @@ class Solution:
             for j in range(budget, p-1, -1):
                 dp[j] = max(dp[j], dp[j-p] + f-p)
         return dp[-1]
+
+class Solution2:
+    def maximumProfit(self, present: List[int], future: List[int], budget: int) -> int:
+        profit_list = []
+        for p, f in zip(present, future):
+            profit = f - p
+            if profit < 0 or p > budget:
+                continue
+            profit_list.append((profit, p))
+        
+        dp = [0] *(budget+1)
+        for profit, price in profit_list:
+            print(profit, price)
+            for i in range(budget, price-1, -1):
+                dp[i] = max(dp[i], dp[i-price]+profit)
+            
+        return dp[-1]
