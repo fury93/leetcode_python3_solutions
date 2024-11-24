b46552104fb74cdd8ba526150b9729075a967a03
fury93 <www.fanreala@gmail.com>
2024-11-24 19:16:36 +0300

    Time: 0 ms (100.00%) | Memory: 16.8 MB (25.00%) - LeetSync

---
+++ 2070-check-if-string-is-decomposable-into-value-equal-substrings/check-if-string-is-decomposable-into-value-equal-substrings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isDecomposable(self, s: str) -> bool:
+        isMetTwo = False
+        for _, gr in groupby(s):
+            ln = len(list(gr))
+            if ln % 3 == 0: continue
+            if ln % 3 != 2 or isMetTwo: return False
+            isMetTwo = True
+        return isMetTwo
