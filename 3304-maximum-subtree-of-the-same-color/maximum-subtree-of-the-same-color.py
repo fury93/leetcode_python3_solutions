454e431160c9b511faab5a8d2230c82c5e9dcb53
fury93 <www.fanreala@gmail.com>
2024-11-24 22:01:22 +0300

    Time: 179 ms (46.88%) | Memory: 51.7 MB (33.33%) - LeetSync

--- 3304-maximum-subtree-of-the-same-color/maximum-subtree-of-the-same-color.py
+++ 3304-maximum-subtree-of-the-same-color/maximum-subtree-of-the-same-color.py
@@ -20,4 +20,28 @@ class Solution:
             return color, freq, 1 
         dfs(0,-1)
         return res
+
+class Solution2:
+    def maximumSubtreeSize(self, edges: List[List[int]], colors: List[int]) -> int:
+        def dfs(node):
+            if node not in tree:
+                return True, 1
+            color = colors[node]
+            same, cnt = True, 0
+            for child in tree[node]:
+                same_child, cnt_child = dfs(child)
+                if colors[child] != color or same_child is False:
+                    same = False                    
+                cnt += cnt_child
+            if same:
+                ans[0] = max(ans[0], 1 + cnt)
+            return same, 1 + cnt
+
+        n = len(colors)
+        tree = defaultdict(list)
+        for u, v in edges:
+            tree[u].append(v)
+        ans = [1]
+        dfs(0)
+        return ans[0]
         
