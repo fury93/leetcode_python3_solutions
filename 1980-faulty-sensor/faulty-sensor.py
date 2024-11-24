37eb034500838b1b8ae2334782b3c1e1408baeab
fury93 <www.fanreala@gmail.com>
2024-11-24 19:03:30 +0300

    Time: 0 ms (100.00%) | Memory: 16.6 MB (75.00%) - LeetSync

---
+++ 1980-faulty-sensor/faulty-sensor.py
@@ -0,0 +1,14 @@
+class Solution:
+    def badSensor(self, sensor1: List[int], sensor2: List[int]) -> int:
+        n = len(sensor1)
+        for i in range(n):
+            if sensor1[i] != sensor2[i]: # find point at which the sensors differ
+                break
+        j = k = i # init two pointers
+        while j < n-1 and sensor1[j] == sensor2[j+1]: # find to what point sensor1 data is shifted
+            j += 1
+        while k < n-1 and sensor1[k+1] == sensor2[k]: # find to what point sensor2 data is shifted
+            k += 1
+        # a sensor is faulty if the ptr reaches end of arr
+        # if both reach end of arr, it's impossible to tell
+        return -1 if k == n-1 and j == n-1 else 1 if j == n-1 else 2
