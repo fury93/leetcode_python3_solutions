0fa2e41f6ce755f059063c77bc5fbe565f3b55f4
fury93 <www.fanreala@gmail.com>
2024-11-24 19:57:38 +0300

    Time: 23 ms (77.14%) | Memory: 32.9 MB (46.79%) - LeetSync

---
+++ 1416-check-if-an-array-is-consecutive/check-if-an-array-is-consecutive.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isConsecutive(self, nums: List[int]) -> bool:
+        nums_set = set(nums)
+        minVal = min(nums)
+        for n in range(minVal, minVal + len(nums)):
+            if n not in nums_set:
+                return False
+        return True
