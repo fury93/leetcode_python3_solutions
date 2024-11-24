497e6ef1c9ef87e333caf72cee23f725a52e449e
fury93 <www.fanreala@gmail.com>
2024-11-24 20:12:53 +0300

    Time: 11 ms (74.45%) | Memory: 30.6 MB (49.43%) - LeetSync

--- 2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.py
+++ 2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -10,3 +10,13 @@ class Solution:
         
         return minNPos + len(nums) - 1 - maxNPos - doubleSwap   
 
+class Solution2:
+    def minimumSwaps(self, nums: List[int]) -> int:
+        mi = min(nums)                                     # find minimum
+        idx1 = nums.index(mi)                              # locate the first mi from the left side
+        
+        nums = [nums[idx1]] + nums[:idx1] + nums[idx1+1:]  # make the swaps & update `nums`
+        
+        mx = max(nums)                                     # find maximum 
+        idx2 = nums[::-1].index(mx)                        # locate the first mx from the right side
+        return idx1 + idx2                                 # return total swaps needed
