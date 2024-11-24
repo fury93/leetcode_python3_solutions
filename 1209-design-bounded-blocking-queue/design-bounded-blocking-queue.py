f5f05ff082f2edb30ff8a10654fc4ac8f0fc7544
fury93 <www.fanreala@gmail.com>
2024-11-24 23:31:15 +0300

    Time: 48 ms (60.45%) | Memory: 17.9 MB (61.91%) - LeetSync

--- 1209-design-bounded-blocking-queue/design-bounded-blocking-queue.py
+++ 1209-design-bounded-blocking-queue/design-bounded-blocking-queue.py
@@ -1,6 +1,27 @@
 import threading
 
 class BoundedBlockingQueue(object):
+
+    def __init__(self, capacity: int):
+        self.queue = deque([])
+        self.enqueueLock = threading.Semaphore(capacity)
+        self.dequeueLock = threading.Semaphore(0)
+
+    def enqueue(self, element: int) -> None:
+        self.enqueueLock.acquire()
+        self.queue.append(element)
+        self.dequeueLock.release()
+
+    def dequeue(self) -> int:
+        self.dequeueLock.acquire()
+        element = self.queue.popleft()
+        self.enqueueLock.release()
+        return element
+
+    def size(self) -> int:
+        return len(self.queue)
+
+class BoundedBlockingQueue2:
     def __init__(self, capacity: int):
         self.capacity = capacity
         
