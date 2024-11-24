b39291b563fb68ac24e868c1f50e1e79a30e3768
fury93 <www.fanreala@gmail.com>
2024-11-24 19:22:44 +0300

    Time: 99 ms (31.58%) | Memory: 28.4 MB (51.85%) - LeetSync

---
+++ 2111-binary-searchable-numbers-in-an-unsorted-array/binary-searchable-numbers-in-an-unsorted-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def binarySearchableNumbers(self, nums: List[int]) -> int:
+        ans, n = 0, len(nums)
+        prefix = [False] * n                  # Create prefix array and initialize them as False
+        cur_max, cur_min = nums[0], nums[-1]  # Initialize prefix_max & suffix_min
+        for i in range(n):                    # Update prefix from left to right
+            prefix[i] = cur_max <= nums[i] 
+            cur_max = max(cur_max, nums[i])
+        for i in range(n-1, -1, -1):          # Update suffix and count from right to left
+            prefix[i] &= cur_min >= nums[i]   # Use `&` operation (`and` is also fine) to make sure 2 condition hold at the same time
+            ans += prefix[i]                  # +1 when `prefix[i]` is True, otherwise +0 (no change)
+            cur_min = min(cur_min, nums[i])    
+        return ans
