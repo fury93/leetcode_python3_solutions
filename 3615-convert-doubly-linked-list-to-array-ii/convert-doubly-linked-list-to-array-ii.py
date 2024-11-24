d49d2669868f4bce03474a12a90b82b84b5fefc8
fury93 <www.fanreala@gmail.com>
2024-11-24 22:32:00 +0300

    Time: 86 ms (11.84%) | Memory: 18.5 MB (11.86%) - LeetSync

--- 3615-convert-doubly-linked-list-to-array-ii/convert-doubly-linked-list-to-array-ii.py
+++ 3615-convert-doubly-linked-list-to-array-ii/convert-doubly-linked-list-to-array-ii.py
@@ -8,6 +8,20 @@ class Node:
 """
 class Solution:
     def toArray(self, node: 'Optional[Node]') -> List[int]:
+        ans = []
+        # move to the head of the list
+        while node.prev:
+            node = node.prev
+        
+        # traverse the list and collect Values
+        while node:
+            ans.append(node.val)
+            node = node.next
+
+        return ans
+        
+class Solution2:
+    def toArray(self, node: 'Optional[Node]') -> List[int]:
         res, cur = deque(), node
         while cur.prev:
             cur = cur.prev
