2e7f9be2c2d458fb82388a9e4e8db2aaa9bc786d
fury93 <www.fanreala@gmail.com>
2024-11-24 19:35:55 +0300

    Time: 607 ms (97.30%) | Memory: 16.9 MB (43.42%) - LeetSync

--- 2209-number-of-equal-count-substrings/number-of-equal-count-substrings.py
+++ 2209-number-of-equal-count-substrings/number-of-equal-count-substrings.py
@@ -1,5 +1,32 @@
 class Solution:
     def equalCountSubstrings(self, s: str, count: int) -> int:
+        
+        n = len(s)
+        windows = 0
+
+        for unique in range(1, len(set(s)) + 1):
+            
+            windowSize = unique * count
+            if windowSize > n:
+                break
+
+            counts = Counter(s[ : windowSize])
+            atMostCount = sum([val >= count for val in counts.values()])
+            windows += (atMostCount == unique)
+
+            for i in range(windowSize, n):
+                counts[s[i - windowSize]] -= 1
+                if counts[s[i - windowSize]] == count - 1:
+                    atMostCount -= 1
+                counts[s[i]] += 1
+                if counts[s[i]] == count:
+                    atMostCount += 1
+                windows += (atMostCount == unique)
+
+        return windows
+        
+class Solution2:
+    def equalCountSubstrings(self, s: str, count: int) -> int:
         ans = 0 
         for k in range(1, 27): 
             freq = Counter()
