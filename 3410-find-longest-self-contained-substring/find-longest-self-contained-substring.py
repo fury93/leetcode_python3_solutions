f25d75f754708858a960b7ffe00629958de7ee02
fury93 <www.fanreala@gmail.com>
2024-11-24 22:13:31 +0300

    Time: 119 ms (90.91%) | Memory: 19.2 MB (60.00%) - LeetSync

--- 3410-find-longest-self-contained-substring/find-longest-self-contained-substring.py
+++ 3410-find-longest-self-contained-substring/find-longest-self-contained-substring.py
@@ -1,4 +1,36 @@
+from bisect import bisect
 class Solution:
+    def maxSubstringLength(self, s: str) -> int:
+        n = len(s)
+        cnt = defaultdict(list)
+        for i, c in enumerate(s):
+            cnt[c].append(i)
+        # print(cnt)
+        def valid(l, r, c1, c2):
+            for c in cnt:
+                if not( (c==c1 or c==c2) or (cnt[c][0] > l and cnt[c][-1] < r) or bisect(cnt[c], l) == bisect(cnt[c], r)):
+                    return False
+            return True
+        ans = -1
+        for c1 in cnt:
+            s1, e1 = cnt[c1][0], cnt[c1][-1]
+            for c2 in cnt:
+                s2, e2 = cnt[c2][0], cnt[c2][-1]
+                # print([s1,e1], [s2,e2])
+                length = e2-s1 + 1
+                if length < ans or length == n:
+                    continue
+                if c1 == c2 and valid(s1, e2, c1, c2):
+                    ans = length
+                if e1 > e2 or s1 > s2:
+                    continue
+                if valid(s1, e2, c1, c2):
+                    ans = length
+                # print(ans, left, right, c1, c2, length)
+        return ans
+                
+                
+class Solution2:
     def maxSubstringLength(self, s: str) -> int:
         cnts = Counter(s)
         
