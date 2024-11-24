b31f2bc29e1165f8ae1124434c10f65f8f9a6837
fury93 <www.fanreala@gmail.com>
2024-11-24 21:58:12 +0300

    Time: 160 ms (100.00%) | Memory: 31.5 MB (60.00%) - LeetSync

--- 3268-minimum-number-of-coins-for-fruits-ii/minimum-number-of-coins-for-fruits-ii.py
+++ 3268-minimum-number-of-coins-for-fruits-ii/minimum-number-of-coins-for-fruits-ii.py
@@ -1,5 +1,28 @@
 class Solution:
     def minimumCoins(self, prices: List[int]) -> int:
+
+        n = len(prices)
+        pq = []
+        if n == 1:
+            return prices[0]
+        
+        #[cost to get here, last_usable_idx]
+        q = deque()
+        q.append([prices[0], 2])
+        for i in range(1,n):
+
+            while q[0][1] < i:
+                q.popleft()
+            
+            my_best = q[0][0] + prices[i]
+            while q and q[-1][0] >= my_best:
+                q.pop()
+            q.append([my_best, i+i+2])
+
+        return q[0][0] if q[0][1] >= n else q[1][0]
+        
+class Solution2:
+    def minimumCoins(self, prices: List[int]) -> int:
         n = len(prices)
         buy, not_buy = prices[0], inf
         heap = [(prices[0], 1)]
