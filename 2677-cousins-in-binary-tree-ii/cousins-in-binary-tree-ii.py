648cefd5c986d8ed1e4f167cd9e0821d0429dc9e
fury93 <www.fanreala@gmail.com>
2024-11-26 21:10:48 +0300

    Time: 189 ms (33.67%) | Memory: 66.2 MB (40.22%) - LeetSync

--- 2677-cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.py
+++ 2677-cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.py
@@ -38,6 +38,85 @@ class Solution:
                 levelSum[level+1] = curLevelSum
         
         return root
+        
             
+class Solution1:
+    def replaceValueInTree(self, root):
+        if not root:
+            return root
+        node_queue = deque([root])
+        level_sums = []
+
+        # First BFS: Calculate sum of nodes at each level
+        while node_queue:
+            level_sum = 0
+            level_size = len(node_queue)
+            for _ in range(level_size):
+                current_node = node_queue.popleft()
+                level_sum += current_node.val
+                if current_node.left:
+                    node_queue.append(current_node.left)
+                if current_node.right:
+                    node_queue.append(current_node.right)
+            level_sums.append(level_sum)
+
+        # Second BFS: Update each node's value to sum of its cousins
+        node_queue.append(root)
+        level_index = 1
+        root.val = 0  # Root has no cousins
+        while node_queue:
+            level_size = len(node_queue)
+            for _ in range(level_size):
+                current_node = node_queue.popleft()
+
+                sibling_sum = (
+                    current_node.left.val if current_node.left else 0
+                ) + (current_node.right.val if current_node.right else 0)
+
+                if current_node.left:
+                    current_node.left.val = (
+                        level_sums[level_index] - sibling_sum
+                    )
+                    node_queue.append(current_node.left)
+                if current_node.right:
+                    current_node.right.val = (
+                        level_sums[level_index] - sibling_sum
+                    )
+                    node_queue.append(current_node.right)
+            level_index += 1
+
+        return root
             
         
+class Solution2:
+    def __init__(self):
+        self.level_sums = [0] * 100000
+
+    def replaceValueInTree(self, root):
+        self._calculate_level_sum(root, 0)
+        self.replace_value_in_tree_internal(root, 0, 0)
+        return root
+
+    def _calculate_level_sum(self, node, level):
+        if node is None:
+            return
+        self.level_sums[level] += node.val
+        self._calculate_level_sum(node.left, level + 1)
+        self._calculate_level_sum(node.right, level + 1)
+
+    def replace_value_in_tree_internal(self, node, sibling_sum, level):
+        if node is None:
+            return
+        left_child_val = 0 if node.left is None else node.left.val
+        right_child_val = 0 if node.right is None else node.right.val
+
+        if level == 0 or level == 1:
+            node.val = 0
+        else:
+            node.val = self.level_sums[level] - node.val - sibling_sum
+        self.replace_value_in_tree_internal(
+            node.left, right_child_val, level + 1
+        )
+        self.replace_value_in_tree_internal(
+            node.right, left_child_val, level + 1
+        )
