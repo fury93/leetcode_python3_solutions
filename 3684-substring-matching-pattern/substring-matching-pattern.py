8d75bb606010b906b38555b2a1fc96a8e014adb9
fury93 <www.fanreala@gmail.com>
2025-01-08 21:37:42 +0300

    Time: 3 ms (39.76%) | Memory: 17.8 MB (43.66%) - LeetSync

---
+++ 3684-substring-matching-pattern/substring-matching-pattern.py
@@ -0,0 +1,6 @@
+class Solution:
+    def hasMatch(self, s: str, p: str) -> bool:
+        s1, s2 = p.split('*')
+        start1, start2 = s.find(s1), s.rfind(s2)
+        return start1 > -1 and start2 > -1 and start1 + len(s1) <= start2 
+        
