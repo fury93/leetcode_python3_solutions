935cc5d1a22b107dfd1d2ebcc6c6f93669130444
fury93 <www.fanreala@gmail.com>
2025-03-06 05:06:46 +0300

    Time: 5 ms (79.04%) | Memory: 18.1 MB (85.35%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        vals, dup, mis = set(), None, 1
+        for val in chain(*grid):
+            if val in vals: dup = val
+            vals.add(val)
+        
+        while mis in vals: mis += 1
+                
+        return [dup, mis]
