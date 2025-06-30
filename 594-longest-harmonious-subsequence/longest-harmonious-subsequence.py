e3e963988623a13411a3b0e46b15bcde9c671d7c
fury93 <www.fanreala@gmail.com>
2025-06-30 04:55:02 +0300

    Time: 27 ms (65.29%) | Memory: 19.2 MB (46.65%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
@@ -5,18 +5,3 @@ class Solution:
         for key, val in cnt.items():
             res = max(res, val + cnt.get(key+1, -val))
         return res
-
-    # todo not finished yet...lol
-    def findLHS2(self, nums: List[int]) -> int:
-        arr = sorted(nums)
-        l, m, res = 0, 0, 0
-        for r, val in enumerate(arr):
-            if r > 0 and arr[r] != arr[r - 1]:
-                m = r
-            diff = abs(val - arr[l])
-            if diff > 1:
-                if l != m:
-                    res = max(res, r - l + 1)
-                l = r + 1
-
-        return res
