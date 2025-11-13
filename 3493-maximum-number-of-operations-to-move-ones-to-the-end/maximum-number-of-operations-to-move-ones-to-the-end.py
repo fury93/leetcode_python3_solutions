5fb797af80292f102b05907d46f1a9a8064912e1
fury93 <www.fanreala@gmail.com>
2025-11-13 14:56:43 +0300

    Time: 61 ms (58.68%) | Memory: 18.8 MB (17.37%) - LeetSync

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.py
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.py
@@ -1,5 +1,14 @@
 class Solution:
     def maxOperations(self, s: str) -> int:
+        res = ones = 0
+        for v, l in groupby(s):
+            if v == '1':
+                ones += len(list(l))
+            else:
+                res += ones
+        return res
+
+    def maxOperations2(self, s: str) -> int:
         res, ones, lastId = 0, 0, len(s) - 1
         for i, ch in enumerate(s):
             if ch == '1':
@@ -10,4 +19,3 @@ class Solution:
                 res += ones
         
         return res
-        
