3e12010d9aa76d5b064e4497df5f1b18ab844931
fury93 <www.fanreala@gmail.com>
2025-12-31 04:18:36 +0300

    Time: 11 ms (70.44%) | Memory: 19.7 MB (6.94%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,9 +1,24 @@
 class Solution:
     def trap(self, h: List[int]) -> int:
-        res, l, r = 0, 0, len(h) - 1
-        lMax, rMax = 0, 0
+        res, stack = 0, []
+        for r, rightBound in enumerate(h):
+            while stack and h[stack[-1]] < rightBound:
+                bar = h[stack.pop()]
+                if stack:
+                    l, leftBound = stack[-1], h[stack[-1]]
+                    minBound = min(rightBound, leftBound)
+                    res += (r - l - 1) * (minBound - bar)
+            stack.append(r)
 
-        while l <= r:
+        return res
+            
+    # two pointers    
+    def trap2(self, h: List[int]) -> int:
+        res, l , r, lMax, rMax = 0, 0, len(h) - 1, 0, 0
+        
+        if r < 2: return res
+
+        while l < r:
             if h[l] < h[r]:
                 lMax = max(lMax, h[l])
                 res += lMax - h[l]
@@ -12,5 +27,5 @@ class Solution:
                 rMax = max(rMax, h[r])
                 res += rMax - h[r]
                 r -= 1
-
+        
         return res
