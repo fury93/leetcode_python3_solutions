85494a20c78fd00595f0825648c606010b6e8345
fury93 <www.fanreala@gmail.com>
2026-01-07 06:58:55 +0300

    Time: 54 ms (96.71%) | Memory: 19.9 MB (17.11%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.py
@@ -1,9 +1,40 @@
-# Dijkstra
+# 0-1 BFS
 class Solution:
     def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
         rows, cols = len(grid), len(grid[0])
+        MAX_DAMAGE = rows + cols + 1
+        q = deque([(0, 0)])
+        damage = [[MAX_DAMAGE] * cols for _ in range(rows)]
+        damage[0][0] = grid[0][0]
+
+        while q:
+            r, c = q.popleft()
+            
+            if r == rows - 1 and c == cols - 1:
+                return damage[-1][-1] < health
+
+            for dr, dc in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols:
+                    newDamage = grid[nr][nc]
+                    if damage[r][c] + newDamage < damage[nr][nc]:
+                        damage[nr][nc] = damage[r][c] + newDamage
+                        
+                        if newDamage:
+                            q.append((nr, nc))
+                        else:
+                            q.appendleft((nr, nc))
+
+        return False
+        
+# Dijkstra
+class Solution2:
+    def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
+        INF = rows + cols + 1
+        rows, cols = len(grid), len(grid[0])
         pq = [(grid[0][0], 0, 0)]
-        visited = [[rows + cols + 1] * cols for _ in range(rows)]
+        # how many health we lost to get to this cell
+        visited = [[INF] * cols for _ in range(rows)]
 
         while pq:
             h, r, c = heappop(pq)
