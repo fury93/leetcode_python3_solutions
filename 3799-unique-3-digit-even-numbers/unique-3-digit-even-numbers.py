053df6a85c0127f5abc329bab30b4bc55f2f29be
fury93 <www.fanreala@gmail.com>
2026-01-09 17:33:39 +0300

    Time: 12 ms (80.32%) | Memory: 19.4 MB (5.02%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
@@ -1,4 +1,13 @@
 class Solution:
-    def totalNumbers(self, digits: List[int]) -> int:
+    def totalNumbers(self, digits: list[int]) -> int:
+        numbers = set()
+
+        for a, b, c in permutations(digits, 3):
+            if c % 2 == 0 and a != 0:
+                numbers.add((a, b, c))
+
+        return len(numbers)
+
+    def totalNumbers2(self, digits: List[int]) -> int:
         cnt = Counter(map(str, digits))
         return sum(cnt >= Counter(str(n)) for n in range(100, 1000, 2))
