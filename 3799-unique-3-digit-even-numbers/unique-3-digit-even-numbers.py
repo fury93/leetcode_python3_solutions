aae602352cf3deb5c1daf97854a8367a3b231a06
fury93 <www.fanreala@gmail.com>
2026-01-09 17:30:41 +0300

    Time: 876 ms (5.02%) | Memory: 19.5 MB (5.02%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
@@ -1,4 +1,4 @@
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
-        cnt = Counter(digits)
-        return sum(cnt >= Counter(map(int, str(n))) for n in range(100, 999, 2))
+        cnt = Counter(map(str, digits))
+        return sum(cnt >= Counter(str(n)) for n in range(100, 1000, 2))
