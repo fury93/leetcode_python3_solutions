08b15f2d861a9357706988a9372376c6ebb6cbe2
fury93 <www.fanreala@gmail.com>
2026-01-10 20:25:46 +0300

    Time: 0 ms (100.00%) | Memory: 19.6 MB (14.10%) - LeetSync

--- 4052-equal-score-substrings/equal-score-substrings.py
+++ 4052-equal-score-substrings/equal-score-substrings.py
@@ -1,5 +1,9 @@
 class Solution:
     def scoreBalance(self, s: str) -> bool:
+        scores = list(accumulate(s, lambda x, y: x + ord(y) - 96, initial = 0))
+        return not scores[-1] & 1 and scores[-1]//2 in scores
+    
+    def scoreBalance2(self, s: str) -> bool:
         weights = [ord(ch) - 96 for ch in s]
         scores = list(accumulate(weights))
         return not scores[-1] & 1 and scores[-1]//2 in scores
