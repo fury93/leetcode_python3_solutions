66c0a4863b1e11aa9a556572a7db3b02167b592a
fury93 <www.fanreala@gmail.com>
2026-01-18 00:20:27 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (68.51%) - LeetSync

--- 4186-count-residue-prefixes/count-residue-prefixes.py
+++ 4186-count-residue-prefixes/count-residue-prefixes.py
@@ -1,3 +1,10 @@
 class Solution:
     def residuePrefixes(self, s: str) -> int:
+        res, uniq = 0, set()
+        for i, ch in enumerate(s, start = 1):
+            uniq.add(ch)
+            res += len(uniq) == i % 3
+        return res
+
+    def residuePrefixes2(self, s: str) -> int:
         return sum(len(set(s[:i])) == i % 3 for i in range(1, len(s)+1))
