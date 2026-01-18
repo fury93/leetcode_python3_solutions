83e963182972b6e95d55b55db25f0e3482e5ad16
fury93 <www.fanreala@gmail.com>
2026-01-18 04:37:30 +0300

    Time: 3 ms (16.67%) | Memory: 19.4 MB (9.80%) - LeetSync

--- 3879-find-minimum-log-transportation-cost/find-minimum-log-transportation-cost.py
+++ 3879-find-minimum-log-transportation-cost/find-minimum-log-transportation-cost.py
@@ -1,5 +1,8 @@
 class Solution:
     def minCuttingCost(self, n: int, m: int, k: int) -> int:
+        return k * max(n - k, m - k, 0)
+
+    def minCuttingCost2(self, n: int, m: int, k: int) -> int:
         def getCost(logSize, maxSize):
             cost = 0
             while logSize > maxSize:
