f53fdc60b9997bea7b16a80b829ca558f20cc932
fury93 <www.fanreala@gmail.com>
2026-01-19 23:03:02 +0300

    Time: 5 ms (50.00%) | Memory: 19.5 MB (18.02%) - LeetSync

--- 929-groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.py
+++ 929-groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.py
@@ -1,9 +1,12 @@
 class Solution:
     def numSpecialEquivGroups(self, words: List[str]) -> int:
+        return len({("".join(sorted(s[0::2])), "".join(sorted(s[1::2]))) for s in words})
+    
+    def numSpecialEquivGroups2(self, words: List[str]) -> int:
         groups = set()
         for w in words:
-            even = ''.join(sorted(w[::2]))
-            odd = ''.join(sorted(w[1::2]))
+            even = "".join(sorted(w[::2]))
+            odd = "".join(sorted(w[1::2]))
             groups.add((even, odd))
         return len(groups)
 
