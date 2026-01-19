73b449dc495f00424e8d981afc7158992704b601
fury93 <www.fanreala@gmail.com>
2026-01-19 23:54:01 +0300

    Time: 2463 ms (45.57%) | Memory: 19.5 MB (10.63%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.py
@@ -1,6 +1,22 @@
-# O(N^2)
 class Solution:
     def beautySum(self, s: str) -> int:
+        res, L = 0, len(s)
+        nums = [ord(ch) - 97 for ch in s]
+        for i in range(L):
+            freq = [0] * 26
+            for j in range(i, L):
+                freq[nums[j]] += 1
+                
+                mx, mn  = 0, L
+                for fr in freq:
+                    if fr > 0:
+                        mx = max(mx, fr)
+                        mn = min(mn, fr)
+                res += mx - mn
+                
+        return res
+
+    def beautySum2(self, s: str) -> int:
         res, freq = 0, defaultdict(int)
         for i in range(len(s)):
             freq = [0] * 26
