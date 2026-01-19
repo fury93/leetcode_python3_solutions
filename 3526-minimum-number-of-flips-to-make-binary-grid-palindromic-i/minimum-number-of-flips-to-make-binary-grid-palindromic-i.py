21f564978e6295667ed2a7447713bcd2f531181c
fury93 <www.fanreala@gmail.com>
2026-01-19 07:13:11 +0300

    Time: 366 ms (15.38%) | Memory: 55.1 MB (57.27%) - LeetSync

--- 3526-minimum-number-of-flips-to-make-binary-grid-palindromic-i/minimum-number-of-flips-to-make-binary-grid-palindromic-i.py
+++ 3526-minimum-number-of-flips-to-make-binary-grid-palindromic-i/minimum-number-of-flips-to-make-binary-grid-palindromic-i.py
@@ -1,6 +1,6 @@
 class Solution:
     def minFlips(self, grid: List[List[int]]) -> int:
         def getFlips(row):
-            return sum(row[i] != row[~i] for i in range(len(row)//2))
+            return sum(row[i] ^ row[~i] for i in range(len(row)//2))
 
-        return min( sum(map(getFlips, grid)), sum(map(getFlips, zip(*grid))) )
+        return min(sum(map(getFlips, grid)), sum(map(getFlips, zip(*grid))))
