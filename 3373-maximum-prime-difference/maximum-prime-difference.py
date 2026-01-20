17ddd9e631b5bb1080345b69efcb148e27eed87a
fury93 <www.fanreala@gmail.com>
2026-01-20 20:54:51 +0300

    Time: 620 ms (96.47%) | Memory: 29.5 MB (12.50%) - LeetSync

--- 3373-maximum-prime-difference/maximum-prime-difference.py
+++ 3373-maximum-prime-difference/maximum-prime-difference.py
@@ -1,22 +1,24 @@
-class Solution:
-    def maximumPrimeDifference(self, nums: List[int]) -> int:
-        def getPrimes(limit):
+# https://leetcode.com/u/yourick/
+
+def getPrimes(limit):
             primes = [True] * (limit + 1)
             primes[0] = primes[1] = False
             
-            for p in range(2, int(limit**0.5) + 1):
+            for p in range(2, math.isqrt(limit) + 1):
                 if primes[p]:
                     for i in range(p * p, limit + 1, p):
                         primes[i] = False
             return primes
     
-        primes = getPrimes(100)
-        
-        l ,r = 0, len(nums) - 1
-        while l < len(nums) and not primes[nums[l]]:
-            l += 1
-        while r >= 0 and not primes[nums[r]]:
-            r -= 1
+PRIMES = getPrimes(100)
+
+class Solution:
+    def maximumPrimeDifference(self, nums: List[int]) -> int:
+        for l in range(len(nums)):
+            if PRIMES[nums[l]]: break
+
+        for r in range(len(nums)-1, -1, -1):
+            if PRIMES[nums[r]]: break
 
         return r - l
         
