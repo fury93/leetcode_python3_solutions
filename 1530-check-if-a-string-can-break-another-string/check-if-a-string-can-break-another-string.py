221790969d8717ef9875c9b4a9bd75294c8fdd59
fury93 <www.fanreala@gmail.com>
2026-01-20 05:58:56 +0300

    Time: 48 ms (92.00%) | Memory: 19.9 MB (58.29%) - LeetSync

--- 1530-check-if-a-string-can-break-another-string/check-if-a-string-can-break-another-string.py
+++ 1530-check-if-a-string-can-break-another-string/check-if-a-string-can-break-another-string.py
@@ -1,5 +1,19 @@
 class Solution:
     def checkIfCanBreak(self, s1: str, s2: str) -> bool:
+        d1, d2 = [0]*26, [0]*26
+        for c1, c2 in zip(s1, s2):
+            d1[ord(c1)-97] += 1
+            d2[ord(c2)-97] += 1
+        
+        superiority, break1, break2 = 0, True, True
+        for i in range(26):
+            superiority += d1[i] - d2[i]
+            if superiority > 0: break2 = False
+            elif superiority < 0: break1 = False
+            
+        return break1 or break2
+
+    def checkIfCanBreak2(self, s1: str, s2: str) -> bool:
         def isBreak(str1, str2):
             for ch1, ch2 in zip(str1, str2):
                 if ch1 < ch2: return False
