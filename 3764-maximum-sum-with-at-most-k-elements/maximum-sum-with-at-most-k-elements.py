feb85c8c25ed3afcee20f84c50420d6194f9227c
fury93 <www.fanreala@gmail.com>
2026-01-20 18:05:45 +0300

    Time: 549 ms (20.59%) | Memory: 60 MB (5.88%) - LeetSync

--- 3764-maximum-sum-with-at-most-k-elements/maximum-sum-with-at-most-k-elements.py
+++ 3764-maximum-sum-with-at-most-k-elements/maximum-sum-with-at-most-k-elements.py
@@ -1,5 +1,14 @@
+from heapq import nlargest
+from itertools import chain
 class Solution:
     def maxSum(self, grid: List[List[int]], limits: List[int], k: int) -> int:
+        candidates = []
+        for row, limit in zip(grid, limits):
+            candidates.append(nlargest(limit, row))
+        
+        return sum(nlargest(k, chain.from_iterable(candidates)))
+
+    def maxSum2(self, grid: List[List[int]], limits: List[int], k: int) -> int:
         candidates = []
         for row, limit in zip(grid, limits):
             candidates.extend(sorted(row, reverse = True)[:limit])
