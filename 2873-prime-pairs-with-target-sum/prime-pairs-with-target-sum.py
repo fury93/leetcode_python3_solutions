e5939e99e98cf0efc03f51f2b36f475210b8b8fc
fury93 <www.fanreala@gmail.com>
2026-01-20 22:25:37 +0300

    Time: 717 ms (92.67%) | Memory: 24.8 MB (100.00%) - LeetSync

--- 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.py
+++ 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.py
@@ -1,28 +1,18 @@
+def getPrimes(n):
+    primes = bytearray([1]) * (n + 1)
+    primes[0] = primes[1] = 0
+    
+    for p in range(2, int(math.isqrt(n)) + 1):
+        if primes[p]:
+            primes[p*p:n+1:p] = bytearray(len(range(p*p, n+1, p)))
+    
+    return primes
+
 class Solution:
     def findPrimePairs(self, n: int) -> List[List[int]]:
-        def getPrimes(limit):
-            primes = [True] * (limit + 1)
-            primes[0] = primes[1] = False
-            
-            for p in range(2, int(limit**0.5) + 1):
-                if primes[p]:
-                    for i in range(p * p, limit + 1, p):
-                        primes[i] = False
-            
-            return [val for val, isPrime in enumerate(primes) if isPrime]
-
         res, primes = [], getPrimes(n)
-        l, r = 0, len(primes) - 1
-
-        while l <= r:
-            sm = primes[l] + primes[r]
-            if sm < n:
-                l += 1
-            elif sm > n:
-                r -= 1
-            else:
-                res.append([primes[l], primes[r]])
-                l += 1
-                r -= 1
+        for i in range(2, n//2 + 1):
+            if primes[i] and primes[n-i]:
+                res.append([i, n-i])
         
         return res
