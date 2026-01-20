56afaefb0eac73bdca5d661aea25f105420e8cbd
fury93 <www.fanreala@gmail.com>
2026-01-20 07:42:38 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (23.44%) - LeetSync

--- 3579-maximum-possible-number-by-binary-concatenation/maximum-possible-number-by-binary-concatenation.py
+++ 3579-maximum-possible-number-by-binary-concatenation/maximum-possible-number-by-binary-concatenation.py
@@ -2,6 +2,11 @@ from itertools import permutations
 class Solution:
     def maxGoodNumber(self, nums: List[int]) -> int:
         bins = [f"{n:b}" for n in nums]
+        bins.sort(key = lambda x: x*10, reverse=True)
+        return int(''.join(bins), 2)
+
+    def maxGoodNumber2(self, nums: List[int]) -> int:
+        bins = [f"{n:b}" for n in nums]
         res = 0
 
         for perm in permutations(bins):
