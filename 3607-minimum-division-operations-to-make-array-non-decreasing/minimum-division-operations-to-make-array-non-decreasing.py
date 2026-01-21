702334b5b1db4dd956fede99435dc9a6384e2f42
fury93 <www.fanreala@gmail.com>
2026-01-21 07:52:28 +0300

    Time: 63 ms (90.36%) | Memory: 66.4 MB (6.02%) - LeetSync

--- 3607-minimum-division-operations-to-make-array-non-decreasing/minimum-division-operations-to-make-array-non-decreasing.py
+++ 3607-minimum-division-operations-to-make-array-non-decreasing/minimum-division-operations-to-make-array-non-decreasing.py
@@ -1,5 +1,24 @@
+N = 10**6 + 1
+lpf = list(range(N))
+
+for i in range(2, math.isqrt(N)+1, 1):
+    if lpf[i] == i:
+        for j in range(i*i, N, i):
+            if lpf[j] == j:
+                lpf[j] = i
+
 class Solution:
+    # with precomputation of lpf(least prime factors)
     def minOperations(self, nums: List[int]) -> int:
+        res = 0
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i] <= nums[i+1]: continue
+            nums[i] = lpf[nums[i]]
+            if nums[i] > nums[i+1]: return -1
+            res += 1
+        return res
+
+    def minOperations2(self, nums: List[int]) -> int:
         @cache
         def getLowestFactor(n):
             if n % 2 == 0: return 2
@@ -7,7 +26,6 @@ class Solution:
             for d in range(3, math.isqrt(n)+1, 1):
                 if n % d == 0:
                      return d
-
             return n
 
         res = 0
