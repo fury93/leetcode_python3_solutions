a2c8adb411bd3c6adbaba47477828a52f9e6ae08
fury93 <www.fanreala@gmail.com>
2026-01-23 03:07:05 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (11.79%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.py
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.py
@@ -1,19 +1,15 @@
 class Solution:
     def largestInteger(self, nums: List[int], k: int) -> int:
-        cnt = Counter(nums)
-        uniq = set(key for key in cnt if cnt[key] == 1)
-        if k == 1:
-            return max(uniq, default = -1)
         if k == len(nums):
             return max(nums)
+
+        cnt = Counter(nums)
+        uniq = set(key for key in cnt if cnt[key] == 1)
         
         if k == 1:
-            return max(nums)
+            return max(uniq, default = -1)
         
-        cnt1 = nums.count(nums[0])
-        cnt2 = nums.count(nums[-1])
+        if nums[0] not in uniq:
+            return nums[-1] if nums[-1] in uniq else -1
 
-        if cnt1 > 1:
-            return -1 if cnt2 > 1 else nums[-1]
-
-        return nums[0] if cnt2 > 1 else max(nums[0], nums[-1])
+        return nums[0] if nums[-1] not in uniq else max(nums[0], nums[-1])
