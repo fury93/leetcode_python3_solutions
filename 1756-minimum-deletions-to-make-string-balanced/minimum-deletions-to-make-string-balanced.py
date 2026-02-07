be974ff47e645435303c8603285deb9c138f37a0
fury93 <www.fanreala@gmail.com>
2026-02-07 03:33:06 +0300

    Time: 146 ms (86.85%) | Memory: 20 MB (63.56%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.py
@@ -4,12 +4,10 @@ class Solution:
         min_deletions = 0
         b_count = 0
 
-        # min_deletions variable represents dp[i]
         for ch in s:
             if ch == "b":
                 b_count += 1
             else:
-                # Two cases: remove 'a' or keep 'a'
                 min_deletions = min(min_deletions + 1, b_count)
 
         return min_deletions
