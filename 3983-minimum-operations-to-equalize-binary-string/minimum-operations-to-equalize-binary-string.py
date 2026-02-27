26d178404367803b7d1732b662e45bab7dfb3e8b
fury93 <www.fanreala@gmail.com>
2026-02-27 03:12:35 +0300

    Time: 0 ms (100.00%) | Memory: 20.4 MB (52.00%) - LeetSync

--- 3983-minimum-operations-to-equalize-binary-string/minimum-operations-to-equalize-binary-string.py
+++ 3983-minimum-operations-to-equalize-binary-string/minimum-operations-to-equalize-binary-string.py
@@ -1,23 +1,31 @@
 class Solution:
     def minOperations(self, s: str, k: int) -> int:
-        n, m = len(s), s.count("0")
-        dist = [math.inf] * (n + 1)
-        nodeSets = [
-            SortedList(range(0, n + 1, 2)),
-            SortedList(range(1, n + 1, 2)),
-        ]
-        q = deque([m])
-        dist[m] = 0
-        nodeSets[m % 2].remove(m)
-        while q:
-            m = q.popleft()
-            c1, c2 = max(k - n + m, 0), min(m, k)
-            lnode, rnode = m + k - 2 * c2, m + k - 2 * c1
-            nodeSet = nodeSets[lnode % 2]
-            idx = nodeSet.bisect_left(lnode)
-            while idx < len(nodeSet) and nodeSet[idx] <= rnode:
-                m2 = nodeSet[idx]
-                dist[m2] = dist[m] + 1
-                q.append(m2)
-                nodeSet.pop(idx)
-        return -1 if dist[0] == math.inf else dist[0]
+        n = len(s)
+        z = s.count('0')
+        
+        if n == k:
+            if z == 0:
+                return 0
+            elif z == n:
+                return 1
+            else:
+                return -1
+        
+        def ceil(x, y):
+            return (x + y - 1) // y
+        
+        ans = inf
+        
+        if z % 2 == 0:
+            m = max(ceil(z, k), ceil(z, n - k))
+            if m % 2 == 1:
+                m += 1
+            ans = min(ans, m)
+        
+        if z % 2 == k % 2:
+            m = max(ceil(z, k), ceil(n - z, n - k))
+            if m % 2 == 0:
+                m += 1
+            ans = min(ans, m)
+        
+        return ans if ans < inf else -1
