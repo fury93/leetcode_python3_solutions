28f3da0635d2777345fae0fb9adb77074f6c8689
fury93 <www.fanreala@gmail.com>
2026-03-07 21:06:10 +0300

    Time: 2 ms (81.39%) | Memory: 19.3 MB (75.58%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -2,25 +2,24 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         time, q, fresh, R, C = 0, deque(), 0,  len(grid), len(grid[0])
 
-        for x, y in product(range(R), range(C)):
-            if grid[x][y] == 2:
-                q.append((x, y))
-            elif grid[x][y] == 1:
+        for r, c in product(range(R), range(C)):
+            if grid[r][c] == 2:
+                q.append((r, c))
+            elif grid[r][c] == 1:
                 fresh += 1
         
         while q and fresh > 0:
             for i in range(len(q)):
-                x, y = q.popleft()
-                for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
-                    xx, yy = x + dx, y + dy
-                    if 0 <= xx < R and 0 <= yy < C and grid[xx][yy] == 1:
-                        grid[xx][yy] = 2
+                r, c = q.popleft()
+                for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < R and 0 <= nc < C and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
                         fresh -= 1
-                        q.append((xx, yy))
+                        q.append((nr, nc))
             time += 1
 
         return time if not fresh else - 1
-
 
 
     def orangesRotting2(self, grid: List[List[int]]) -> int:
