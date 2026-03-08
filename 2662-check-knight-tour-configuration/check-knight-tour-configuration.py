e409674d650098cbcfe889afcc80e93902cf429e
fury93 <www.fanreala@gmail.com>
2026-03-08 05:38:16 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (78.57%) - LeetSync

--- 2662-check-knight-tour-configuration/check-knight-tour-configuration.py
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.py
@@ -1,10 +1,10 @@
 class Solution:
     def checkValidGrid(self, grid: List[List[int]]) -> bool:
         if grid[0][0] != 0: return False
-        R, C = len(grid), len(grid[0])
-        moves = [None] * (R * C)
+        N = len(grid)
+        moves = [None] * N**2
 
-        for r, c in product(range(R), range(C)):
+        for r, c in product(range(N), range(N)):
             moves[grid[r][c]] = (r, c)
 
         for prev, cur in pairwise(moves):
