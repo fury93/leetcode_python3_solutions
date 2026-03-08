c55159b15e27b8bab6539d874bbeb77d72171dd8
fury93 <www.fanreala@gmail.com>
2026-03-08 19:52:25 +0300

    Time: 67 ms (100.00%) | Memory: 24.3 MB (86.82%) - LeetSync

--- 1106-escape-a-large-maze/escape-a-large-maze.py
+++ 1106-escape-a-large-maze/escape-a-large-maze.py
@@ -2,6 +2,33 @@ class Solution:
     def isEscapePossible(self, blocked: List[List[int]], source: List[int], target: List[int]) -> bool:
         N = 10**6
         escapeDist = len(blocked) * 2
+        blocked = {tuple(block) for block in blocked}
+        
+        def check(start, end):
+            stack = [tuple(start)]
+            visited = {tuple(start)}
+            
+            while stack:
+                r, c = stack.pop()
+                if [r, c] == end: return True
+                
+                manhDist = abs(r - start[0]) + abs(c - start[1])
+                if manhDist > escapeDist:
+                    return True
+                
+                for dr, dc in [(0,1), (0,-1), (1,0), (-1,0)]:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < N and 0 <= nc < N and (nr, nc) not in blocked and (nr, nc) not in visited:
+                        visited.add((nr, nc))
+                        stack.append((nr, nc))
+            return False
+
+        return check(source, target) and check(target, source)
+
+    # DFS recursive
+    def isEscapePossible2(self, blocked: List[List[int]], source: List[int], target: List[int]) -> bool:
+        N = 10**6
+        escapeDist = len(blocked) * 2
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         blocked = {(r, c) for r, c in blocked}
         isReachDestination = False
@@ -14,7 +41,6 @@ class Solution:
 
             manhDist = abs(r - start[0]) + abs(c - start[1])
             if manhDist > escapeDist:
-                print(r, c)
                 return True
 
             for dr, dc in directions:
