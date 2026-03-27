2d31a9b4f2be887d339477082cf4e704fb35dc25
fury93 <www.fanreala@gmail.com>
2026-03-27 09:40:28 +0300

    Time: 7 ms (22.35%) | Memory: 19.4 MB (66.47%) - LeetSync

---
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.py
@@ -0,0 +1,13 @@
+class Solution:
+    def areSimilar(self, mat: List[List[int]], k: int) -> bool:
+
+        deq = map(deque, mat)
+
+        for i, row in enumerate(deq):
+            
+            shifted = row.copy()
+            shifted.rotate(k)
+
+            if row != shifted: return False
+
+        return True
