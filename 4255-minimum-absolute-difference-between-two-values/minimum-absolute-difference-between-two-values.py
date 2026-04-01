d548f639e31fc9bf99ff6a8cf572b0b77bd4a1dc
fury93 <www.fanreala@gmail.com>
2026-04-01 21:37:35 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (79.82%) - LeetSync

--- 4255-minimum-absolute-difference-between-two-values/minimum-absolute-difference-between-two-values.py
+++ 4255-minimum-absolute-difference-between-two-values/minimum-absolute-difference-between-two-values.py
@@ -1,11 +1,10 @@
 class Solution:
     def minAbsoluteDifference(self, nums: list[int]) -> int:
-        res, prevPos = len(nums), None
+        res, prevPos = inf, None
         for i, n in enumerate(nums):
             if n == 0: continue
             if prevPos is not None and n != nums[prevPos]:
-                res = min(res, abs(i - prevPos))
+                res = min(res, i - prevPos)
             prevPos = i
 
-        return -1 if res == len(nums) else res
-
+        return -1 if res == inf else res
