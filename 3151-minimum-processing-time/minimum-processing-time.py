013a1fcc2002e93d4b08a68de66c0cef1beb602a
fury93 <www.fanreala@gmail.com>
2026-04-03 00:01:25 +0300

    Time: 63 ms (54.58%) | Memory: 33.5 MB (87.25%) - LeetSync

---
+++ 3151-minimum-processing-time/minimum-processing-time.py
@@ -0,0 +1,3 @@
+class Solution:
+    def minProcessingTime(self, processorTime: List[int], tasks: List[int]) -> int:
+        return max(a + b for a, b in zip(sorted(processorTime), sorted(tasks, reverse=True)[::4]))
